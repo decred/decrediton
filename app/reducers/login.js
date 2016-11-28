6bbcb4137f6c94ff5f3e6a975d81d3a40069ddfa
Alex Yocom-Piatt <ay-p@decred.org>
2016-11-28 13:46:47 -0600

    Add babel-transform-object-rest-spread

--- app/reducers/login.js
+++ app/reducers/login.js
@@ -3,19 +3,17 @@ import { LOGIN, SET_CLIENT } from '../actions/LoginActions';
 const login = (state = {}, action) => {
     switch (action.type) {
         case LOGIN:
-            return [...state, 
-                {
+            return {...state, 
                     address: action.address,
                     port: action.port,
                     passphrase: action.passphrase,
                     loggedIn: true,
-                }
-            ];
+        
+            };
         case SET_CLIENT:
-            return [...state, {
+            return {...state, 
                     client: action.client
-                }
-            ];
+                };
         default:
             return state
     }
