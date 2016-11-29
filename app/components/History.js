0f9853c6c7948a36bd8111ad9405a80c270b374c
Alex Yocom-Piatt <ay-p@decred.org>
2016-11-29 16:05:07 +0000

    Fill out history page with a stub table for txs

--- app/components/History.js
+++ app/components/History.js
@@ -3,6 +3,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { getBalance } from '../actions/client';
 import { Link } from 'react-router';
+import { Button, Row, Col, Table } from 'react-bootstrap';
 
 class History extends Component{
   static propTypes = {
@@ -14,10 +15,47 @@ class History extends Component{
     
     const clientSet = (
       <div>
-        <h1>History</h1>
-        <h2>Balance:</h2>
-        <h2>{getBalance(client)}</h2>
-        <Link to="/">Back Home</Link>
+        <Row>
+          <Col sm={2}>
+            <Link to="/">Back Home</Link>
+          </Col>
+          <Col sm={3}>
+            <h5>Transaction History</h5>
+          </Col>
+          <Col sm={7}>
+            <h5>Balance: {getBalance(client)}</h5>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+          <Table striped bordered condensed hover>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>TXID</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>20:20:20 November 28th 2016</td>
+                <td>txid</td>
+                <td>200</td>
+              </tr>
+              <tr>
+                <td>20:20:20 November 28th 2016</td>
+                <td>txid</td>
+                <td>200</td>
+              </tr>
+              <tr>
+                <td>20:20:20 November 28th 2016</td>
+                <td>txid</td>
+                <td>200</td>
+              </tr>
+            </tbody>
+          </Table>
+          </Col>
+        </Row>
       </div>
     )
     
