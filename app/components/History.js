91fee88d7a9182a1a2cfd9f54074ec733c49e379
Alex Yocom-Piatt <ay-p@decred.org>
2016-11-30 12:00:17 -0600

    Add back in stub table

--- app/components/History.js
+++ app/components/History.js
@@ -47,6 +47,32 @@ class History extends Component{
             <Row>
               <Col sm={12} >
                 <h1>History Page</h1>
+                <Table striped bordered condensed hover>
+                  <thead>
+                    <tr>
+                      <th>Date</th>
+                      <th>TXID</th>
+                      <th>Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>20:20:20 November 28th 2016</td>
+                      <td>txid</td>
+                      <td>200</td>
+                    </tr>
+                    <tr>
+                      <td>20:20:20 November 28th 2016</td>
+                      <td>txid</td>
+                      <td>200</td>
+                    </tr>
+                    <tr>
+                      <td>20:20:20 November 28th 2016</td>
+                      <td>txid</td>
+                      <td>200</td>
+                    </tr>
+                  </tbody>
+                </Table>
               </Col>
             </Row>
           </div>
