cf0ae03a91469db9bb3ec6c57917781c6463340e
Alex Yocom-Piatt <ay-p@decred.org>
2016-12-12 16:57:42 -0600

    Fix header for drawer/menuitems

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,6 +2,36 @@ import React, { Component, PropTypes } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import { Link } from 'react-router';
+
+const styles = {
+  sidebar: {
+    width: 256,
+    height: '100%',
+  },
+  sidebarLink: {
+    display: 'block',
+    padding: '16px 0px',
+    color: '#757575',
+    textDecoration: 'none',
+  },
+  sidebarLinkCurrentPage: {
+    display: 'block',
+    padding: '16px 0px',
+    color: 'white',
+    textDecoration: 'bold',
+  },
+  divider: {
+    margin: '8px 0',
+    height: 1,
+    backgroundColor: '#757575',
+  },
+  content: {
+    padding: '16px',
+    height: '100%',
+    backgroundColor: '#2ed8a3',
+  },
+};
 
 class Header extends Component {
 
@@ -9,18 +39,23 @@ class Header extends Component {
     super(props);
     this.state = {open:false};
   }
-
+  handleToggle = () => this.setState({open: !this.state.open});
+  
   render() {
+    const page = "HOME";
     return (
       <div>
-        <AppBar onLeftIconButtonTouchTap={() => {this._toggle}} title="Decrediton" />
+        <AppBar onLeftIconButtonTouchTap={this.handleToggle} title="Decrediton" />
         <Drawer
           ref="Drawer"
           docked={false}
           open={this.state.open}
           onRequestChange={open => this.setState({open})}
         >
-          <MenuItem>Settings</MenuItem>
+          <MenuItem><Link to="/" style={styles.sidebarLink}>Home</Link></MenuItem>
+          <MenuItem><Link to="/history" style={styles.sidebarLink}>Transaction History</Link></MenuItem>
+          <MenuItem><Link to="/send" style={styles.sidebarLink}>Send Decred</Link></MenuItem>
+          <MenuItem><Link to="/receive" style={styles.sidebarLink}>Receive Decred</Link></MenuItem>
         </Drawer>
       </div>
     );
