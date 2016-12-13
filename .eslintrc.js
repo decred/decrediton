0bc0b4cd627784fd4f971c54147cf95ce9f6925c
John C. Vernaleo <john@netpurgatory.com>
2016-12-13 14:15:14 -0500

    Add eslint with basic rules.
    Run with:
    npm run lint
    
    Add lint fix command too:
    npm run lint-fix
    
    Closes #34

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "parser": "babel-eslint",
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
