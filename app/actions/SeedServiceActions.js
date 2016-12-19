01ec20140e4f0cde8ad1eee149e94035d98e6ab9
Alex Yocom-Piatt <ay-p@decred.org>
2016-12-19 14:11:49 +0000

    lint-fix

--- app/actions/SeedServiceActions.js
+++ app/actions/SeedServiceActions.js
@@ -87,7 +87,7 @@ function decodeSeedError(error) {
 function decodeSeedSuccess(pubPass, privPass, response) {
   return (dispatch) => {
     dispatch({response: response, type: DECODESEED_SUCCESS });
-    dispatch(createWalletRequest(pubPass, privPass, response.decoded_seed, true))
+    dispatch(createWalletRequest(pubPass, privPass, response.decoded_seed, true));
   };
 }
 
