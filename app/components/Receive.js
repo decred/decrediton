0cc15724eef437d9b739eeadc18dbb7d197c6037
Alex Yocom-Piatt <ay-p@decred.org>
2017-01-05 17:18:33 +0000

    Add stub QR and add first attempt for receive

--- app/components/Receive.js
+++ app/components/Receive.js
@@ -33,12 +33,32 @@ class Receive extends Component{
           onClick={!getNextAddressRequestAttempt? () => this.props.getNextAddressAttempt(0) : null}
           label={getNextAddressRequestAttempt ? 'Getting new address' : 'Get New Address'}/>
       </div>);
-
+    const copayReceive = (
+      	<div id="page-content-wrapper">
+					<div class="row row-first-heading row-wallet-settings">
+						<div class="col-xs-12">
+							<p>My Decred Address</p>
+						</div>
+					</div>
+					<div class="row row-my-decred text-center grey-background">
+						<div class="col-xs-12 white-background">
+							<img src="img/qr.jpg" alt="" />
+						</div>
+						<div class="col-xs-12">
+							<input type="text" class="form-control input-well-code" value="DsVmEFoSorV3bpTQHshHpV1asdfKSTkqskC" />
+							<p>Share this wallet address to receive payments, To protect your privacy, new addresses are generated automatically once you use them.</p>
+						</div>
+						<div class="col-xs-12">
+							<button type="button" class="btn btn-info">Generate new address</button>
+						</div>
+					</div>
+				</div>
+    )
     /* Check to see that client is not undefined */
     if (walletService === null) {
       return (<ErrorScreen />);
     } else {
-      return(receiveView);
+      return(copayReceive);
     }
   }
 }
