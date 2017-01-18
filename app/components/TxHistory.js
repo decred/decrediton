c6854dbdc69a140060c63a115f95ae22e71d33b8
Alex Yocom-Piatt <ay-p@decred.org>
2017-01-18 13:47:43 -0500

    Pull in more css from design spec

--- app/components/TxHistory.js
+++ app/components/TxHistory.js
@@ -1,52 +1,70 @@
 import React, { Component } from 'react';
-import {Table, TableBody, TableHeader, TableHeaderColumn,
-  TableRow, TableRowColumn} from 'material-ui/Table';
 import { reverseHash } from '../helpers/byteActions';
 
 const styles = {
-  topBar: {
-    position: 'absolute',
-    top:'0px',
-    right: '0px',
-    left:'0px',
-    height: '70px',
-    backgroundColor: '#132f4b',
-    color: 'white',
+  historyContainer: {
+    width: '100%',
+    maxWidth: '100%',
+    margin: 'auto',
   },
-  title: {
-    marginTop: '23px',
-    marginLeft: '20px',
-    position: 'absolute',
-    fontSize: '1.5em',
+  transactionRow: {
+    padding: '8px 0 10px 0',
+    display: 'block',
+    borderBottom: '1px solid #e2e2e2',
+    fontSize: '2.25rem',
+    fontWeight: '600',
+    padding: '0',
+    backgroundColor: '#ffffff',
+    width: '100%',
+    float: 'left',
+    position: 'relative',
+    minHeight: '1px',
+    boxSizing: 'border-box',
   },
+  plusMinusIcon: {
+    height: '16.5px',
+    width: '16.5px',
+    margin: '0 15px',
+    verticalAlign: 'middle',
+    backgroundColor: 'green',
+  },
+  txAmount: {
+    fontSize:'1.0rem',
+  },
+  txDateSince: {
+    float: 'right',
+    paddingTop: '6px',
+    fontSize: '1.1rem',
+    fontWeight: 'normal',
+  },
+  txArrow: {
+    height: '13px',
+    width: '13px',
+    margin: '-2px 15px',
+    backgroundColor: 'transparent',
+  }
 };
 
 class TxHistory extends Component {
   render() {
     const transactions = this.props.transactions;
+    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+    var today = new Date();
     return (
-      <Table fixedHeader={true} striped bordered condensed hover showCheckboxes={false} >
-        <TableHeader displaySelectAll={false}>
-          <TableRow>
-            <TableHeaderColumn>Block Number</TableHeaderColumn>
-            <TableHeaderColumn>Date</TableHeaderColumn>
-            <TableHeaderColumn>Transaction Hash</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody displayRowCheckbox={false}>
-          {transactions.map(function(tx, i) {
-          var parseDate = new Date(tx.transaction.getMinedTransactions().getTimestamp()*1000);
-          var s = Buffer.from(tx.transaction.getMinedTransactions().getTransactionsList()[0].getHash()).toString('hex');
-          var reversed = reverseHash(s);
-          return (
-            <TableRow key={i}>
-              <TableRowColumn>{tx.transaction.getMinedTransactions().getHeight()}</TableRowColumn>
-                <TableRowColumn><span>{parseDate.toString()}</span></TableRowColumn>
-              <TableRowColumn colSpan={3}>{reversed}</TableRowColumn>
-            </TableRow>);
+      <div style={styles.historyContainer}>
+        {transactions.map(function(tx, i) {
+        var parseDate = new Date(tx.transaction.getMinedTransactions().getTimestamp()*1000);
+        var diffDays = Math.round(Math.abs((parseDate.getTime() - today.getTime())/(oneDay)));
+        var s = Buffer.from(tx.transaction.getMinedTransactions().getTransactionsList()[0].getHash()).toString('hex');
+        var reversed = reverseHash(s);
+        return (
+            <div style={styles.transactionRow} key={i}>
+              <span style={styles.plusMinusIcon}>+/-</span>
+              <span style={styles.txAmount}>{tx.transaction.getMinedTransactions().getHeight()}</span>
+              <span style={styles.txDateSince}>{diffDays} Days Since<span style={styles.txArrow}>></span></span>
+            </div>);
           })}
-        </TableBody>
-      </Table>);
+      </div>);
   }
 }
 
