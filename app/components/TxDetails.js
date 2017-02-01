c26000838f28c157556985361f0472ffcb24858f
Alex Yocom-Piatt <ay-p@decred.org>
2017-02-01 15:17:32 -0500

    Getting closer

--- app/components/TxDetails.js
+++ app/components/TxDetails.js
@@ -3,18 +3,61 @@ import React, { Component } from 'react';
 import Balance from './Balance';
 import IndicatorPending from './icons/indicator-pending.svg';
 import IndicatorConfirmed from './icons/indicator-confirmed.svg';
-import PlusSmall from './icons/plus-small.svg';
-import MinusSmall from './icons/minus-small.svg';
-import ArrowRightGray from './icons/arrow-right-gray.svg';
-import ArrowRightKeyBlue from './icons/arrow-right-key-blue.svg';
+import PlusBig from './icons/plus-big.svg';
+import MinusBig from './icons/minus-big.svg';
 import dateFormat from 'dateformat';
 import './fonts.css';
 
 const styles = {
-  historyContainer: {
-    width: '100%',
-    maxWidth: '100%',
-    margin: 'auto',
+  view: {
+    width: '880px',
+    height: '100%',
+    float: 'right',
+    backgroundColor: '#f3f6f6',
+  },
+  header: {
+    paddingRight: '80px',
+    paddingLeft: '100px',
+    backgroundColor: '#fff',
+  },
+  content: {
+    overflow: 'auto',
+    height: '556px',
+    padding: '54px 60px 54px 80px',
+  },
+
+  transition1: {
+    transition: 'all 100ms cubic-bezier(.86, 0, .07, 1)',
+  },
+  headerTop: {
+    height: '106px',
+    paddingBottom: '20px',
+  },
+  headerTitleOverview: {
+    height: '54px',
+    paddingTop: '13px',
+    color: '#596d81',
+    fontSize: '27px',
+  },
+  headerMetaOverview: {
+    height: '54px',
+    paddingTop: '5px',
+    fontFamily: 'Inconsolata, monospace',
+    fontSize: '53px',
+  },
+  contentTitle: {
+    display: 'block',
+    height: '44px',
+    marginRight: 'auto',
+    marginBottom: '10px',
+    marginLeft: 'auto',
+    borderBottom: '1px solid transparent',
+    color: '#596d81',
+    fontSize: '27px',
+    transition: 'all 250ms cubic-bezier(.86, 0, .07, 1)',
+  },
+  contentNest: {
+    paddingTop: '1px',
   },
   indicatorPending: {
     display: 'inline-block',
@@ -51,108 +94,127 @@ const styles = {
     backgroundRepeat: 'no-repeat',
     color: '#2ed8a3',
   },
-
-  transactionIn: {
-    height: '52px',
-    paddingRight: '45px',
-    paddingLeft: '56px',
-    borderBottom: '1px solid #e7eaed',
-    backgroundColor: '#fff',
-    cursor: 'pointer',
-    backgroundImage: `url(${ArrowRightGray}),url(${PlusSmall})`,
-    backgroundPosition: '97% 50%, 20px 50%',
-    backgroundSize: '5px 10px, 16px 16px',
-    backgroundRepeat: 'no-repeat, no-repeat',
-    ':hover': {
-      backgroundColor: 'rgba(212, 240, 253, .5)',
-      backgroundImage: `url(${ArrowRightKeyBlue}),url(${PlusSmall})`,
-      backgroundSize: '5px, 16px',
-    },
-    transition: 'all 100ms cubic-bezier(0.86, 0, 0.07, 1) 0s'
+  headerTopTransactionDetails: {
+    height: '106px',
+    paddingTop: '43px',
+    textAlign: 'center',
+    marginRight: '-20px',
   },
-
-  transactionOut: {
-    height: '52px',
-    paddingRight: '45px',
-    paddingLeft: '56px',
-    borderBottom: '1px solid #e7eaed',
-    backgroundColor: '#fff',
-    cursor: 'pointer',
-    backgroundImage: `url(${ArrowRightGray}),url(${MinusSmall})`,
-    backgroundPosition: '97% 50%, 20px 50%',
-    backgroundSize: '5px 10px, 16px 16px',
-    backgroundRepeat: 'no-repeat, no-repeat',
-    ':hover': {
-      backgroundColor: 'rgba(212, 240, 253, .5)',
-      backgroundImage: `url(${ArrowRightKeyBlue}),url(${MinusSmall})`,
-      backgroundSize: '5px, 16px',
-    },
-    transition: 'all 100ms cubic-bezier(0.86, 0, 0.07, 1) 0s'
-  },
-
-  transactionAmount: {
-    width: '44%',
-    height: '39px',
-    paddingTop: '13px',
-    float: 'left',
-  },
-
-  transactionAccount: {
-    width: '34%',
-    height: '100%',
-    float: 'left',
-  },
-
-  transactionTimeDate: {
-    width: '22%',
-    height: '36px',
-    paddingTop: '16px',
-    float: 'left',
-    color: '#0c1e3e',
+  viewButtonLightSlateGray: {
+    display: 'inline-block',
+    padding: '17px 18px 18px',
+    float: 'right',
+    borderRadius: '5px',
+    backgroundColor: '#8997a5',
+    boxShadow: '0 0 10px 0 rgba(0, 0, 0, .2)',
+    color: '#fff',
     fontSize: '13px',
-    textAlign: 'right',
+    lineHeight: '9px',
+    fontWeight: '600',
+    textAlign: 'center',
+    textDecoration: 'none',
+    textTransform: 'capitalize',
   },
 
-  transactionAccountName: {
-    width: '45%',
-    height: '36px',
-    paddingTop: '17px',
-    paddingRight: '0px',
-    float: 'left',
-    fontSize: '11px',
-    textAlign: 'right',
-  },
-
-  transactionAccountIndicator: {
-    width: '50%',
-    height: '35px',
-    paddingTop: '17px',
-    paddingLeft: '10px',
-    float: 'left',
-  },
-
-  transactionAmountNumber: {
+  headerMetaTransactionDetailsIn: {
+    paddingTop: '16px',
+    paddingLeft: '50px',
+    backgroundImage: `url(${PlusBig})`,
+    backgroundPosition: '0px 50%',
+    backgroundSize: '30px',
+    backgroundRepeat: 'no-repeat',
     fontFamily: 'Inconsolata, monospace',
-    fontSize: '19px',
-    lineHeight: '13px',
+    fontSize: '35px',
     fontWeight: '700',
   },
 
-  transactionAmountHash: {
-    fontFamily: 'Inconsolata, monospace',
-    color: '#c4cbd2',
-    fontSize: '11px',
-    lineHeight: '11px',
-  },
-
-  transactionAmountNumberCurrency: {
-    fontSize: '13px',
+  headerMetaTransactionDetailsCurrency: {
+    fontSize: '19px',
     fontWeight: '400',
   },
 
-  transactionAmountNumberNumberFormatSmall: {
+  headerMetaTransactionDetailsTimeAndDate: {
+    display: 'inline-block',
+    paddingTop: '4px',
+    float: 'right',
+    fontFamily: 'Source Sans Pro, sans-serif',
+    fontSize: '19px',
+    fontWeight: '400',
+  },
+
+  headerMetaTransactionDetailsOut: {
+    paddingTop: '16px',
+    paddingLeft: '50px',
+    backgroundImage: `url(${MinusBig})`,
+    backgroundPosition: '0px 50%',
+    backgroundSize: '30px',
+    backgroundRepeat: 'no-repeat',
+    fontFamily: 'Inconsolata, monospace',
+    fontSize: '35px',
+    fontWeight: '700',
+  },
+
+  headerTopTransactionDetails: {
+    marginRight: '-20px',
+  },
+
+  transactionDetails: {
+    paddingTop: '18px',
+    paddingBottom: '8px',
+    borderBottom: '1px solid #e7eaed',
     fontSize: '13px',
   },
+
+  transactionDetailsLeft: {
+    width: '150px',
+    float: 'left',
+  },
+
+  transactionDetailsRight: {
+    width: '570px',
+    float: 'right',
+  },
+
+  transactionDetailsName: {
+    display: 'block',
+    width: '160px',
+    height: '28px',
+    paddingTop: '4px',
+    paddingRight: '20px',
+    float: 'left',
+    clear: 'left',
+    fontSize: '13px',
+    lineHeight: '19px',
+    textAlign: 'right',
+  },
+
+  transactionDetailsValue: {
+    display: 'inline-block',
+    height: '28px',
+    paddingTop: '9px',
+    float: 'left',
+    fontFamily: 'Inconsolata, monospace',
+    lineHeight: '10px',
+    fontWeight: '700',
+    textAlign: 'left',
+  },
+
+  transactionDetailsValueText: {
+    fontWeight: '400',
+  },
+
+  transactionDetailsTitle: {
+    width: '140px',
+    height: '28px',
+    paddingTop: '4px',
+    color: '#596d81',
+    fontSize: '20px',
+    textAlign: 'right',
+  },
+
+  transactionDetailsLast: {
+    borderBottom: '0px none transparent',
+  }
 };
 
 class TxDetails extends Component {
@@ -164,13 +226,65 @@ class TxDetails extends Component {
     const { tx } = this.props;
     const { clearTxDetails } = this.props;
     return(
-      <div>
-        {Buffer.from(tx.getHash()).toString('hex')}
-        <div onClick={() => clearTxDetails()}>
-          back
+      <div style={styles.view}>
+        <div style={styles.header}>
+          <div style={styles.headerTopTransactionDetails}>
+            <a style={styles.viewButtonLightSlateGray} onClick={() => clearTxDetails()}>back</a>
+          </div>
+          <div style={styles.headerTitleOverview}>Primary account</div>
+          <div style={styles.headerMetaTransactionDetailsIn}>
+            <Balance amount={1343240000} />
+            <div style={styles.headerMetaTransactionDetailsTimeAndDate}>20 Jan 2017 14:51</div>
+          </div>
+        </div>
+        <div style={styles.content}>
+          <div style={styles.contentNest}>
+            <div style={styles.transactionDetails}>
+              <div style={styles.transactionDetailsName}>Transaction:</div>
+              <div style={styles.transactionDetailsValue}>68c733bd42e80db76791429558c0b00c2e1ce8d89a2074d977638a252b8b4c1a</div>
+              <div style={styles.transactionDetailsName}>
+                <div style={styles.indicatorConfirmed}>confirmed</div>
+              </div>
+              <div style={styles.transactionDetailsValue}>11 <span style={styles.transactionDetailsValueText}>confirmations</span>
+              </div>
+              <div style={styles.transactionDetailsName}>To address:</div>
+              <div style={styles.transactionDetailsValue}>Tsbg8igLhyeCTUx4WJEcTk8318AJfqYWf5g</div>
+              <div style={styles.transactionDetailsName}>Transaction fee:</div>
+              <div style={styles.transactionDetailsValue}>0.0001 <span style={styles.transactionDetailsValueText}>DCR</span>
+              </div>
+            </div>
+            <div style={styles.transactionDetails}>
+              <div style={styles.transactionDetailsTitle}>Properties</div>
+              <div style={styles.transactionDetailsName}>Block:</div>
+              <div style={styles.transactionDetailsValue}>000000000000be05fdd578ba392dc367640a243756ba4ecbf10cafc423b49331a7</div>
+              <div style={styles.transactionDetailsName}>Height:</div>
+              <div style={styles.transactionDetailsValue}>10.504</div>
+            </div>
+            <div style={styles.transactionDetails}>
+              <div style={styles.transactionDetailsTitle}>Inputs</div>
+              <div style={styles.transactionDetailsName}>College funds:</div>
+              <div style={styles.transactionDetailsValue}>0.0001 <span style={styles.transactionDetailsValueText}>DCR</span>
+              </div>
+            </div>
+            <div style={styles.transactionDetailsLast}>
+              <div style={styles.transactionDetailsTitle}>Outputs</div>
+              <div style={styles.transactionDetailsName}>Tsbg8igLh… :</div>
+              <div style={styles.transactionDetailsValue}>5.00&nbsp;<span style={styles.transactionDetailsValueText}>DCR</span>
+              </div>
+              <div style={styles.transactionDetailsName}>Change:</div>
+              <div style={styles.transactionDetailsValue}>4.9999 <span style={styles.transactionDetailsValueText}>DCR</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>);
   }
 }
 
 export default TxDetails;
+/*
+        {Buffer.from(tx.getHash()).toString('hex')}
+        <div onClick={() => clearTxDetails()}>
+          back
+        </div>
+*/
