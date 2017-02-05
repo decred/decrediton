be4913cf8dd84a8a6023b54afe074aef0d0c29cb
Alex Yocom-Piatt <ay-p@decred.org>
2017-02-05 19:41:42 +0000

    Clean out old stuff.

--- app/components/GetStarted.js
+++ app/components/GetStarted.js
@@ -10,12 +10,6 @@ import ShowError from './ShowError';
 import Radium from 'radium';
 import SideBar from './SideBar';
 
-import {
-  Step,
-  Stepper,
-  StepLabel,
-  StepContent
-} from 'material-ui/Stepper';
 import FlatButton from 'material-ui/FlatButton';
 
 const styles = {
@@ -37,12 +31,12 @@ const styles = {
     width: '880px',
     height: '100%',
     float: 'right',
-    backgroundColor: '#f3f6f6',
+    backgroundColor: '#0c1e3e',
   },
   header: {
     paddingRight: '80px',
     paddingLeft: '100px',
-    backgroundColor: '#fff',
+    backgroundColor: '#596d81',
   },
   content: {
     overflow: 'auto',
@@ -60,14 +54,15 @@ const styles = {
   headerTitleOverview: {
     height: '54px',
     paddingTop: '13px',
-    color: '#596d81',
+    color: '#fff',
     fontSize: '27px',
   },
   headerMetaOverview: {
+    clear: 'left',
     height: '54px',
-    paddingTop: '5px',
-    fontFamily: 'Inconsolata, monospace',
-    fontSize: '53px',
+    width: '50%',
+    fontSize: '13px',
+    color: '#c4cbd2',
   },
   contentTitle: {
     display: 'block',
@@ -168,169 +163,12 @@ class Home extends Component{
         <ShowError error={getLoaderError} />
         <ShowError error={getVersionServiceError}/>
       </div>);
-    var openingWallet;
-    if (walletOpenRequestAttempt) {
-      openingWallet = (
-        <div>
-          <CircularProgress size={80} thickness={6}/>
-        </div>
-      );
-    } else {
-      openingWallet = (
-        <div>
-          <ShowError error={walletOpenError} />
-          <h3>Opening wallet</h3>
-          <h5>Please enter the information below to connect to your dcrwallet</h5>
-          <WalletOpenForm/>
-      </div>);
-    }
-    const getStartedWalletOpen = (openingWallet);
-
-
-    var creatingWallet;
-    if (walletCreateRequestAttempt) {
-      creatingWallet = (
-        <div>
-          <CircularProgress size={80} thickness={6}/>
-        </div>
-      );
-    } else {
-      creatingWallet = (
-        <div>
-          <ShowError error={walletCreateError} />
-          <h3>Create wallet</h3>
-          <h5>Please enter the information below to create your dcrwallet</h5>
-          <CreateWalletForm/>
-        </div>
-      );
-    }
-    const getStartedWalletCreate = (creatingWallet);
-
-    var startRpc;
-    if (startRpcRequestAttempt) {
-      startRpc = (
-        <div>
-          <CircularProgress size={80} thickness={6}/>
-        </div>
-      );
-    } else {
-      startRpc = (
-      <div>
-        <ShowError error={startRpcError} />
-      </div>
-      );
-    }
-
-    var discoveringAddresses;
-    if (discoverAddressRequestAttempt) {
-      discoveringAddresses = (
-        <div>
-          <CircularProgress size={80} thickness={6}/>
-        </div>
-      );
-    } else {
-      discoveringAddresses = (
-      <div>
-        <ShowError error={discoverAddressError} />
-        <h3>Discover used addresses</h3>
-        <h5>Please enter your private password below to discover accounts:</h5>
-        <DiscoverAddressForm />
-      </div>);
-    }
-    const getStartedDiscoverAddress = (discoveringAddresses);
 
     var ibdBlockProgress = 0.00;
     if (fetchHeadersResponse !== null) {
       ibdBlockProgress = (fetchHeadersResponse.getMainChainTipBlockHeight() / neededBlocks) * 100;
       ibdBlockProgress = ibdBlockProgress.toFixed(2);
     }
-    var fetchingHeaders;
-    if (fetchHeadersRequestAttempt) {
-      fetchingHeaders = (
-        <div>
-          <LinearProgress mode="determinate"
-            min={0}
-            max={neededBlocks}
-            value={fetchHeadersResponse !== null ? fetchHeadersResponse.getMainChainTipBlockHeight() : 0.0} />
-          <p>{ibdBlockProgress}%</p>
-        </div>
-      );
-    } else {
-      fetchingHeaders = (
-      <div>
-      </div>);
-    }
-    const fetchHeaders = (fetchingHeaders);
-      /*
-    const getStartedGettingLoader = (
-      <div >
-        <h3>Getting wallet loader service</h3>
-        <LinearProgress mode="indeterminate" />
-      </div>);
-
-    const getStartedWalletExistRequest = (
-      <div >
-        <h3>Checking if wallet exists</h3>
-        <LinearProgress mode="indeterminate" />
-      </div>);
-
-    const getStartedWalletExist = (
-      <div>
-        <p>{error}</p>
-        <h3>Check if wallet exists</h3>
-      </div>);
-
-
-
-    const getStartedOpeningWallet = (
-      <div>
-        <h3>Opening wallet</h3>
-        <LinearProgress mode="indeterminate" />
-      </div>);
-
-    // Step 4 complete
-    if (isLoggedIn) {
-      if (client === undefined) {
-        return(getStarted);
-      } else {
-        return(homeView);
-      }
-    }
-    // Step 4 action
-    if (isLoggingIn) {
-      return (getStartedLoggingIn);
-    }
-    // Step 3 complete/ Step 4 start
-    if (walletOpenResponse !== null) {
-      return(getStarted);
-    }
-    // Step 3 action
-    if (walletOpenRequestAttempt) {
-      return (getStartedOpeningWallet);
-    }
-    // Step 2 complete/ Step 3 start
-    if (walletExistResponse !== null && walletExistResponse.exists) {
-      return(getStartedWalletOpen);
-    }
-    // Step 2b creating wallet
-    if (walletCreateRequestAttempt) {
-      return(getStartedWalletCreating);
-    }
-    // Step 2 wallet exist action complete, though
-    // wallet does not exist
-
-    if (walletExistResponse !== null && !walletExistResponse.exists) {
-      return(getStartedCreateWallet);
-    }
-    // Step 2 action
-    if (walletExistRequestAttempt) {
-      return(getStartedWalletExistRequest);
-    }
-    // Step 1 complete/ Step 2 start
-    if (loader !== null) {
-      return(getStartedWalletExist);
-    }
-    */
 
     var startupStepView = (<div>something went wrong</div>);
 
@@ -363,8 +201,9 @@ class Home extends Component{
       <div style={styles.view}>
         <div style={styles.header}>
           <div style={styles.headerTop}></div>
-          <div style={styles.headerTitleOverview}>Open wallet</div>
+          <div style={styles.headerTitleOverview}>Opening Wallet</div>
           <div style={styles.headerMetaOverview}>
+            Please enter the information below to  create your dcrwallet
           </div>
         </div>
         <div style={styles.content}>
@@ -372,7 +211,10 @@ class Home extends Component{
             <div style={styles.contentTitleText}></div>
           </div>
           <div style={styles.contentNest}>
-            {getStartedWalletOpen}
+            { walletOpenRequestAttempt ? 
+              <CircularProgress size={80} thickness={6}/> :
+              <WalletOpenForm />
+            }
           </div>
         </div>
       </div>
@@ -392,7 +234,9 @@ class Home extends Component{
             <div style={styles.contentTitleText}>Text stuff</div>
           </div>
           <div style={styles.contentNest}>
-            {getStartedWalletCreate}
+            {walletCreateRequestAttempt ? 
+              <CircularProgress size={80} thickness={6}/> :
+              <CreateWalletForm/>}
           </div>
         </div>
       </div>
@@ -412,7 +256,10 @@ class Home extends Component{
               <div style={styles.contentTitleText}>Start RPC, Subscribe Block</div>
             </div>
             <div style={styles.contentNest}>
-              {startRpc}
+              { startRpcRequestAttempt ? 
+                <CircularProgress size={80} thickness={6}/> :
+                <div>Some unexpected error occured, please check logs</div>
+              }
             </div>
           </div>
         </div>
@@ -423,8 +270,9 @@ class Home extends Component{
         <div style={styles.view}>
           <div style={styles.header}>
             <div style={styles.headerTop}></div>
-            <div style={styles.headerTitleOverview}>Private passphrase</div>
+            <div style={styles.headerTitleOverview}>Opening Wallet</div>
             <div style={styles.headerMetaOverview}>
+              Please enter the information below to  create your dcrwallet
             </div>
           </div>
           <div style={styles.content}>
@@ -432,7 +280,10 @@ class Home extends Component{
               <div style={styles.contentTitleText}>For discover addresses</div>
             </div>
             <div style={styles.contentNest}>
-              {getStartedDiscoverAddress}
+              { discoverAddressRequestAttempt ? 
+                <CircularProgress size={80} thickness={6}/> : 
+                <DiscoverAddressForm />
+              }
             </div>
           </div>
         </div>
@@ -452,7 +303,16 @@ class Home extends Component{
               <div style={styles.contentTitleText}>Catching up block chain</div>
             </div>
             <div style={styles.contentNest}>
-              {fetchHeaders}
+              { fetchHeadersRequestAttempt ? 
+                <div>
+                  <LinearProgress mode="determinate"
+                    min={0}
+                    max={neededBlocks}
+                    value={fetchHeadersResponse !== null ? fetchHeadersResponse.getMainChainTipBlockHeight() : 0.0} />
+                  <p>{ibdBlockProgress}%</p>
+                </div> :
+                <div></div>
+              }
             </div>
           </div>
         </div>
