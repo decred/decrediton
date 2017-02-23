05cdb939c7865a8f7c0e0b576426ffbebb1f0471
Alex Yocom-Piatt <ay-p@decred.org>
2017-02-23 18:25:08 +0000

    Add first stubbing for stakepool api page

---
+++ app/components/views/StakePool.js
@@ -0,0 +1,92 @@
+// @flow
+import React, { Component, PropTypes } from 'react';
+import ErrorScreen from '../ErrorScreen';
+import Button from '../ButtonTanel';
+import SideBar from '../SideBar';
+import Header from '../Header';
+import qr from 'qr-image';
+
+const styles = {
+  body: {
+    position: 'fixed',
+    left: '0px',
+    top: '50%',
+    right: '0px',
+    display: 'block',
+    overflow: 'hidden',
+    width: '1178px',
+    height: '770px',
+    marginTop: '-385px',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    backgroundColor: '#FFF',
+  },
+  view: {
+    width: '880px',
+    height: '100%',
+    float: 'right',
+    backgroundColor: '#f3f6f6',
+  },
+  content: {
+    overflow: 'auto',
+    height: '556px',
+    padding: '54px 60px 54px 80px',
+  },
+  img: {
+    width: '250px',
+    paddingLeft: '244px',
+  },
+  center: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+};
+
+class StakePool extends Component{
+  static propTypes = {
+    walletService: PropTypes.object,
+    getNextAddressResponse: PropTypes.object,
+    getNextAddressRequestAttempt: PropTypes.bool.isRequired,
+  };
+
+  render() {
+    const { walletService } = this.props;
+    const { getNextAddressResponse, getNextAddressRequestAttempt } = this.props;
+
+    const copayReceive = (
+      <div style={styles.view}>
+        <Header
+          headerTitleOverview="Stake pool settings"
+          headerMetaOverview={
+            getNextAddressResponse !== null ?
+              <div style={{fontSize:'33px'}}>{getNextAddressResponse.getAddress()}</div> :
+              <div></div>
+          }
+        />
+        <div style={styles.content}>
+          <div style={styles.center}>
+            <Button
+              size="large"
+              block={false}
+              onClick={!getNextAddressRequestAttempt? () => this.props.getNextAddressAttempt(0) : null}
+              >
+              Generate new address
+            </Button>
+          </div>
+        </div>
+			</div>
+    );
+    if (walletService === null) {
+      return (<ErrorScreen />);
+    } else {
+      return(
+        <div style={styles.body}>
+          <SideBar />
+          {copayReceive}
+        </div>);
+    }
+  }
+}
+
+export default StakePool;
---
+++ app/components/views/StakePoolPage.js
@@ -0,0 +1,18 @@
+// @flow
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import StakePool from './StakePool';
+import * as ClientActions from '../../actions/ClientActions';
+import * as ControlActions from '../../actions/ControlActions';
+
+function mapStateToProps(state) {
+  return {
+    walletService: state.grpc.walletService,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Object.assign({}, ClientActions, ControlActions), dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StakePool);
