9ca919276dc08303733b838f47188194e8ee07aa
Alex Yocom-Piatt <ay-p@decred.org>
2017-02-24 17:37:33 -0500

    More tries to get stakepool requests working

--- app/actions/StakePoolActions.js
+++ app/actions/StakePoolActions.js
@@ -1,4 +1,4 @@
-import { stakePoolInfo } from '../middleware/stakepoolapi';
+import { stakePoolInfo, apiClientRequest } from '../middleware/stakepoolapi';
 export const GETSTAKEPOOLINFO_ATTEMPT = 'GETSTAKEPOOLINFO_ATTEMPT';
 export const GETSTAKEPOOLINFO_FAILED = 'GETSTAKEPOOLINFO_FAILED';
 export const GETSTAKEPOOLINFO_SUCCESS = 'GETSTAKEPOOLINFO_SUCCESS';
@@ -18,7 +18,17 @@ function getStakePoolInfoSuccess(response) {
         usablePools.push(response.data[stakePoolNames[i]]);
       }
     }
-
+    apiClientRequest(
+      "https://teststakepool.decred.org/api/v1/", 
+      "getpurchaseinfo", 
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0ODc5NzM3ODQsImlzcyI6Imh0dHBzOi8vdGVzdHN0YWtlcG9vbC5kZWNyZWQub3JnIiwibG9nZ2VkSW5BcyI6MTR9.HdJuTqDJPbVWPPzetOfQ7jK7PgadPeXWZulqgzZN-4U",
+      function(response, err) {
+        if (err) {
+          dispatch(console.log(err));
+        } else {
+          dispatch(console.log(response));
+        }
+      })
     dispatch({ data: usablePools, type: GETSTAKEPOOLINFO_SUCCESS });
   };
 }
--- app/middleware/stakepoolapi.js
+++ app/middleware/stakepoolapi.js
@@ -1,12 +1,28 @@
 import axios from 'axios';
-
+import querystring from 'querystring';
 export function stakePoolInfo(cb) {
   axios.get('https://decred.org/api/?c=gsd')
   .then(function (response) {
     cb(response);
   })
   .catch(function (error) {
-    cb(nil, error);
+    cb(null, error);
   });
 
+}
+
+export function apiClientRequest(apiUrl, request, apiToken, cb) {
+  var config = {
+    headers: { 
+      "Authorization": "Bearer " + apiToken,
+    }
+  }
+  console.log(apiUrl+request);
+  axios.get(apiUrl+request)
+  .then(function(response) {
+    cb(response);
+  })
+  .catch(function(error) {
+    cb(null, error);
+  });
 }
