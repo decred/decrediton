eefbb8b00c61e0e28581f357bb7b1edeac2b1de5
Alex Yocom-Piatt <ay-p@decred.org>
2017-03-20 22:12:17 +0000

    Use full words

--- app/helpers/dateFormat.js
+++ app/helpers/dateFormat.js
@@ -1,20 +1,26 @@
 export function timeSince(date) {
-  var seconds = Math.floor(((new Date().getTime()/1000) - date)),
-    interval = Math.floor(seconds / 31536000);
+  var seconds = Math.floor((new Date() - date) / 1000);
 
-  if (interval > 1) return interval + 'y';
+  var interval = Math.floor(seconds / 31536000);
 
+  if (interval > 1) {
+    return interval + ' years';
+  }
   interval = Math.floor(seconds / 2592000);
-  if (interval > 1) return interval + 'm';
-
+  if (interval > 1) {
+    return interval + ' months';
+  }
   interval = Math.floor(seconds / 86400);
-  if (interval >= 1) return interval + 'd';
-
+  if (interval > 1) {
+    return interval + ' days';
+  }
   interval = Math.floor(seconds / 3600);
-  if (interval >= 1) return interval + 'h';
-
+  if (interval > 1) {
+    return interval + ' hours';
+  }
   interval = Math.floor(seconds / 60);
-  if (interval > 1) return interval + 'm ';
-
-  return Math.floor(seconds) + 's';
+  if (interval > 1) {
+    return interval + ' minutes';
+  }
+  return Math.floor(seconds) + ' seconds';
 }
