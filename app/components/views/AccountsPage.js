bb22ce3d78684ae88a6fd232af70e4eefa7a9065
Alex Yocom-Piatt <ay-p@decred.org>
2017-03-22 14:48:58 -0400

    Typo

--- app/components/views/AccountsPage.js
+++ app/components/views/AccountsPage.js
@@ -13,7 +13,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(Object.assign({}, ContraolActions, ClientActions), dispatch);
+  return bindActionCreators(Object.assign({}, ControlActions, ClientActions), dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Accounts);
