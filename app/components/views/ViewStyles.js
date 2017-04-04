395df7da9cfb5cae2e9602e7600d7767eb129a51
Alex Yocom-Piatt <ay-p@decred.org>
2017-04-04 20:29:49 +0000

    TxDetailsStyles

--- app/components/views/ViewStyles.js
+++ app/components/views/ViewStyles.js
@@ -1,4 +1,208 @@
 import Delete from '../icons/delete.svg';
+import IndicatorPending from '../icons/indicator-pending.svg';
+import IndicatorConfirmed from '../icons/indicator-confirmed.svg';
+import PlusBig from '../icons/plus-big.svg';
+import MinusBig from '../icons/minus-big.svg';
+
+export const TxDetailStyles = {
+  view: {
+    width: '880px',
+    height: '100%',
+    float: 'right',
+    backgroundColor: '#f3f6f6',
+  },
+  header: {
+    paddingRight: '80px',
+    paddingLeft: '100px',
+    backgroundColor: '#fff',
+  },
+  content: {
+    overflow: 'auto',
+    height: '556px',
+    padding: '54px 60px 54px 80px',
+  },
+
+  transition1: {
+    transition: 'all 100ms cubic-bezier(.86, 0, .07, 1)',
+  },
+  headerTop: {
+    height: '106px',
+    paddingBottom: '20px',
+  },
+  headerTitleOverview: {
+    height: '54px',
+    paddingTop: '13px',
+    color: '#596d81',
+    fontSize: '27px',
+  },
+  headerMetaOverview: {
+    height: '54px',
+    paddingTop: '5px',
+    fontFamily: 'Inconsolata, monospace',
+    fontSize: '53px',
+  },
+  contentTitle: {
+    display: 'block',
+    height: '44px',
+    marginRight: 'auto',
+    marginBottom: '10px',
+    marginLeft: 'auto',
+    borderBottom: '1px solid transparent',
+    color: '#596d81',
+    fontSize: '27px',
+    transition: 'all 250ms cubic-bezier(.86, 0, .07, 1)',
+  },
+  contentNest: {
+    paddingTop: '1px',
+  },
+  indicatorPending: {
+    display: 'inline-block',
+    padding: '5px 8px 5px 20px',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderRadius: '3px',
+    fontSize: '12px',
+    lineHeight: '8px',
+    textAlign: 'right',
+    textTransform: 'capitalize',
+    borderColor: '#2971ff',
+    backgroundImage: `url(${IndicatorPending})`,
+    backgroundPosition: '6px 50%',
+    backgroundSize: '10px',
+    backgroundRepeat: 'no-repeat',
+    color: '#2971ff',
+  },
+
+  indicatorConfirmed: {
+    display: 'inline-block',
+    padding: '5px 8px 5px 20px',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderRadius: '3px',
+    fontSize: '12px',
+    lineHeight: '8px',
+    textAlign: 'right',
+    textTransform: 'capitalize',
+    borderColor: '#2ed8a3',
+    backgroundImage: `url(${IndicatorConfirmed})`,
+    backgroundPosition: '6px 50%',
+    backgroundSize: '10px',
+    backgroundRepeat: 'no-repeat',
+    color: '#2ed8a3',
+  },
+  headerTopTransactionDetails: {
+    height: '106px',
+    paddingTop: '43px',
+    textAlign: 'center',
+    marginRight: '-20px',
+  },
+  headerMetaTransactionDetailsIn: {
+    height: '38px',
+    paddingTop: '16px',
+    paddingLeft: '50px',
+    backgroundImage: `url(${PlusBig})`,
+    backgroundPosition: '0px 50%',
+    backgroundSize: '30px',
+    backgroundRepeat: 'no-repeat',
+    fontFamily: 'Inconsolata, monospace',
+    fontSize: '35px',
+    fontWeight: '700',
+  },
+
+  headerMetaTransactionDetailsCurrency: {
+    fontSize: '19px',
+    fontWeight: '400',
+  },
+
+  headerMetaTransactionDetailsTimeAndDate: {
+    display: 'inline-block',
+    paddingTop: '4px',
+    float: 'right',
+    fontFamily: 'Source Sans Pro, sans-serif',
+    fontSize: '19px',
+    fontWeight: '400',
+  },
+
+  headerMetaTransactionDetailsOut: {
+    height: '38px',
+    paddingTop: '16px',
+    paddingLeft: '50px',
+    backgroundImage: `url(${MinusBig})`,
+    backgroundPosition: '0px 50%',
+    backgroundSize: '30px',
+    backgroundRepeat: 'no-repeat',
+    fontFamily: 'Inconsolata, monospace',
+    fontSize: '35px',
+    fontWeight: '700',
+  },
+
+  transactionDetails: {
+    paddingTop: '18px',
+    paddingBottom: '8px',
+    borderBottom: '1px solid #e7eaed',
+    fontSize: '13px',
+    height: '100px',
+  },
+
+  transactionDetailsTop: {
+    paddingTop: '18px',
+    paddingBottom: '8px',
+    borderBottom: '1px solid #e7eaed',
+    fontSize: '13px',
+    height: '140px',
+  },
+
+  transactionDetailsLeft: {
+    width: '150px',
+    float: 'left',
+  },
+
+  transactionDetailsRight: {
+    width: '570px',
+    float: 'right',
+  },
+
+  transactionDetailsName: {
+    display: 'block',
+    width: '160px',
+    height: '28px',
+    paddingTop: '4px',
+    paddingRight: '20px',
+    float: 'left',
+    clear: 'left',
+    fontSize: '13px',
+    lineHeight: '19px',
+    textAlign: 'right',
+  },
+
+  transactionDetailsValue: {
+    display: 'inline-block',
+    height: '28px',
+    paddingTop: '9px',
+    float: 'left',
+    fontFamily: 'Inconsolata, monospace',
+    lineHeight: '10px',
+    fontWeight: '700',
+    textAlign: 'left',
+  },
+
+  transactionDetailsValueText: {
+    fontWeight: '400',
+  },
+
+  transactionDetailsTitle: {
+    width: '140px',
+    height: '28px',
+    paddingTop: '4px',
+    color: '#596d81',
+    fontSize: '20px',
+    textAlign: 'right',
+  },
+
+  transactionDetailsLast: {
+    borderBottom: '0px none transparent',
+  }
+};
 
 export const GetStartedStyles = {
   body: {
