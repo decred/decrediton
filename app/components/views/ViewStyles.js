f0a41fa5b7389516a7555f67a250dc87d7cdbb12
Alex Yocom-Piatt <ay-p@decred.org>
2017-04-04 20:31:06 +0000

    HelpStyles

--- app/components/views/ViewStyles.js
+++ app/components/views/ViewStyles.js
@@ -4,6 +4,43 @@ import IndicatorConfirmed from '../icons/indicator-confirmed.svg';
 import PlusBig from '../icons/plus-big.svg';
 import MinusBig from '../icons/minus-big.svg';
 
+export const HelpStyles = {
+  body: {
+    position: 'fixed',
+    left: '0px',
+    top: '50%',
+    right: '0px',
+    display: 'block',
+    overflow: 'hidden',
+    width: '1178px',
+    height: '770px',
+    marginTop: '-385px',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    backgroundColor: '#FFF',
+  },
+  view: {
+    width: '880px',
+    height: '100%',
+    float: 'right',
+    backgroundColor: '#f3f6f6',
+  },
+  content: {
+    overflow: 'auto',
+    height: '556px',
+    padding: '54px 60px 54px 80px',
+  },
+  img: {
+    width: '250px',
+    paddingLeft: '244px',
+  },
+  center: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+};
+
 export const TxDetailStyles = {
   view: {
     width: '880px',
