ae93e5795145b871dce92f3640c49abdf8949ba2
Alex Yocom-Piatt <ay-p@decred.org>
2017-06-01 12:11:38 -0400

    Add grpc-tools to devDependencies and use those bins for regen.sh

--- app/middleware/regen.sh
+++ app/middleware/regen.sh
@@ -2,4 +2,4 @@
 
 # This script regenerates the static js bindings for the api.proto file
 # You will need protoc (3.2.0) and grpc-tools (which installs grpc_tools_node_protoc_plugin) installed for this to run properly
-protoc --js_out=import_style=commonjs,binary:./walletrpc --grpc_out=./walletrpc --plugin=protoc-gen-grpc=$(which grpc_tools_node_protoc_plugin) ./api.proto
+./../../node_modules/grpc-tools/bin/protoc --js_out=import_style=commonjs,binary:./walletrpc --grpc_out=./walletrpc --plugin=protoc-gen-grpc=./../../node_modules/grpc-tools/bin/grpc_node_plugin ./api.proto
