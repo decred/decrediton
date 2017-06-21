5352fa4108e05db2e3d00183ba51d4d77b943a7e
Alex Yocom-Piatt <ay-p@decred.org>
2017-06-21 17:13:13 +0000

    Correctly set the expiry for purchase tickets

--- app/actions/ControlActions.js
+++ app/actions/ControlActions.js
@@ -607,20 +607,20 @@ function purchaseTicketsSuccess(purchaseTicketsResponse) {
 
 export function purchaseTicketsAttempt(passphrase, accountNum, spendLimit, requiredConf,
 numTickets, expiry, ticketFee, txFee, stakepool) {
-  var request = new PurchaseTicketsRequest();
-  request.setPassphrase(new Uint8Array(Buffer.from(passphrase)));
-  request.setAccount(accountNum);
-  request.setSpendLimit(spendLimit);
-  request.setRequiredConfirmations(requiredConf);
-  request.setTicketAddress(stakepool.TicketAddress);
-  request.setNumTickets(numTickets);
-  request.setPoolAddress(stakepool.PoolAddress);
-  request.setPoolFees(stakepool.PoolFees);
-  request.setExpiry(expiry);
-  request.setTxFee(txFee*1e8);
-  request.setTicketFee(ticketFee*1e8);
-
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const {getAccountsResponse} = getState().grpc;
+    var request = new PurchaseTicketsRequest();
+    request.setPassphrase(new Uint8Array(Buffer.from(passphrase)));
+    request.setAccount(accountNum);
+    request.setSpendLimit(spendLimit);
+    request.setRequiredConfirmations(requiredConf);
+    request.setTicketAddress(stakepool.TicketAddress);
+    request.setNumTickets(numTickets);
+    request.setPoolAddress(stakepool.PoolAddress);
+    request.setPoolFees(stakepool.PoolFees);
+    request.setExpiry(getAccountsResponse.getCurrentBlockHeight() + expiry);
+    request.setTxFee(txFee*1e8);
+    request.setTicketFee(ticketFee*1e8);
     dispatch({
       request: request,
       type: PURCHASETICKETS_ATTEMPT });
