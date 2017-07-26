5917b9cab9c6805f80614a9a0bf0cee860509e3b
Alex Yocom-Piatt <ay-p@decred.org>
2017-07-26 17:35:07 -0400

    default place on tickets page

--- app/components/views/StakePool.js
+++ app/components/views/StakePool.js
@@ -1120,7 +1120,7 @@ class StakePool extends Component{
         <div style={StakePoolStyles.body}>
           <SideBar />
           {stakePool}
-          <ReactToolTip place="left" type="info" effect="solid"/>
+          <ReactToolTip type="info" effect="solid"/>
         </div>);
     }
   }
