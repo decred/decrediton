c27e86f44cd370a453f25e6a669fb554e3a08749
Sean Durkin <sndurkin@gmail.com>
2017-08-07 14:50:35 -0400

    Convert ChangePassphraseModal from inline styles to CSS.

--- app/components/ChangePassphraseModal/Modal.js
+++ app/components/ChangePassphraseModal/Modal.js
@@ -1,84 +1,9 @@
 // @flow
 import React from "react";
-import Radium from "radium";
 import KeyBlueButton from "../KeyBlueButton";
 import SlateGrayButton from "../SlateGrayButton";
+import "../../style/ChangePassphraseModal.less";
 
-const styles = {
-  autoBuyerPassphraseModal: {
-    position: "absolute",
-    zIndex: "10",
-    overflow: "auto",
-    height: "556px",
-    padding: "54px 160px 54px 180px",
-    width: "46%",
-    backgroundColor: "rgba(12,30,62, 0.5)"
-  },
-  autobuyerPassphraseModalSection: {
-    padding: "20px",
-    backgroundColor: "#fff",
-  },
-  autobuyerPassphraseModalSectionHeadingSubSection: {
-    minHeight: "0px",
-    paddingBottom: "10px",
-    textTransform: "none",
-    color: "#0c1e3e",
-    fontSize: "19px",
-    lineHeight: "18px",
-    position: "relative",
-    zIndex: "0",
-  },
-  autobuyerPassphraseModalSectionHeadingSubSectionNotice: {
-    color: "#0c1e3e",
-    fontSize: "13px",
-    lineHeight: "15px",
-    position: "relative",
-    zIndex: "0",
-  },
-  highlightTextNeonGreen: {
-    color: "#2ed8a3",
-  },
-  autobuyerPassphraseModalSubSectionPrivPassphrase: {
-    marginBottom: "3px",
-    paddingTop: "11px",
-    paddingRight: "10px",
-    position: "relative",
-    zIndex: "0",
-    minHeight: "50px",
-  },
-  bigPrefix: {
-    float: "left",
-    color: "#0c1e3e",
-    fontSize: "19px",
-    height: "100%",
-    width: "200px",
-    paddingTop: "5px",
-    paddingRight: "10px",
-    textAlign: "right",
-    textTransform: "capitalize",
-  },
-  inputPassphraseField: {
-    width: "260px",
-    padding: "7px 10px",
-    float: "left",
-    borderStyle: "solid",
-    borderWidth: "0px 0px 1px",
-    borderColor: "#000 #000 #a9b4bf",
-    color: "#0c1e3e",
-    fontSize: "13px",
-    fontWeight: "700",
-    letterSpacing: "2px",
-    margin: "0",
-  },
-  modalSubSectionSaveCancel: {
-    height: "44px",
-  },
-  inputError: {
-    textAlign: "right",
-    color: "red",
-    fontSize: "10px",
-  }
-};
 
 const Modal = ({
   hidden,
@@ -96,51 +21,52 @@ const Modal = ({
   submitPassphrase,
   cancelPassphrase
 }) => (
- <div hidden={hidden} style={styles.autoBuyerPassphraseModal}>
-    <div style={styles.autobuyerPassphraseModalSection}>
-      <div style={styles.autobuyerPassphraseModalSectionHeadingSubSection}>{heading}</div>
-      <div style={styles.autobuyerPassphraseModalSectionHeadingSubSectionNotice}>{description}</div>
-      <div style={styles.autobuyerPassphraseModalSubSectionPrivPassphrase}>
-        <div style={styles.bigPrefix}>Old Private Passphrase:</div>
+ <div hidden={hidden} className="change-passphrase-modal">
+    <div className="change-passphrase-modal-section">
+      <div className="change-passphrase-modal-section-heading-sub-section">{heading}</div>
+      <div className="change-passphrase-modal-section-heading-sub-section-notice">{description}</div>
+      <div className="change-passphrase-modal-field-ct">
+        <div className="change-passphrase-modal-label">Old Private Passphrase:</div>
         <input
-          id='oldPassphrase'
-          style={styles.inputPassphraseField}
-          type="password" placeholder=""
+          id="oldPassphrase"
+          className="change-passphrase-modal-field"
+          type="password"
+          placeholder=""
           defaultValue={oldPrivPass}
           onChange={(e) => updateOldPrivatePassphrase(e.target.value)}
         />
-        <div style={styles.inputError}>{oldPrivPassError}</div>
+        <div className="change-passphrase-modal-error">{oldPrivPassError}</div>
       </div>
-      <div style={styles.autobuyerPassphraseModalSubSectionPrivPassphrase}>
-        <div style={styles.bigPrefix}>New Private Passphrase:</div>
+      <div className="change-passphrase-modal-field-ct">
+        <div className="change-passphrase-modal-label">New Private Passphrase:</div>
         <input
-          id='passphrase'
-          style={styles.inputPassphraseField}
+          id="passphrase"
+          className="change-passphrase-modal-field"
           type="password"
           placeholder=""
           defaultValue={privpass}
           onChange={(e) => updatePrivatePassphrase(e.target.value)}
         />
-        <div style={styles.inputError}>{privPassError}</div>
+        <div className="change-passphrase-modal-error">{privPassError}</div>
       </div>
-      <div style={styles.autobuyerPassphraseModalSubSectionPrivPassphrase}>
-        <div style={styles.bigPrefix}>Confirm:</div>
+      <div className="change-passphrase-modal-field-ct">
+        <div className="change-passphrase-modal-label">Confirm:</div>
         <input
           id='confirmPassphrase'
-          style={styles.inputPassphraseField}
+          className="change-passphrase-modal-field"
           type="password"
           placeholder=""
           defaultValue={confirmPrivPass}
           onChange={(e) => updateConfirmPrivatePassphrase(e.target.value)}
         />
-        <div style={styles.inputError}>{confirmPrivPassError}</div>
+        <div className="change-passphrase-modal-error">{confirmPrivPassError}</div>
       </div>
-      <div style={styles.modalSubSectionSaveCancel}>
-        <KeyBlueButton style={{float: "left"}} onClick={submitPassphrase}>Update</KeyBlueButton>
-        <SlateGrayButton style={{float: "right"}} onClick={cancelPassphrase}>cancel</SlateGrayButton>
+      <div className="change-passphrase-modal-toolbar">
+        <KeyBlueButton className="change-passphrase-modal-toolbar-save-button" onClick={submitPassphrase}>Update</KeyBlueButton>
+        <SlateGrayButton className="change-passphrase-modal-toolbar-cancel-button" onClick={cancelPassphrase}>cancel</SlateGrayButton>
       </div>
     </div>
   </div>
 );
 
-export default Radium(Modal);
+export default Modal;
