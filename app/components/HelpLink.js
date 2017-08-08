cad699e3af7fee868ef059cb9ecfd2ff088045be
Sean Durkin <sndurkin@gmail.com>
2017-08-08 19:40:46 -0400

    Convert HelpLink from inline styles to CSS and fix bug with text color on hover.

--- app/components/HelpLink.js
+++ app/components/HelpLink.js
@@ -1,46 +1,16 @@
 // @flow
-import Radium from "radium";
 import React from "react";
 import { shell } from "electron";
-import leftLinkColor from "./icons/menu-link-left-color.png";
-
-var styles = {
-  linkColor: {
-    cursor: "pointer",
-    display: "block",
-    width: "50%",
-    height: "53px",
-    backgroundImage: `url(${leftLinkColor})`,
-    backgroundPosition: "0px 50%",
-    backgroundSize: "0px",
-    backgroundRepeat: "repeat-y",
-    ":hover": {
-      backgroundColor: "#616262",
-      backgroundSize: "5px",
-    },
-  },
-  helpLink:{
-    display: "block",
-    height: "38px",
-    paddingTop: "15px",
-    paddingLeft: "40px",
-    color: "black",
-    fontSize: "18px",
-    textAlign: "left",
-    textDecoration: "none",
-    textTransform: "capitalize",
-  },
-};
+import "../style/MiscComponents.less";
 
 class HelpLink extends React.Component {
   render() {
     return (
-      <div style={styles.linkColor}>
-        <a style={styles.helpLink} onClick={function(x){shell.openExternal(x);}.bind(null, this.props.href)}>
-          {this.props.children}
-        </a>
-      </div>
+      <a className="help-link" onClick={function(x){shell.openExternal(x);}.bind(null, this.props.href)}>
+        {this.props.children}
+      </a>
     );
   }
 }
-module.exports = Radium(HelpLink);
+
+export default HelpLink;
