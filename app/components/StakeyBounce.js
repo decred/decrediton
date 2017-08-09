ba6b4ae6ae539f97d9797efb455969e223b15b03
Alex Yocom-Piatt <ay-p@decred.org>
2017-08-09 10:49:21 -0500

    Add stakey bounce loading for stakepool page (#565)

---
+++ app/components/StakeyBounce.js
@@ -0,0 +1,17 @@
+// @flow
+import React from "react";
+import "../style/Stakey.less";
+
+class StakeyBounce extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="stakey"/>
+    );
+  }
+}
+
+export default StakeyBounce;
