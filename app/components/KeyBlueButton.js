5159a1c95585842bd129ca4af06d684691ea83ca
Sean Durkin <sndurkin@gmail.com>
2017-08-09 09:24:54 -0400

    Convert KeyBlueButton from inline styles to CSS.

--- app/components/KeyBlueButton.js
+++ app/components/KeyBlueButton.js
@@ -1,71 +1,33 @@
 // @flow
-import Radium from "radium";
 import React from "react";
-
-var styles = {
-  base: {
-    cursor: "pointer",
-    display: "inline-block",
-    padding: "17px 18px 18px",
-    borderRadius: "5px",
-    backgroundColor: "#2971ff",
-    boxShadow: "0 0 10px 0 rgba(0, 0, 0, .2)",
-    color: "#fff",
-    fontSize: "13px",
-    lineHeight: "9px",
-    fontWeight: "600",
-    textAlign: "center",
-    textDecoration: "none",
-    textTransform: "capitalize",
-    transitionProperty: "all",
-    transitionDuration: "100ms",
-    transitionTimingFunction: "cubic-bezier(0.86, 0, 0.07, 1)",
-    transitionDelay: "0s",
-    ":hover": {
-      backgroundColor: "#1b58ff",
-    },
-    ":active": {
-      boxShadow: "0 0 0 0 rgba(0, 0, 0, .2)",
-    }
-  },
-  baseDisabled: {
-    cursor: "default",
-    display: "inline-block",
-    padding: "17px 18px 18px",
-    borderRadius: "5px",
-    backgroundColor: "grey",
-    boxShadow: "0 0 10px 0 rgba(0, 0, 0, .2)",
-    color: "#fff",
-    fontSize: "13px",
-    lineHeight: "9px",
-    fontWeight: "600",
-    textAlign: "center",
-    textDecoration: "none",
-    textTransform: "capitalize",
-  },
-  block: {
-    display: "block",
-  },
-};
+import "../style/MiscComponents.less";
 
 class KeyBlueButton extends React.Component {
   render() {
+    let className = !this.props.disabled ? "key-blue-button"
+                                         : "key-blue-button-disabled";
+    let style = {};
+    Object.assign(style, this.props.style);
+    if(!this.props.disabled && this.props.block) {
+      style.display = "block";
+    }
+
+    if(this.props.className) {
+      className += " " + this.props.className;
+    }
+
     return (
       <div
-        style={! this.props.disabled ? [
-          styles.base,
-          this.props.style,
-          this.props.block && styles.block
-        ] : [styles.baseDisabled,
-          this.props.style] }
-        className={this.props.className}
-        type={this.props.type}
-        disabled={this.props.disabled}
-        onClick={this.props.onClick}
-        hidden={this.props.hidden}>
+          className={className}
+          style={style}
+          type={this.props.type}
+          disabled={this.props.disabled}
+          onClick = {this.props.onClick}
+          hidden={this.props.hidden}>
         {this.props.children}
       </div>
     );
   }
 }
-module.exports = Radium(KeyBlueButton);
+
+export default KeyBlueButton;
