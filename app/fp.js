7c1ead987dc0e3a8b612cd6f97ab0b91631d40d6
go1dfish <justageekbiz@gmail.com>
2017-08-18 02:21:00 -0700

    Split HistoryPage into logic and presentation components

--- app/fp.js
+++ app/fp.js
@@ -1,4 +1,5 @@
 export { default as compose } from "lodash/fp/compose";
+export { default as reduce } from "lodash/fp/reduce";
 export { default as get } from "lodash/fp/get";
 export { default as eq } from "lodash/fp/eq";
 export { createSelector } from "reselect";
@@ -21,3 +22,8 @@ export const selectorMap = (fns) => createSelector(
   Object.keys(fns).map(key => fns[key]),
   (...args) => Object.keys(fns).reduce((res, key, idx) => ({...res, [key]: args[idx] }), {})
 );
+
+// Currently redux state is not immutable causing issues with real selectors
+// This is a temporary hack to allow same code style until that is fixed.
+export const createSelectorEager = (keyFns, resultFn) =>
+  (...args) => resultFn(...keyFns.map(fn => fn(...args)));
