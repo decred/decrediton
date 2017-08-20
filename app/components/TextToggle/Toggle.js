883f3390779f1b5e9ce303d3bba022ef16296f75
Sean Durkin <sndurkin@gmail.com>
2017-08-20 00:36:28 -0400

    Convert TextToggle from inline styles to CSS.

--- app/components/TextToggle/Toggle.js
+++ app/components/TextToggle/Toggle.js
@@ -1,96 +1,6 @@
 // @flow
-import Radium from "radium";
 import React from "react";
-
-const styles = {
-  textToggle: {
-    display: "block",
-    height: "44px",
-    float: "right",
-    textAlign: "left",
-  },
-
-  textToggleButtonLeft: {
-    minWidth: "140px",
-    float: "left",
-    border: "1px #e7eaed solid",
-    borderRadius: "5px 0px 0px 5px",
-    backgroundColor: "#fff",
-    boxShadow: "none",
-    color: "#8997a5",
-    display: "inline-block",
-    padding: "12px 18px",
-    fontSize: "13px",
-    fontWeight: "600",
-    textAlign: "center",
-    textDecoration: "none",
-    textTransform: "capitalize",
-    cursor: "pointer",
-  },
-
-  textToggleButtonRight: {
-    minWidth: "140px",
-    float: "left",
-    border: "1px #e7eaed solid",
-    borderRadius: "0px 5px 5px 0px",
-    backgroundColor: "#fff",
-    boxShadow: "none",
-    color: "#8997a5",
-    display: "inline-block",
-    padding: "12px 18px",
-    fontSize: "13px",
-    fontWeight: "600",
-    textAlign: "center",
-    textDecoration: "none",
-    textTransform: "capitalize",
-    cursor: "pointer",
-  },
-
-  textToggleButtonActiveLeft: {
-    backgroundColor: "#f3f6f6",
-    color: "#0c1e3e",
-    cursor: "default",
-    minWidth: "140px",
-    float: "left",
-    border: "1px solid #f3f6f6",
-    borderRadius: "5px 0px 0px 5px",
-    boxShadow: "none",
-    display: "inline-block",
-    padding: "12px 18px",
-    fontSize: "13px",
-    fontWeight: "600",
-    textAlign: "center",
-    textDecoration: "none",
-    textTransform: "capitalize",
-  },
-  textToggleButtonActiveRight: {
-    backgroundColor: "#f3f6f6",
-    color: "#0c1e3e",
-    cursor: "default",
-    minWidth: "140px",
-    float: "left",
-    border: "1px solid #f3f6f6",
-    borderRadius: "0px 5px 5px 0px",
-    boxShadow: "none",
-    display: "inline-block",
-    padding: "12px 18px",
-    fontSize: "13px",
-    fontWeight: "600",
-    textAlign: "center",
-    textDecoration: "none",
-    textTransform: "capitalize",
-  },
-  textToggleButtonDescription: {
-    width: "100%",
-    height: "100%",
-    textTransform: "capitalize",
-    display: "block",
-    paddingTop: "12px",
-    paddingRight: "35px",
-    paddingLeft: "35px",
-      //textAlign: 'left',
-  }
-};
+import "../../style/TextToggle.less";
 
 const Toggle = ({
   leftText,
@@ -98,16 +8,16 @@ const Toggle = ({
   activeButton,
   onClick
 }) => (
-  <div style={styles.textToggle}>
+  <div className="text-toggle">
     <div
-      style={activeButton === "right" ? styles.textToggleButtonLeft : styles.textToggleButtonActiveLeft }
+      className={"text-toggle-button-left" + (activeButton === "left" ? " text-toggle-button-active" : "")}
       onClick={activeButton == "right" ? () => onClick("left") : null}
     >{leftText}</div>
     <div
-      style={activeButton == "left" ? styles.textToggleButtonRight : styles.textToggleButtonActiveRight }
+      className={"text-toggle-button-right" + (activeButton === "right" ? " text-toggle-button-active" : "")}
       onClick={activeButton == "left" ? () => onClick("right") : null}
     >{rightText}</div>
   </div>
 );
 
-export default Radium(Toggle);
+export default Toggle;
