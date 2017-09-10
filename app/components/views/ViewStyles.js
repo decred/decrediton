23f68f493d8d0060b1a5e62a3d49cd8b5f920025
Matheus Degiovani <math@matheusd.com>
2017-09-10 07:58:26 -0300

    Refactor Help Page from inline to css styles

--- app/components/views/ViewStyles.js
+++ app/components/views/ViewStyles.js
@@ -1342,42 +1342,6 @@ export const StakePoolStyles = {
     display: "block",
   }
 };
-export const HelpStyles = {
-  body: {
-    position: "fixed",
-    left: "0px",
-    top: "50%",
-    right: "0px",
-    display: "block",
-    overflow: "hidden",
-    width: "1178px",
-    height: "770px",
-    marginTop: "-385px",
-    marginRight: "auto",
-    marginLeft: "auto",
-    backgroundColor: "#FFF",
-  },
-  view: {
-    width: "880px",
-    height: "100%",
-    float: "right",
-    backgroundColor: "#f3f6f6",
-  },
-  content: {
-    overflow: "auto",
-    height: "556px",
-    padding: "54px 60px 54px 80px",
-  },
-  img: {
-    width: "250px",
-    paddingLeft: "244px",
-  },
-  center: {
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-  }
-};
 export const TxDetailsStyles = {
   view: {
     width: "880px",
