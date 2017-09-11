4e1b45c93ff100201264d8b7ae88afccb206143c
Matheus Degiovani <math@matheusd.com>
2017-09-11 13:24:51 -0300

    Refactor Header to move snackbar from inline to css styles

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -3,58 +3,16 @@ import React from "react";
 import Snackbar from "material-ui/Snackbar";
 import Notification from "./Notification";
 import "../../style/Header.less";
-import WalletGray from "../../style/icons/wallet-gray.svg";
-import TicketSmall from "../../style/icons/tickets-ticket.svg";
-import PlusBig from "../../style/icons/plus-big.svg";
-import MinusBig from "../../style/icons/minus-big.svg";
+import "../../style/Snackbar.less";
 
-const snackbarStyles = {
-  stake: {
-    height: "78px",
-    padding: "0px 50px",
-    backgroundColor: "rgba(12, 30, 62, 0.5)",
-    backgroundImage: `url(${TicketSmall})`,
-    backgroundPosition: "15px 50%",
-    backgroundSize: "20px",
-    backgroundRepeat: "no-repeat",
-  },
-  receive: {
-    height: "78px",
-    padding: "0px 50px",
-    backgroundColor: "rgba(12, 30, 62, 0.5)",
-    backgroundImage: `url(${PlusBig})`,
-    backgroundPosition: "15px 50%",
-    backgroundSize: "20px",
-    backgroundRepeat: "no-repeat",
-  },
-  send: {
-    height: "78px",
-    padding: "0px 50px",
-    backgroundColor: "rgba(12, 30, 62, 0.5)",
-    backgroundImage: `url(${MinusBig})`,
-    backgroundPosition: "15px 50%",
-    backgroundSize: "20px",
-    backgroundRepeat: "no-repeat",
-  },
-  transfer: {
-    height: "78px",
-    padding: "0px 50px",
-    backgroundColor: "rgba(12, 30, 62, 0.5)",
-    backgroundImage: `url(${WalletGray})`,
-    backgroundPosition: "15px 50%",
-    backgroundSize: "20px",
-    backgroundRepeat: "no-repeat",
-  }
-};
-
-const snackbarBodyStyle = ({ type }) => ({
-  "Ticket": snackbarStyles.stake,
-  "Vote": snackbarStyles.stake,
-  "Revoke": snackbarStyles.stake,
-  "Receive": snackbarStyles.receive,
-  "Send": snackbarStyles.send,
-  "Transfer": snackbarStyles.transfer
-})[type] || {};
+const snackbarClasses = ({ type }) => ({
+  "Ticket": "snackbar snackbar-stake",
+  "Vote": "snackbar snackbar-stake",
+  "Revoke": "snackbar snackbar-stake",
+  "Receive": "snackbar snackbar-receive",
+  "Send": "snackbar snackbar-send",
+  "Transfer": "snackbar snackbar-transfer"
+})[type] || "snackbar ";
 
 const Header = ({
   headerTop,
@@ -66,11 +24,11 @@ const Header = ({
 }) => (
   <div>
     <Snackbar
-      className="snackbar"
+      className={snackbarClasses(message || "")}
       open={!!message}
       message={message ? <Notification {...message} /> : ""}
       autoHideDuration={4000}
-      bodyStyle={snackbarBodyStyle(message || {})}
+      bodyStyle={{backgroundColor: "inherited"}}
       onRequestClose={reason => reason !== "clickaway" ? onDismissMessage() : null}
     />
     <div className="header">
