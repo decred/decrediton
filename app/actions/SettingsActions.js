b498751b68a2bf76d6268f707b75ccf7c0717889
Alex Yocom-Piatt <ay-p@decred.org>
2017-09-18 14:41:12 -0500

    Move setting config update into saveSettings (#674)

--- app/actions/SettingsActions.js
+++ app/actions/SettingsActions.js
@@ -5,6 +5,9 @@ export const SETTINGS_CHANGED = "SETTINGS_CHANGED";
 export const SETTINGS_UNCHANGED = "SETTINGS_UNCHANGED";
 
 export function saveSettings(settings) {
+  var cfg = getCfg();
+  cfg.set("currency_display", settings.currencyDisplay);
+  cfg.set("network", settings.network);
   return {
     settings,
     type: SETTINGS_SAVE
@@ -12,15 +15,12 @@ export function saveSettings(settings) {
 }
 export function updateStateSettingsChanged(settings) {
   return (dispatch, getState) => {
-    var cfg = getCfg();
     const { tempSettings, currentSettings } = getState().settings;
     if ((settings.currencyDisplay !== tempSettings.currencyDisplay) ||
 	(settings.network !== tempSettings.network)){
       if (settings.currencyDisplay !== currentSettings.currencyDisplay) {
-        cfg.set("currency_display", settings.currencyDisplay);
         dispatch({ tempSettings: settings, type: SETTINGS_CHANGED});
       } else if (settings.network !== currentSettings.network) {
-        cfg.set("network", settings.network);
         dispatch({ tempSettings: settings, type: SETTINGS_CHANGED});
       } else {
         dispatch({ tempSettings: currentSettings, type: SETTINGS_UNCHANGED});
