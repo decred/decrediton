eebf4046661c6890524d4eeeee01cb38ca85ef3b
Matheus Degiovani <math@matheusd.com>
2017-09-19 11:12:39 -0300

    Restrict send amount to decimal number and accept comma

--- app/helpers/strings.js
+++ app/helpers/strings.js
@@ -6,3 +6,15 @@
 export function addSpacingAroundText(s) {
   return " " + s + " ";
 }
+
+// restrictToStdDecimalChars returns a new version of the string s, removing
+// all non-decimal chars and converting "," to "." and making sure the number
+// is a decimal-looking number (123.456)
+export function restrictToStdDecimalNumber(s) {
+  return s
+    .replace(/,/g, ".")                       // comma to period
+    .replace(/[^\d.]/g, "")                   // remove non-numbers
+    .replace(/\.[\.]+/g, ".")                 // remove repetitive periods
+    .replace(/^\.(.*)$/, "0.$1")              // prepend 0 when starting with period
+    .replace(/^([\d]*)(\.?[\d]*).*/, "$1$2"); // use only the first run of numbers
+}
