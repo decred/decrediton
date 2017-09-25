083711fdf85ea437b83749f288b6a55a70ca9434
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-09-25 15:50:32 +0100

    Convert singlequote to doublequote

--- test/unit/components/Balance.spec.js
+++ test/unit/components/Balance.spec.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Balance } from '../../../app/components/Balance';
-import { shallow } from 'enzyme';
-import sinon from 'sinon';
+import React from "react";
+import { Balance } from "../../../app/components/Balance";
+import { shallow } from "enzyme";
+import sinon from "sinon";
 
-test('atoms display', () => {
+test("atoms display", () => {
   const spy = sinon.spy();
   // Render a checkbox with label in the document
   const balance = shallow(
     <Balance currencyDisplay="atoms" amount={42} onClick={spy} />
   );
 
-  expect(balance.find('.balance-base').text()).toEqual('42 atoms');
+  expect(balance.find(".balance-base").text()).toEqual("42 atoms");
 
-  balance.find('.balance-base').simulate('click');
+  balance.find(".balance-base").simulate("click");
 
   expect(spy.calledOnce).toEqual(true);
 });
-test('DCR display', () => {
+test("DCR display", () => {
   const spy = sinon.spy();
   // Render a checkbox with label in the document
   const balance = shallow(
     <Balance currencyDisplay="DCR" amount={420000001} onClick={spy} />
   );
 
-  expect(balance.find('.balance-base').text()).toEqual('4.20000001 DCR');
+  expect(balance.find(".balance-base").text()).toEqual("4.20000001 DCR");
 
-  balance.find('.balance-base').simulate('click');
+  balance.find(".balance-base").simulate("click");
 
   expect(spy.calledOnce).toEqual(true);
 });
