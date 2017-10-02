b40cb427ccc794afc044833addf227f9868048db
go1dfish <me@go1dfish.me>
2017-10-02 16:31:14 -0400

    Refactor SettingsActions/VersionActions (#737)

--- app/actions/SettingsActions.js
+++ app/actions/SettingsActions.js
@@ -4,41 +4,38 @@ export const SETTINGS_SAVE = "SETTINGS_SAVE";
 export const SETTINGS_CHANGED = "SETTINGS_CHANGED";
 export const SETTINGS_UNCHANGED = "SETTINGS_UNCHANGED";
 
-export function saveSettings(settings) {
-  var cfg = getCfg();
+export const saveSettings = (settings) => {
+  const cfg = getCfg();
   cfg.set("currency_display", settings.currencyDisplay);
   cfg.set("network", settings.network);
-  return {
-    settings,
-    type: SETTINGS_SAVE
-  };
-}
-export function updateStateSettingsChanged(settings) {
-  return (dispatch, getState) => {
-    const { tempSettings, currentSettings } = getState().settings;
-    if ((settings.currencyDisplay !== tempSettings.currencyDisplay) ||
-	(settings.network !== tempSettings.network)){
-      if (settings.currencyDisplay !== currentSettings.currencyDisplay) {
-        dispatch({ tempSettings: settings, type: SETTINGS_CHANGED});
-      } else if (settings.network !== currentSettings.network) {
-        dispatch({ tempSettings: settings, type: SETTINGS_CHANGED});
-      } else {
-        dispatch({ tempSettings: currentSettings, type: SETTINGS_UNCHANGED});
-      }
-    }
-  };
-}
+  return { type: SETTINGS_SAVE, settings };
+};
 
-export function updateStateVoteSettingsChanged(settings) {
-  return (dispatch, getState) => {
-    var cfg = getCfg();
-    const { tempSettings, currentSettings } = getState().settings;
-    if (settings.enableTicketBuyer !== tempSettings.enableTicketBuyer) {
-      cfg.set("enableticketbuyer", settings.enableTicketBuyer);
+export const updateStateSettingsChanged = (settings) => (dispatch, getState) => {
+  const { settings: { tempSettings, currentSettings }} = getState();
+  if (
+    (settings.currencyDisplay !== tempSettings.currencyDisplay) ||
+    (settings.network !== tempSettings.network)
+  ){
+    if (settings.currencyDisplay !== currentSettings.currencyDisplay) {
       dispatch({ tempSettings: settings, type: SETTINGS_CHANGED});
-      writeCfg();
+    } else if (settings.network !== currentSettings.network) {
+      dispatch({ tempSettings: settings, type: SETTINGS_CHANGED});
     } else {
       dispatch({ tempSettings: currentSettings, type: SETTINGS_UNCHANGED});
     }
-  };
-}
+  }
+};
+
+export const updateStateVoteSettingsChanged = (settings) => (dispatch, getState) => {
+  const cfg = getCfg();
+  const { settings: { tempSettings, currentSettings }} = getState();
+
+  if (settings.enableTicketBuyer !== tempSettings.enableTicketBuyer) {
+    cfg.set("enableticketbuyer", settings.enableTicketBuyer);
+    dispatch({ tempSettings: settings, type: SETTINGS_CHANGED});
+    writeCfg();
+  } else {
+    dispatch({ tempSettings: currentSettings, type: SETTINGS_UNCHANGED});
+  }
+};
