d201d838237638e039a0dda3f64358ae86fee0b6
Matheus Degiovani <opensource@matheusd.com>
2017-10-19 13:50:25 -0200

    Fix StakePool cleared bug (#804)

--- app/actions/StakePoolActions.js
+++ app/actions/StakePoolActions.js
@@ -2,8 +2,9 @@
 import Promise from "promise";
 import { getPurchaseInfo, setStakePoolAddress, setVoteChoices } from "../wallet/stakePool";
 import { getNextAddress } from "../wallet/service";
-import { getCfg } from "../config.js";
+import { getCfg, updateStakePoolConfig } from "../config.js";
 import { importScriptAttempt } from "./ControlActions";
+import { getStakePoolInfo } from "wallet/config";
 import * as sel from "../selectors";
 
 export const UPDATESTAKEPOOLCONFIG_ATTEMPT = "UPDATESTAKEPOOLCONFIG_ATTEMPT";
@@ -144,3 +145,14 @@ export const clearStakePoolConfigError = () => (dispatch, getState) =>
 export const clearStakePoolConfigSuccess = () => (dispatch, getState) =>
   sel.currentStakePoolConfigSuccessMessage(getState())
     ? dispatch({ type: UPDATESTAKEPOOLCONFIG_CLEAR_SUCCESS }) : null;
+
+export const DISCOVERAVAILABLESTAKEPOOLS_SUCCESS = "DISCOVERAVAILABLESTAKEPOOLS_SUCCESS";
+export const discoverAvailableStakepools = () => (dispatch) =>
+  getStakePoolInfo()
+    .then((foundStakepoolConfigs) => {
+      if (foundStakepoolConfigs) {
+        let config = getCfg();
+        updateStakePoolConfig(config, foundStakepoolConfigs);
+        dispatch({ type: DISCOVERAVAILABLESTAKEPOOLS_SUCCESS, currentStakePoolConfig: config.get("stakepools")});
+      } // TODO: add error notification after global snackbar is merged
+    });
