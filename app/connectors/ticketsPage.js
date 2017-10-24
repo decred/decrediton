0a773160cfa99c83468327db7fadf0a3b7e85d54
Alex Yocom-Piatt <ay-p@decred.org>
2017-10-24 13:56:23 -0400

    lint-fix

--- app/connectors/ticketsPage.js
+++ app/connectors/ticketsPage.js
@@ -3,7 +3,6 @@ import { bindActionCreators } from "redux";
 import { selectorMap } from "../fp";
 import * as sel from "../selectors";
 import * as ca from "../actions/ControlActions";
-import * as spa from "../actions/StakePoolActions";
 
 const mapStateToProps = selectorMap({
   spendingAccounts: sel.spendingAccounts,
