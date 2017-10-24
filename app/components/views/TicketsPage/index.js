d2eacc67870858e9b3bd8513b49ad8f65b99837a
Alex Yocom-Piatt <ay-p@decred.org>
2017-10-24 15:58:40 -0400

    add prevRoute state tracking

--- app/components/views/TicketsPage/index.js
+++ app/components/views/TicketsPage/index.js
@@ -8,22 +8,42 @@ const opts = { stiffness: 150, damping: 20 };
 
 const wrapperComponent = props => <div className="tab-content" { ...props } />;
 
-const Tickets = ({ children, location }) => {
-  const tabs = ["purchase", "mytickets", "governance", "statistics"];
-  const page = "tickets";
-  // this will be removed w/ react router 4
-  const pathname = location.pathname.split("/")[2];
-  // Still unable to determine 'from' tab which would determine which transition to use for middle tabs
-  const effect = pathname === tabs[0] ? enterLeft : pathname === tabs[tabs.length - 1] ? enterRight : enterLeft;
-  return (
-    <Aux>
-      <Header {...{ tabs, page, pathname }}/>
-      <RouteTransition className="tabbed-page" {...{ wrapperComponent, opts, pathname, ...effect }}>
-        { children }
-      </RouteTransition>
-    </Aux>
-  );
-};
+class Tickets extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = this.getInitialState();
+  }
+
+  getInitialState() {
+    return {
+      prevRoute: null,
+    };
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+      this.setState({prevRoute: this.props.location.pathname.split("/")[2]});
+    }
+  }
+  render () {
+    const { children, location } = this.props;
+    const { prevRoute } = this.state;
+    const tabs = ["purchase", "mytickets", "governance", "statistics"];
+    const page = "tickets";
+    // this will be removed w/ react router 4
+    const pathname = location.pathname.split("/")[2];
+    // Still unable to determine 'from' tab which would determine which transition to use for middle tabs
+    const effect = !prevRoute ? enterLeft : tabs.indexOf(prevRoute) > tabs.indexOf(pathname) ? enterLeft : enterRight;
+    return (
+      <Aux>
+        <Header {...{ tabs, page, pathname }}/>
+        <RouteTransition className="tabbed-page" {...{ wrapperComponent, opts, pathname, ...effect }}>
+          { children }
+        </RouteTransition>
+      </Aux>
+    );
+  }
+}
 
 Tickets.propTypes = { location: PropTypes.object.isRequired };
 
