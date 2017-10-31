416ac9fd0b14a1e7b6a50c1ff81ee141e507da8e
vctt94 <victor.duarte.oliveira@gmail.com>
2017-10-31 13:48:54 -0200

    changes on TicketAutoBuyerInfo and removing app/components/views/TicketsPage/Page.js

--- app/components/TicketAutoBuyerInfo.js
+++ app/components/TicketAutoBuyerInfo.js
@@ -10,31 +10,30 @@ class TicketAutobuyerInfo extends React.Component {
       <div className="purchase-tickets-modal page-content">
         <div className="purchase-tickets-header">
           <div className="purchase-tickets-header-text">
-            <T id="tickets.info.title" m="Ticket Purchase Information" />
+            <T id="TicketAutoBuyer.info.title" m="Ticket Auto Buyer Information" />
           </div>
           <SlateGrayButton className="purchase-tickets-close-button" onClick={() =>this.props.closeModal()}>Close</SlateGrayButton>
         </div>
         <div className="purchase-tickets-column">
           {this.renderSection(
-            <T id="tickets.info.balanceToMaintain.header" m="Balance to Maintain" />,
-            <T id="tickets.info.balanceToMaintain.message" m="If your balance is lower than this number, you will not buy tickets. The default of 0 will use all the funds in your account to buy tickets." />
+            <T id="TicketAutoBuyer.info.balanceToMaintain.header" m="Balance to Maintain" />,
+            <T id="TicketAutoBuyer.info.balanceToMaintain.message" m="If your balance is lower than this number, you will not buy tickets. The default of 0 will use all the funds in your account to buy tickets." />
           )}
           {this.renderSection(
-            <T id="tickets.info.maxFee.header" m="Max Fee" />,
-            <T id="tickets.info.maxFee.message" m="Tickets are entered into the mempool in order of their fee per kilobyte. This sets the maximum fee you are willing to pay." />
+            <T id="TicketAutoBuyer.info.maxFee.header" m="Max Fee" />,
+            <T id="TicketAutoBuyer.info.maxFee.message" m="Tickets are entered into the mempool in order of their fee per kilobyte. This sets the maximum fee you are willing to pay." />
           )}
           {this.renderSection(
-            <T id="tickets.info.maxPriceAbsolute.header" m="Max Price Absolute" />,
-            <T id="tickets.info.maxPriceAbsolute.message" m="If the ticket price is above this value, you will not buy more tickets. The default of 0 turns this off." />
+            <T id="TicketAutoBuyer.info.maxPriceAbsolute.header" m="Max Price Absolute" />,
+            <T id="TicketAutoBuyer.info.maxPriceAbsolute.message" m="If the ticket price is above this value, you will not buy more tickets. The default of 0 turns this off." />
           )}
           {this.renderSection(
-            <T id="tickets.info.maxPriceRelative.header" m="Max Price Relative" />,
-            <T id="tickets.info.maxPriceRelative.message" m="ickets are entered into the mempool in order of their fee per kilobyte. This sets the maximum fee you are willing to pay." />
+            <T id="TicketAutoBuyer.info.maxPriceRelative.header" m="Max Price Relative" />,
+            <T id="TicketAutoBuyer.info.maxPriceRelative.message" m="Tickets are entered into the mempool in order of their fee per kilobyte. This sets the maximum fee you are willing to pay." />
           )}
           {this.renderSection(
-            <T id="tickets.info.maxPerBlock.header" m="Max Per Block" />,
-            <T id="tickets.info.maxPerBlock.message" m="Do not buy more than this number of tickets per block. A negative number means buy one ticket every n blocks. e.g. -2 would mean buy a ticket every second block." />
-
+            <T id="TicketAutoBuyer.info.maxPerBlock.header" m="Max Per Block" />,
+            <T id="TicketAutoBuyer.info.maxPerBlock.message" m="Do not buy more than this number of tickets per block. A negative number means buy one ticket every n blocks. e.g. -2 would mean buy a ticket every second block." />
           )}
         </div>
       </div>
