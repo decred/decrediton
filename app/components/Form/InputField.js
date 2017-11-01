50719a4d2331c96d81f41a580fd9ef5c9490230c
vctt94 <victor.duarte.oliveira@gmail.com>
2017-11-01 18:35:43 -0200

    fixing error place

--- app/components/Form/InputField.js
+++ app/components/Form/InputField.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "style/Input.less";
 
 const inputField = ({
   input,
@@ -7,12 +8,14 @@ const inputField = ({
   type,
   meta: { touched, error },
                      }) => (
-    <div className="message-form-group">
-      <div className="message-label">
-        {label}
+    <div className="input-label-wrapper">
+      <div className="message-form-group">
+        <div className="message-label">
+          {label}
+        </div>
+        <input {...input} placeholder={placeholder} type={type} />
       </div>
-      <input {...input} placeholder={placeholder} type={type} />
-      {touched && (error && <div className="error-wrapper"><span className="error input-error">{error}</span></div>)}
+      {touched && (error && <div className="error-wrapper"><span className="input-error">{error}</span></div>)}
     </div>
   );
 
