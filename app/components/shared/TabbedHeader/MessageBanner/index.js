6368a826777a4d4c039804856ba48d23cb58904f
jascha_sundaresan <jascha.sundaresan@oracle.com>
2017-11-14 03:36:42 -0800

    trims messagebanner

--- app/components/shared/TabbedHeader/MessageBanner/index.js
+++ app/components/shared/TabbedHeader/MessageBanner/index.js
@@ -1,17 +1,11 @@
 import { messageBanner } from "connectors";
 import { FormattedMessage as T } from "react-intl";
-import Message from "./Message";
+import "style/MessageBanner.less";
 
-const MessageBanner = ({
-  synced,
-}) => {
-  return (
-    !synced ?
-    <div className="notification not-synced">
-      <T id="home.notSyncedInfo" m="The wallet is not fully synced yet. Note: Balances will not be accurate until syncing is complete." />
-    </div> :
-    <Message />
-  );
-};
+const MessageBanner = ({ synced }) =>
+  !synced &&
+  <div className="notification-not-synced">
+    <T id="home.notSyncedInfo" m="The wallet is not fully synced yet. Note: Balances will not be accurate until syncing is complete." />
+  </div>;
 
 export default messageBanner(MessageBanner);
