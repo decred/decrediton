03a18ba5720b8b8dc564db8304158f48de3708cd
Alex Yocom-Piatt <ay-p@decred.org>
2017-11-27 13:26:13 -0500

    Add links to page

--- app/components/views/Help.js
+++ app/components/views/Help.js
@@ -1,5 +1,5 @@
 import { FormattedMessage as T } from "react-intl";
-import { TabbedHeader } from "shared";
+import { TabbedHeader, Tooltip } from "shared";
 import HelpLink from "HelpLink";
 import HelpLinkInfoModal from "HelpLinkInfoModal";
 import { ConstitutionModalContent } from "modals";
@@ -13,8 +13,10 @@ const Help = ({ routes }) => (
         <HelpLink className={"help-github-icon"} href="https://github.com/decred/decrediton"><T id="help.github" m="Github" /></HelpLink>
         <HelpLink className={"help-docs-icon"} href="https://docs.decred.org/"><T id="help.documentation" m="Documentation" /></HelpLink>
         <HelpLink className={"help-stakepools-icon"} href="https://decred.org/#modalOpen"><T id="help.stakepools" m=" Stakepools" /></HelpLink>
+        <HelpLink className={"help-rocketchat-icon"} href="https://rocketchat.decred.org"><T id="help.rocketchat" m="RocketChat" /></HelpLink>
       </div>
       <div className={"help-icon-row"}>
+        <Tooltip text={ <T id="help.matrix.info" m="Use matrix.decred.org as your custom server." /> }><HelpLink className={"help-matrix-icon"} href="https://riot.im/app"><T id="help.freenode" m="Freenode" /></HelpLink></Tooltip>
         <HelpLink className={"help-freenode-icon"} href="https://webchat.freenode.net/?channels=decred&uio=d4"><T id="help.freenode" m="Freenode" /></HelpLink>
         <HelpLink className={"help-forum-icon"} href="https://forum.decred.org"><T id="help.forum" m="Forum" /> </HelpLink>
         <HelpLinkInfoModal className={"help-constitution-icon"}
