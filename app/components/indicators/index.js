c4a7ccfb7efc9888434d70cc86ac65b4ba3a0114
Alex Yocom-Piatt <ay-p@decred.org>
2017-12-04 14:04:06 -0600

    Move RescanProgress to indicators

--- app/components/indicators/index.js
+++ app/components/indicators/index.js
@@ -3,4 +3,5 @@ export { default as StakeyBounceXs } from "./StakeyBounceExtraSmall";
 export { default as LoadingMoreTransactionsIndicator } from "./LoadingMoreTransactions";
 export { default as NoMoreTransactionsIndicator } from "./NoMoreTransactions";
 export { default as DecredLoading } from "./DecredLoading";
+export { default as RescanProgress } from "./RescanProgress";
 
