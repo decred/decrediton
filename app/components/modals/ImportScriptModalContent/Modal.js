92cc6e384ba0ab969899b2cfc6e3a913a93dda9f
Alex Yocom-Piatt <ay-p@decred.org>
2017-12-05 17:00:13 -0500

    styling

--- app/components/modals/ImportScriptModalContent/Modal.js
+++ app/components/modals/ImportScriptModalContent/Modal.js
@@ -11,50 +11,48 @@ const Modal = ({
   setPassPhrase,
   onSubmit
 }) => (
-  <div className="import-script-modal">
-    <div className="modal-section">
-      <div className="import-script-modal-field-ct">
-        <div className="import-script-modal-label">
-          <T id="importScriptModal.redeemScript" m="Redeem Script" />
-          :</div>
-        <TextInput
-          id="script"
-          className="import-script-modal-field"
-          type="text"
-          placeholder=""
-          value={script}
-          onChange={(e) => setScript(e.target.value)}
-        />
-      </div>
-      <div className="import-script-modal-error-ct">
-        <div className="import-script-modal-label"></div>
-        <div className="import-script-modal-error">{(hasFailedAttempt && !script)
-          ? <T id="importScriptModal.errors.noScript" m="*Please enter your script" />
-          : null}</div>
-      </div>
-      <div className="import-script-modal-field-ct">
-        <div className="import-script-modal-label">
-          <T id="importScriptModal.privatePassphrase" m="Private Passphrase" />
-           :</div>
-        <PasswordInput
-          id="passphrase"
-          className="import-script-modal-field"
-          placeholder=""
-          value={passPhrase}
-          onChange={(e) => setPassPhrase(e.target.value)}
-        />
-      </div>
-      <div className="import-script-modal-error-ct">
-        <div className="import-script-modal-label"></div>
-        <div className="import-script-modal-error">{(hasFailedAttempt && !passPhrase)
-          ? <T id="importScriptModal.errors.noPassphrase" m="*Please enter your private passphrase" />
-          : null}</div>
-      </div>
-      <div className="import-script-modal-toolbar">
-        <KeyBlueButton style={{float: "left"}} onClick={onSubmit}>import</KeyBlueButton>
-      </div>
+  <Aux>
+    <div className="import-script-modal-field-ct">
+      <div className="import-script-modal-label">
+        <T id="importScriptModal.redeemScript" m="Redeem Script" />
+        :</div>
+      <TextInput
+        id="script"
+        className="import-script-modal-field"
+        type="text"
+        placeholder=""
+        value={script}
+        onChange={(e) => setScript(e.target.value)}
+      />
     </div>
-  </div>
+    <div className="import-script-modal-error-ct">
+      <div className="import-script-modal-label"></div>
+      <div className="import-script-modal-error">{(hasFailedAttempt && !script)
+        ? <T id="importScriptModal.errors.noScript" m="*Please enter your script" />
+        : null}</div>
+    </div>
+    <div className="import-script-modal-field-ct">
+      <div className="import-script-modal-label">
+        <T id="importScriptModal.privatePassphrase" m="Private Passphrase" />
+         :</div>
+      <PasswordInput
+        id="passphrase"
+        className="import-script-modal-field"
+        placeholder=""
+        value={passPhrase}
+        onChange={(e) => setPassPhrase(e.target.value)}
+      />
+    </div>
+    <div className="import-script-modal-error-ct">
+      <div className="import-script-modal-label"></div>
+      <div className="import-script-modal-error">{(hasFailedAttempt && !passPhrase)
+        ? <T id="importScriptModal.errors.noPassphrase" m="*Please enter your private passphrase" />
+        : null}</div>
+    </div>
+    <div className="import-script-modal-toolbar">
+      <KeyBlueButton style={{float: "left"}} onClick={onSubmit}>import</KeyBlueButton>
+    </div>
+  </Aux>
 );
 
 export default Modal;
--- app/components/modals/PassphraseModalContent/Modal.js
+++ app/components/modals/PassphraseModalContent/Modal.js
@@ -11,37 +11,35 @@ const Modal =({
   hasAttemptedSubmit,
   onSubmit
 }) => (
-  <div className="passphrase-modal">
-    <div className="modal-section">
-      <div className="passphrase-modal-field-error-ct">
-        <div className="passphrase-modal-field-ct">
-          <div className="passphrase-modal-label">
-            <T id="passphraseModal.privatePassphrase" m="Private Passphrase" />
-            :</div>
-          <PasswordInput
-            id="passphrase"
-            className="passphrase-modal-field"
-            placeholder=""
-            value={passPhrase}
-            onChange={(e) => setPassPhrase(e.target.value)}
-          />
-        </div>
-        <div className="passphrase-modal-error-ct">
-          <div className="passphrase-modal-label"></div>
-          <div className="passphrase-modal-error">
-            {(hasAttemptedSubmit && !passPhrase)
-            ? <T id="passphraseModal.errors.noPassphrase" m="*Please enter your private passphrase" />
-            : null}
-          </div>
-        </div>
+  <Aux>
+    <div className="passphrase-modal-field-error-ct">
+      <div className="passphrase-modal-field-ct">
+        <div className="passphrase-modal-label">
+          <T id="passphraseModal.privatePassphrase" m="Private Passphrase" />
+          :</div>
+        <PasswordInput
+          id="passphrase"
+          className="passphrase-modal-field"
+          placeholder=""
+          value={passPhrase}
+          onChange={(e) => setPassPhrase(e.target.value)}
+        />
       </div>
-      <div className="passphrase-modal-toolbar">
-        <KeyBlueButton className="passphrase-modal-save-button" onClick={onSubmit}>
-          <T id="passphraseModal.confirm" m="confirm" />
-        </KeyBlueButton>
+      <div className="passphrase-modal-error-ct">
+        <div className="passphrase-modal-label"></div>
+        <div className="passphrase-modal-error">
+          {(hasAttemptedSubmit && !passPhrase)
+          ? <T id="passphraseModal.errors.noPassphrase" m="*Please enter your private passphrase" />
+          : null}
+        </div>
       </div>
     </div>
-  </div>
+    <div className="passphrase-modal-toolbar">
+      <KeyBlueButton className="passphrase-modal-save-button" onClick={onSubmit}>
+        <T id="passphraseModal.confirm" m="confirm" />
+      </KeyBlueButton>
+    </div>
+  </Aux>
 );
 
 export default Modal;
