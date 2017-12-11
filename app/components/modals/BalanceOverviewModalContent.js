3f43ed6a9ce7e059a08ef942a4f59db6b6e39725
Alex Yocom-Piatt <ay-p@decred.org>
2017-12-11 16:03:33 -0600

    Apply consistent styling to information modals (#975)

--- app/components/modals/BalanceOverviewModalContent.js
+++ app/components/modals/BalanceOverviewModalContent.js
@@ -1,24 +1,32 @@
+import { FormattedMessage as T } from "react-intl";
+
 export default () => (
   <Aux>
-    <p className="balance-overview-info-ticket-purchase-text-column">
-      <span className="balance-overview-info-highlight-green">Total</span> - This is the total balance controlled by this account currently.
-      <br/>
-      <br/>
-      <span className="balance-overview-info-highlight-blue">Spendable Balance</span> - The spendable/accessible balance for the this account.
-      <br/>
-      <br/>
-      <span className="balance-overview-info-highlight-blue">Locked By Tickets</span> - This is the balance that is currently locked by tickets for this account. Once the tickets are voted or revoked these funds will be unlocked.
-      <br/>
-      <br/>
-      <span className="balance-overview-info-highlight-blue">Voting Authority</span> - This balance shows the total amount that this account has voting authority over.  For example, if you use a voting-only wallet this will show that total amount controlled.
-      <br/>
-      <br/>
-      <span className="balance-overview-info-highlight-blue">Immature Rewards</span> - These are regular coinbase rewards that are currently maturing (from PoW mining reward for instance).
-      <br/>
-      <br/>
-      <span className="balance-overview-info-highlight-blue">Immature Stake Generation </span>- This balance shows the current stake rewards and revocations that are awaiting maturity (256 blocks on mainnet).
-      <br/>
-      <br/>
+    <p className="info-modal-column">
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-green"><T id="balanceInfoModal.total.label" m="Total" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.total.text" m="This is the total balance controlled by this account currently." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.spendable.label" m="Spendable Balance" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.spendable.text" m="The spendable/accessible balance for the this account." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.locked.label" m="Locked By Tickets" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.locked.text" m="This is the balance that is currently locked by tickets for this account. Once the tickets are voted or revoked these funds will be unlocked." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.voting.label" m="Voting Authority" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.voting.text" m="This balance shows the total amount that this account has voting authority over.  For example, if you use a voting-only wallet this will show that total amount controlled." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.immatureRewards.label" m="Immature Rewards" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.immatureRewards.text" m="These are regular coinbase rewards that are currently maturing (from PoW mining reward for instance)." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.immatureStake.label" m="Immature Stake Generation" /> </span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.immatureStake.text" m="This balance shows the current stake rewards and revocations that are awaiting maturity (256 blocks on mainnet)." /></span>
+      </span>
     </p>
   </Aux>
 );
--- app/components/modals/TicketAutoBuyerInfoModalContent.js
+++ app/components/modals/TicketAutoBuyerInfoModalContent.js
@@ -2,22 +2,27 @@ import { FormattedMessage as T } from "react-intl";
 
 export default () => (
   <Aux>
-    <div className="purchase-tickets-column">
-      <div className="purchase-tickets-section">
-        <span className="purchase-tickets-section-header"><T id="ticketAutoBuyer.info.balanceToMaintain.header" m="Balance to Maintain" /></span> - <T id="ticketAutoBuyer.info.balanceToMaintain.message" m="If your balance is lower than this number, you will not buy tickets. The default of 0 will use all the funds in your account to buy tickets." />
-      </div>
-      <div className="purchase-tickets-section">
-        <span className="purchase-tickets-section-header"><T id="ticketAutoBuyer.info.maxFee.header" m="Max Fee" /></span> - <T id="ticketAutoBuyer.info.maxFee.message" m="Tickets are entered into the mempool in order of their fee per kilobyte. This sets the maximum fee you are willing to pay." />
-      </div>
-      <div className="purchase-tickets-section">
-        <span className="purchase-tickets-section-header"><T id="ticketAutoBuyer.info.maxPriceAbsolute.header" m="Max Price Absolute" /></span> - <T id="ticketAutoBuyer.info.maxPriceAbsolute.message" m="If the ticket price is above this value, you will not buy more tickets. The default of 0 turns this off." />
-      </div>
-      <div className="purchase-tickets-section">
-        <span className="purchase-tickets-section-header"><T id="ticketAutoBuyer.info.maxPriceRelative.header" m="Max Price Relative" /></span> - <T id="ticketAutoBuyer.info.maxPriceRelative.message" m="Tickets are entered into the mempool in order of their fee per kilobyte. This sets the maximum fee you are willing to pay." />
-      </div>
-      <div className="purchase-tickets-section">
-        <span className="purchase-tickets-section-header"><T id="ticketAutoBuyer.info.maxPerBlock.header" m="Max Per Block" /></span> - <T id="ticketAutoBuyer.info.maxPerBlock.message" m="Do not buy more than this number of tickets per block. A negative number means buy one ticket every n blocks. e.g. -2 would mean buy a ticket every second block." />
-      </div>
-    </div>
+    <p className="info-modal-column">
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="ticketAutoBuyer.info.balanceToMaintain.header" m="Balance to Maintain" /></span>
+        <span className="info-modal-row-text"><T id="ticketAutoBuyer.info.balanceToMaintain.message" m="If your balance is lower than this number, you will not buy tickets. The default of 0 will use all the funds in your account to buy tickets." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="ticketAutoBuyer.info.maxFee.header" m="Max Fee" /></span>
+        <span className="info-modal-row-text"><T id="ticketAutoBuyer.info.maxFee.message" m="Tickets are entered into the mempool in order of their fee per kilobyte. This sets the maximum fee you are willing to pay." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="ticketAutoBuyer.info.maxPriceAbsolute.header" m="Max Price Absolute" /></span>
+        <span className="info-modal-row-text"><T id="ticketAutoBuyer.info.maxPriceAbsolute.message" m="If the ticket price is above this value, you will not buy more tickets. The default of 0 turns this off." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="ticketAutoBuyer.info.maxPriceRelative.header" m="Max Price Relative" /></span>
+        <span className="info-modal-row-text"><T id="ticketAutoBuyer.info.maxPriceRelative.message" m="Tickets are entered into the mempool in order of their fee per kilobyte. This sets the maximum fee you are willing to pay." /></span>
+      </span>
+      <span className="info-modal-row">
+        <span className="info-modal-row-label highlight-blue"><T id="ticketAutoBuyer.info.maxPerBlock.header" m="Max Per Block" /></span>
+        <span className="info-modal-row-text"><T id="ticketAutoBuyer.info.maxPerBlock.message" m="Do not buy more than this number of tickets per block. A negative number means buy one ticket every n blocks. e.g. -2 would mean buy a ticket every second block." /></span>
+      </span>
+    </p>
   </Aux>
 );
