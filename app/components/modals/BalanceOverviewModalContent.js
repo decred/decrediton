fc706c19cbfcc434a5df6cc6986570b5b0eabc00
Alex Yocom-Piatt <ay-p@decred.org>
2017-12-11 16:34:51 -0500

    i18n for balance info

--- app/components/modals/BalanceOverviewModalContent.js
+++ app/components/modals/BalanceOverviewModalContent.js
@@ -4,28 +4,28 @@ export default () => (
   <Aux>
     <p className="info-modal-column">
       <span className="info-modal-row">
-        <span className="info-modal-row-label highlight-green">Total</span>
-        <span className="info-modal-row-text">This is the total balance controlled by this account currently.</span>
+        <span className="info-modal-row-label highlight-green"><T id="balanceInfoModal.total.label" m="Total" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.total.text" m="This is the total balance controlled by this account currently." /></span>
       </span>
       <span className="info-modal-row">
-        <span className="info-modal-row-label highlight-blue">Spendable Balance</span>
-        <span className="info-modal-row-text"> The spendable/accessible balance for the this account.</span>
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.spendable.label" m="Spendable Balance" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.spendable.text" m="The spendable/accessible balance for the this account." /></span>
       </span>
       <span className="info-modal-row">
-        <span className="info-modal-row-label highlight-blue">Locked By Tickets</span>
-        <span className="info-modal-row-text"> This is the balance that is currently locked by tickets for this account. Once the tickets are voted or revoked these funds will be unlocked.</span>
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.locked.label" m="Locked By Tickets" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.locked.text" m="This is the balance that is currently locked by tickets for this account. Once the tickets are voted or revoked these funds will be unlocked." /></span>
       </span>
       <span className="info-modal-row">
-        <span className="info-modal-row-label highlight-blue">Voting Authority</span>
-        <span className="info-modal-row-text"> This balance shows the total amount that this account has voting authority over.  For example, if you use a voting-only wallet this will show that total amount controlled.</span>
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.voting.label" m="Voting Authority" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.voting.text" m="This balance shows the total amount that this account has voting authority over.  For example, if you use a voting-only wallet this will show that total amount controlled." /></span>
       </span>
       <span className="info-modal-row">
-        <span className="info-modal-row-label highlight-blue">Immature Rewards</span>
-        <span className="info-modal-row-text"> These are regular coinbase rewards that are currently maturing (from PoW mining reward for instance).</span>
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.immatureRewards.label" m="Immature Rewards" /></span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.immatureRewards.text" m="These are regular coinbase rewards that are currently maturing (from PoW mining reward for instance)." /></span>
       </span>
       <span className="info-modal-row">
-        <span className="info-modal-row-label highlight-blue">Immature Stake Generation </span>
-        <span className="info-modal-row-text"> This balance shows the current stake rewards and revocations that are awaiting maturity (256 blocks on mainnet).</span>
+        <span className="info-modal-row-label highlight-blue"><T id="balanceInfoModal.immatureStake.label" m="Immature Stake Generation" /> </span>
+        <span className="info-modal-row-text"><T id="balanceInfoModal.immatureStake.text" m="This balance shows the current stake rewards and revocations that are awaiting maturity (256 blocks on mainnet)." /></span>
       </span>
     </p>
   </Aux>
