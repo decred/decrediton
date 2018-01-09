b901cd009b22c324b29d6e4be2e7b2ce0313a697
Matheus Degiovani <opensource@matheusd.com>
2018-01-09 17:37:57 -0200

    Change log button styling during daemon loading

--- app/components/views/GetStartedPage/DaemonLoading/Form.js
+++ app/components/views/GetStartedPage/DaemonLoading/Form.js
@@ -33,14 +33,16 @@ const DaemonLoadingFormBody = ({
     <div className="get-started-content-new-seed">
     {getDaemonStarted ? getCurrentBlockCount == null ?
       showLongWaitMessage ?
-      <div className="get-started-fetch-headers-message">
-        <T id="getStarted.chainLoading" m="The Decred chain is currently loading and may take a few minutes." />
-        <div style={{marginTop: "1em"}}>
+      <Aux>
+        <div className="get-started-fetch-headers-message">
+          <T id="getStarted.chainLoading" m="The Decred chain is currently loading and may take a few minutes." />
+        </div>
+        <div className="get-started-bottom-buttons">
           <SlateGrayButton onClick={onShowLogs}>
             <T id="getStarted.btnLogs" m="Logs" />
           </SlateGrayButton>
         </div>
-      </div> :
+      </Aux> :
       <div></div> :
       <Aux>
         <div className="get-started-content-instructions">
