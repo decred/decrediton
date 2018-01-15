16cb03c2a4c03b3b9764dc71dc79c62599540ad8
vctt94 <victor.duarte.oliveira@gmail.com>
2018-01-15 12:25:02 -0200

    refactoring tabbed page to use tabbed component

--- app/components/shared/TabbedComponent.js
+++ app/components/shared/TabbedComponent.js
@@ -7,7 +7,7 @@ const mapStyles = styles => ({ left: styles.left + "%" });
 const enterLeft = { atEnter: { left: -100 }, atActive: { left: 0 }, atLeave: { left: 100 }, mapStyles };
 const enterRight = { atEnter: { left: 100 }, atActive: { left: 0 }, atLeave: { left: -100 }, mapStyles };
 
-const wrapperComponent = props => <div className="component-tab-content" { ...props } />;
+const wrapperComponentTemp = props => <div className="component-tab-content" { ...props } />;
 
 class TabbedPage extends React.Component{
   constructor(props) { super(props); }
@@ -19,15 +19,16 @@ class TabbedPage extends React.Component{
     }
   }
   render () {
-    const { children, routes } = this.props;
+    const { children, routes, className } = this.props;
     const { prevTab } = this.state;
     const tabs = getTabs(routes);
     const pathname = getTab(routes);
     const effect = !prevTab ? enterLeft : tabs.indexOf(prevTab) > tabs.indexOf(pathname) ? enterLeft : enterRight;
+    const wrapperComponent = this.props.wrapperComponent ? this.props.wrapperComponent : wrapperComponentTemp;
     return (
       <Aux>
         <TabbedHeader {...{ routes }}/>
-        <RouteTransition opts={ theme("springs.tab") } {...{ wrapperComponent, pathname, ...effect }}>
+        <RouteTransition className={className} opts={ theme("springs.tab") } {...{ wrapperComponent, pathname, ...effect }}>
           { children }
         </RouteTransition>
       </Aux>
@@ -35,6 +36,11 @@ class TabbedPage extends React.Component{
   }
 }
 
-TabbedPage.propTypes = { location: PropTypes.object.isRequired };
+TabbedPage.propTypes = {
+  children: PropTypes.object.isRequired,
+  routes: PropTypes.array.isRequired,
+  className: PropTypes.string,
+  wrapperComponent: PropTypes.function
+};
 
 export default TabbedPage;
