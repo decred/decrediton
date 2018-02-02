0217b24ade49922f901ab9364fc7aae7a08fac7c
Alex Yocom-Piatt <ay-p@decred.org>
2018-02-02 08:36:07 -0600

    Use same method in fetch headers to avoid rescan blink (#1134)

--- app/components/views/GetStartedPage/RescanWallet/index.js
+++ app/components/views/GetStartedPage/RescanWallet/index.js
@@ -1,6 +1,55 @@
 import {
   RescanWalletFormHeader as RescanWalletHeader,
-  RescanWalletFormBody as RescanWalletBody
+  RescanWalletFormBody
 } from "./Form";
 
+class RescanWalletBody extends React.Component {
+
+  constructor(props)  {
+    super(props);
+    this.state = this.getInitialState();
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+    this.resetState();
+    if(this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+  }
+
+  getInitialState() {
+    return {
+      showLongWaitMessage: false
+    };
+  }
+
+  render() {
+    const { showLongWaitMessage } = this.state;
+
+    return (
+      <RescanWalletFormBody
+        {...{
+          ...this.props,
+          showLongWaitMessage
+        }}
+      />
+    );
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    this.timeoutId = setTimeout(() => {
+      if(this.mounted) {
+        this.setState({ showLongWaitMessage: true });
+      }
+      delete this.timeoutId;
+    }, 2000);
+  }
+
+  resetState() {
+    this.setState(this.getInitialState());
+  }
+
+}
 export { RescanWalletHeader, RescanWalletBody };
