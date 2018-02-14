00460b7e0f48e2a1e49d7e4580d0effb9d88f87c
Matheus Degiovani <opensource@matheusd.com>
2018-02-14 09:10:25 -0200

    Fix txdetails layout

--- app/components/views/TxDetails.js
+++ app/components/views/TxDetails.js
@@ -1,4 +1,5 @@
-import { Balance, TabbedHeader, CopyToClipboard } from "shared";
+import { Balance, CopyToClipboard } from "shared";
+import { StandaloneHeader, StandalonePage } from "layout";
 import { shell } from "electron";
 import { transactionDetails } from "connectors";
 import { SlateGrayButton } from "buttons";
@@ -72,12 +73,7 @@ const TxDetails = ({ routes,
   const openTxUrl = () => shell.openExternal(txUrl);
   const openBlockUrl = () => shell.openExternal(txBlockUrl);
   const title = txType ? intl.formatMessage(messages[txType]) :
-    <div className="txdetails-header-title">
-      <span className="bold">
-        {txDirection === "in" ? "" : "-"}
-      </span>
-      <Balance title bold amount={txAmount}/>
-    </div>;
+    <Balance title bold amount={txDirection !== "in" ? -txAmount : txAmount}/>;
 
   let nonWalletInputs = [];
   let nonWalletOutputs = [];
@@ -94,113 +90,119 @@ const TxDetails = ({ routes,
   }
   const hasNonWalletIO = nonWalletInputs.length || nonWalletOutputs.length;
 
+  const backBtn =
+    <SlateGrayButton onClick={ goBack }>
+      <T id="txDetails.backBtn" m="Back" />
+    </SlateGrayButton>;
+
+  const header =
+    <StandaloneHeader
+      title={title}
+      iconClassName={icon}
+      description={subtitle}
+      actionButton={backBtn}
+    />;
+
   return (
-    <Aux>
-      <TabbedHeader {...{ routes, icon, title, subtitle }}>
-        <SlateGrayButton onClick={ goBack }>
-          <T id="txDetails.backBtn" m="Back" />
-        </SlateGrayButton>
-      </TabbedHeader>
-      <div className="page-content">
-        <div className="txdetails-content-nest">
-          <div className="txdetails-top">
-            <div className="txdetails-name">
-              <T id="txDetails.transactionLabel" m="Transaction" />:
-            </div>
-            <div className="txdetails-value">
-              <a onClick={ openTxUrl } style={{ cursor: "pointer" }}>{txHash}</a>
-            </div>
-            <div className="txdetails-name">
-              {isConfirmed ? (<div className="txdetails-indicator-confirmed">
-                <T id="transaction.indicatorConfirmed" m="Confirmed" />
-              </div>) : (<div className="txdetails-indicator-pending">
-                <T id="transaction.indicatorPending" m="Pending" /></div>)}
-            </div>
-            <div className="txdetails-value">
-              <span className="txdetails-value-text">
-                <T id="transaction.confirmationHeight"
-                  m="{confirmations, plural, =0 {pending} one {# confirmation} other {# confirmations}}"
-                  values={{confirmations: (isConfirmed ? currentBlockHeight - txHeight : 0)}} />
-              </span>
-            </div>
-            <div className="txdetails-overview">
-              <div className="txdetails-input-area">
-                <div className="txdetails-overview-title-consumed">
-                  <T id="txDetails.walletInputs" m="Wallet Inputs" />
-                </div>
-                <div className="txdetails-input-arrow"></div>
-                {txInputs.map(({ accountName, amount }, idx) => (
-                  <div key={idx} className="txdetails-row">
-                    <div className="txdetails-address">{accountName}</div>
-                    <div className="txdetails-amount"><Balance amount={amount} /></div>
-                  </div>
-                ))}
+    <StandalonePage header={header}>
+      <div className="txdetails-content-nest">
+        <div className="txdetails-top">
+          <div className="txdetails-name">
+            <T id="txDetails.transactionLabel" m="Transaction" />:
+          </div>
+          <div className="txdetails-value">
+            <a onClick={ openTxUrl } style={{ cursor: "pointer" }}>{txHash}</a>
+          </div>
+          <div className="txdetails-name">
+            {isConfirmed ? (<div className="txdetails-indicator-confirmed">
+              <T id="transaction.indicatorConfirmed" m="Confirmed" />
+            </div>) : (<div className="txdetails-indicator-pending">
+              <T id="transaction.indicatorPending" m="Pending" /></div>)}
+          </div>
+          <div className="txdetails-value">
+            <span className="txdetails-value-text">
+              <T id="transaction.confirmationHeight"
+                m="{confirmations, plural, =0 {pending} one {# confirmation} other {# confirmations}}"
+                values={{confirmations: (isConfirmed ? currentBlockHeight - txHeight : 0)}} />
+            </span>
+          </div>
+          <div className="txdetails-overview">
+            <div className="txdetails-input-area">
+              <div className="txdetails-overview-title-consumed">
+                <T id="txDetails.walletInputs" m="Wallet Inputs" />
               </div>
-              <div className="txdetails-output-area">
-                <div className="txdetails-overview-title-created">
-                  <T id="txDetails.walletOutputs" m="Wallet Outputs" />
+              <div className="txdetails-input-arrow"></div>
+              {txInputs.map(({ accountName, amount }, idx) => (
+                <div key={idx} className="txdetails-row">
+                  <div className="txdetails-address">{accountName}</div>
+                  <div className="txdetails-amount"><Balance amount={amount} /></div>
                 </div>
-                {txOutputs.map(({ accountName, address, amount }, idx) => (
-                  <div key={idx} className="txdetails-row">
-                    <div className="txdetails-address">{txDirection === "out" ? "change" : accountName ? addSpacingAroundText(accountName) : addSpacingAroundText(address)}</div>
-                    <div className="txdetails-amount"><Balance amount={amount} /></div>
-                  </div>
-                ))}
-              </div>
+              ))}
             </div>
-
-            {hasNonWalletIO
-              ? <Aux>
-                <div className="txdetails-overview">
-                  <div className="txdetails-input-area">
-                    <div className="txdetails-overview-title-consumed">
-                      <T id="txDetails.nonWalletInputs" m="Non Wallet Inputs" />
-                    </div>
-                    {nonWalletInputs.map(({ address, amount }, idx) => (
-                      <div key={idx} className="txdetails-row">
-                        <div className="txdetails-address">{addSpacingAroundText(address)}</div>
-                        <div className="txdetails-amount"><Balance amount={amount} /></div>
-                      </div>
-                    ))}
-                  </div>
-                  <div className="txdetails-output-area">
-                    <div className="txdetails-overview-title-created">
-                      <T id="txDetails.nonWalletOutputs" m="Non Wallet Outputs" />
-                    </div>
-                    {nonWalletOutputs.map(({ address, amount }, idx) => (
-                      <div key={idx} className="txdetails-row">
-                        <div className="txdetails-address">{addSpacingAroundText(address)}</div>
-                        <div className="txdetails-amount">{amount}</div>
-                      </div>
-                    ))}
-                  </div>
+            <div className="txdetails-output-area">
+              <div className="txdetails-overview-title-created">
+                <T id="txDetails.walletOutputs" m="Wallet Outputs" />
+              </div>
+              {txOutputs.map(({ accountName, address, amount }, idx) => (
+                <div key={idx} className="txdetails-row">
+                  <div className="txdetails-address">{txDirection === "out" ? "change" : accountName ? addSpacingAroundText(accountName) : addSpacingAroundText(address)}</div>
+                  <div className="txdetails-amount"><Balance amount={amount} /></div>
                 </div>
-              </Aux> : null}
+              ))}
+            </div>
+          </div>
 
-            {txDirection !== "in" && txType !== "Vote" &&
+          {hasNonWalletIO
+            ? <Aux>
+              <div className="txdetails-overview">
+                <div className="txdetails-input-area">
+                  <div className="txdetails-overview-title-consumed">
+                    <T id="txDetails.nonWalletInputs" m="Non Wallet Inputs" />
+                  </div>
+                  {nonWalletInputs.map(({ address, amount }, idx) => (
+                    <div key={idx} className="txdetails-row">
+                      <div className="txdetails-address">{addSpacingAroundText(address)}</div>
+                      <div className="txdetails-amount"><Balance amount={amount} /></div>
+                    </div>
+                  ))}
+                </div>
+                <div className="txdetails-output-area">
+                  <div className="txdetails-overview-title-created">
+                    <T id="txDetails.nonWalletOutputs" m="Non Wallet Outputs" />
+                  </div>
+                  {nonWalletOutputs.map(({ address, amount }, idx) => (
+                    <div key={idx} className="txdetails-row">
+                      <div className="txdetails-address">{addSpacingAroundText(address)}</div>
+                      <div className="txdetails-amount">{amount}</div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </Aux> : null}
+
+          {txDirection !== "in" && txType !== "Vote" &&
+          <Aux>
+            <div className="txdetails-name"><T id="txDetails.transactionFeeLabel" m="Transaction fee" />:</div>
+            <div className="txdetails-value"><Balance amount={txFee} /></div>
+          </Aux> }
+        </div>
+        <div className="txdetails-details">
+          <div className="txdetails-title"><T id="txDetails.properties" m="Properties" /></div>
+          {isConfirmed &&
             <Aux>
-              <div className="txdetails-name"><T id="txDetails.transactionFeeLabel" m="Transaction fee" />:</div>
-              <div className="txdetails-value"><Balance amount={txFee} /></div>
-            </Aux> }
-          </div>
-          <div className="txdetails-details">
-            <div className="txdetails-title"><T id="txDetails.properties" m="Properties" /></div>
-            {isConfirmed &&
-              <Aux>
-                <div className="txdetails-name"><T id="txDetails.blockLabel" m="Block" />:</div>
-                <div className="txdetails-value">
-                  <a onClick={ openBlockUrl } style={{ cursor: "pointer" }}>{txBlockHash}</a>
-                </div>
-                <div className="txdetails-name"><T id="txDetails.blockHeightLabel" m="Height" /> :</div>
-                <div className="txdetails-value">{txHeight}</div>
-              </Aux>
-            }
-            <div className="txdetails-name"><T id="txDetails.rawTransactionLabel" m="Raw Transaction" />:</div>
-            <div className="txdetails-value"><div className="txdetails-value-rawtx">{rawTx}</div><CopyToClipboard textToCopy={rawTx} className="receive-content-nest-copy-to-clipboard-icon" /></div>
-          </div>
+              <div className="txdetails-name"><T id="txDetails.blockLabel" m="Block" />:</div>
+              <div className="txdetails-value">
+                <a onClick={ openBlockUrl } style={{ cursor: "pointer" }}>{txBlockHash}</a>
+              </div>
+              <div className="txdetails-name"><T id="txDetails.blockHeightLabel" m="Height" /> :</div>
+              <div className="txdetails-value">{txHeight}</div>
+            </Aux>
+          }
+          <div className="txdetails-name"><T id="txDetails.rawTransactionLabel" m="Raw Transaction" />:</div>
+          <div className="txdetails-value"><div className="txdetails-value-rawtx">{rawTx}</div><CopyToClipboard textToCopy={rawTx} className="receive-content-nest-copy-to-clipboard-icon" /></div>
         </div>
       </div>
-    </Aux>
+    </StandalonePage>
   );
 };
 
