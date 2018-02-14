93c0bb64cc57504e7c6c022e5286713362e18c6d
Matheus Degiovani <opensource@matheusd.com>
2018-02-14 18:43:31 -0200

    moah cleanup

--- app/components/views/InvalidRPCVersion.js
+++ app/components/views/InvalidRPCVersion.js
@@ -1,23 +1,22 @@
 // @flow
 import { FormattedMessage as T } from "react-intl";
 import { invalidRPCVersion } from "connectors";
+import { StandalonePageBody } from "layout";
 
 const InvalidRPCVersion = ({ requiredWalletRPCVersion, walletRPCVersion, routes }) => (
-  <div className="page-view">
-    <div className="page-content">
-      <div className="invalid-rpc-info">
-        <T
-          id="invalidRPCVersion.info"
-          m={`The API of the currently running wallet ({walletRPCVersion}) is not compatible with Decrediton (required version {requiredWalletRPCVersion}).
+  <StandalonePageBody>
+    <div className="invalid-rpc-info">
+      <T
+        id="invalidRPCVersion.info"
+        m={`The API of the currently running wallet ({walletRPCVersion}) is not compatible with Decrediton (required version {requiredWalletRPCVersion}).
 
-          Please update the daemon (dcrd) and wallet (dcrwallet) to the latest version, then try again.
+        Please update the daemon (dcrd) and wallet (dcrwallet) to the latest version, then try again.
 
-          See the "Help ⮕ About" menu for the current version of the executables.`}
-          values={{ walletRPCVersion, requiredWalletRPCVersion }}
-        />
-      </div>
+        See the "Help ⮕ About" menu for the current version of the executables.`}
+        values={{ walletRPCVersion, requiredWalletRPCVersion }}
+      />
     </div>
-  </div>
+  </StandalonePageBody>
 );
 
 InvalidRPCVersion.propTypes = {
