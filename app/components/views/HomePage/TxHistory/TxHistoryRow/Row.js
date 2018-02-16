d75196c1c0c01050bf28985548b5b716490279e5
vctt94 <victor.duarte.oliveira@gmail.com>
2018-02-16 14:29:14 -0200

    .adding 24h .make row clickable

--- app/components/views/HomePage/TxHistory/TxHistoryRow/Row.js
+++ app/components/views/HomePage/TxHistory/TxHistoryRow/Row.js
@@ -2,9 +2,9 @@ import Status from "./Status";
 import "style/TxHistory.less";
 
 const Row = ({ txAccountName, pending, txTimestamp, onClick, className, children }) => (
-  <div className={"tx-history-overview-row " + className} >
+  <div className={"tx-history-overview-row " + className} {...{ onClick }}>
     {children}
-    <Status {...{ txAccountName, pending, txTimestamp, onClick }} />
+    <Status {...{ txAccountName, pending, txTimestamp }} />
   </div>
 );
 
--- app/components/views/HomePage/TxHistory/TxHistoryRow/Status.js
+++ app/components/views/HomePage/TxHistory/TxHistoryRow/Status.js
@@ -1,19 +1,25 @@
-import { FormattedMessage as T } from "react-intl";
-import { tsToDate } from "helpers/dateFormat";
+import { FormattedDate } from "react-intl";
 import "style/TxHistory.less";
+import { tsToDate } from "helpers/dateFormat";
 
-const Status = ({ pending, txTimestamp, onClick }) => (
+const Status = ({ pending, txTimestamp }) => (
   <Aux>
     {!pending ? (
       <div className="transaction-time-date-spacer">
-        <T id="transaction.timestamp"
-          m="{timestamp, date, medium} {timestamp, time, medium}"
-          values={{ timestamp: tsToDate(txTimestamp) }} />
+        <FormattedDate
+          value={tsToDate(txTimestamp)}
+          year='numeric'
+          month='short'
+          day='2-digit'
+          hour='2-digit'
+          minute='2-digit'
+          hour12={false}
+        />
       </div>) : (
-      <div className="pending-overview-details" {...{ onClick }}>
-        ...
+        <div className="pending-overview-details">
+          ...
       </div>
-    )}
+      )}
   </Aux>
 );
 
