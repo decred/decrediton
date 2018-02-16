20a5e81d2edb36ce0a6db09e6e645c096a7f834f
vctt94 <victor.duarte.oliveira@gmail.com>
2018-02-16 14:53:41 -0200

    ok, now time is right

--- app/components/views/HomePage/TxHistory/TxHistoryRow/Status.js
+++ app/components/views/HomePage/TxHistory/TxHistoryRow/Status.js
@@ -1,4 +1,4 @@
-import { FormattedDate } from "react-intl";
+import { FormattedDate, FormattedMessage } from "react-intl";
 import "style/TxHistory.less";
 import { tsToDate } from "helpers/dateFormat";
 
@@ -6,20 +6,22 @@ const Status = ({ pending, txTimestamp }) => (
   <Aux>
     {!pending ? (
       <div className="transaction-time-date-spacer">
-        <FormattedDate
-          value={tsToDate(txTimestamp)}
-          year='numeric'
-          month='short'
-          day='2-digit'
-          hour='2-digit'
-          minute='2-digit'
-          hour12={false}
+        <FormattedMessage
+          id="myId"
+          defaultMessage="{day} {month} {year} {hour}:{minute}"
+          values={{
+            day: <FormattedDate value={tsToDate(txTimestamp)} day="2-digit"/>,
+            month: <FormattedDate value={tsToDate(txTimestamp)} month="short"/>,
+            year: <FormattedDate value={tsToDate(txTimestamp)} year="numeric"/>,
+            hour: <FormattedDate value={tsToDate(txTimestamp)} hour="2-digit" hour12={false}/>,
+            minute: <FormattedDate value={tsToDate(txTimestamp)} minute="2-digit"/>
+          }}
         />
       </div>) : (
-        <div className="pending-overview-details">
+      <div className="pending-overview-details">
           ...
       </div>
-      )}
+    )}
   </Aux>
 );
 
