7b5e94616a586f959b681ebd65ac1e14cd6adf8e
Alex Yocom-Piatt <ay-p@decred.org>
2018-02-23 15:43:36 -0500

    A few more tweaks for the loading bar

--- app/components/indicators/LinearProgressFull.js
+++ app/components/indicators/LinearProgressFull.js
@@ -1,18 +1,28 @@
 import "style/Loading.less";
 
-const LinearProgressFull = ({ barText, value, min, max }) =>
-  <div className="linear-progress">
-    <div className="linear-progress-bar" style={{ width: `${(value/max-min)*100}` + "%" }}>
-      <div className="linear-progress-box one"  style={{ left: `${(value/max-min)*100}` + "%" }}/>
-      <div className="linear-progress-box two"  style={{ left: `${(value/max-min)*100}` + "%" }}/>
-      <div className="linear-progress-box three"  style={{ left: `${(value/max-min)*100}` + "%" }}/>
-      <div className="linear-progress-box four"  style={{ left: `${(value/max-min)*100}` + "%" }}/>
-      <div className="linear-progress-box five" style={{ left: `${(value/max-min)*100}` + "%" }}/>
-      <div className="linear-progress-box six"  style={{ left: `${(value/max-min)*100}` + "%" }}/>
-      <div className="linear-progress-text">
-        {barText}
+@autobind
+class LinearProgressFull extends React.Component {
+
+  render() {
+    const { barText, value, min, max, error } = this.props;
+    const perComplete = value/max-min;
+    const leftStartingPoint = perComplete*95 + "%";
+    return (
+      <div className="linear-progress">
+        <div className={error ? "linear-progress-bar error" : "linear-progress-bar"} style={error ? {} : { width: `${perComplete*100}` + "%" }}>
+          { perComplete > 0.1   && perComplete < 1 && <div className="linear-progress-box one"    style={{ left: leftStartingPoint }}/> }
+          { perComplete > 0.25  && perComplete < 1 && <div className="linear-progress-box two"    style={{ left: leftStartingPoint }}/> }
+          { perComplete > 0.4   && perComplete < 1 && <div className="linear-progress-box three"  style={{ left: leftStartingPoint }}/> }
+          { perComplete > 0.6   && perComplete < 1 && <div className="linear-progress-box four"   style={{ left: leftStartingPoint }}/> }
+          { perComplete > 0.75  && perComplete < 1 && <div className="linear-progress-box five"   style={{ left: leftStartingPoint }}/> }
+          { perComplete > 0.9   && perComplete < 1 && <div className="linear-progress-box six"    style={{ left: leftStartingPoint }}/> }
+          <div className="linear-progress-text">
+            {barText}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>;
+    );
+  }
+}
 
 export default LinearProgressFull;
