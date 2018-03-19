5890ae14d87953fc55cf320289702ff5ca2e7f43
Alex Yocom-Piatt <ay-p@decred.org>
2018-03-19 17:46:17 -0400

    First pass on modal styling

--- app/components/modals/PassphraseModal/ButtonsToolbar.js
+++ app/components/modals/PassphraseModal/ButtonsToolbar.js
@@ -4,7 +4,7 @@ import { FormattedMessage as T } from "react-intl";
 const PassphraseModalButtonsToolbar = ({ onSubmit, onCancelModal, submitLabel }) =>
   <div className="passphrase-modal-toolbar">
     <KeyBlueButton className="passphrase-modal-save-button" onClick={onSubmit}>
-      {submitLabel ? submitLabel : <T id="passphraseModal.confirm" m="confirm" />}
+      {submitLabel ? submitLabel : <T id="passphraseModal.continue" m="Continue" />}
     </KeyBlueButton>
     <InvisibleButton className="passphrase-modal-close-button" onClick={onCancelModal}>
       <T id="passphraseModal.btnCancel" m="Cancel" />
