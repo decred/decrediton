2031f13296ad3392f0c10618488091bc8ac0faa8
Alex Yocom-Piatt <ay-p@decred.org>
2018-03-20 14:24:32 -0400

    First pass to clean up stakepool add form

--- app/components/views/TicketsPage/PurchaseTab/StakePools/AddForm.js
+++ app/components/views/TicketsPage/PurchaseTab/StakePools/AddForm.js
@@ -24,72 +24,70 @@ const StakePoolsAddForm = ({
   onCancelAddStakePool
 }) => (
   <Aux>
-    <div className="stakepool-flex-height">
-      <div className="stakepool-content-nest-from-address">
-        <div className="stakepool-content-nest-prefix-send">
-          <T id="stake.addPoolTitle" m="Stake Pool" />
-        :</div>
-        <div className="stakepool-unconfigured-select">
-          <StakePoolSelect
-            options={unconfiguredStakePools}
-            value={selectedUnconfigured}
-            onChange={onChangeSelectedUnconfigured}
-          />
+    <div className="stakepool-add-title">
+      <T id="stakepool.addPoolTitle" m="Add a Stakepool" />
+    </div>
+    <div className="stakepool-add-area">
+      <div className="stakepool-add-area-left">
+        <div className="stakepool-field">
+          <div className="stakepool-field-label">
+            <T id="stakepool.label" m="Stakepool" />:
+          </div>
+          <div className="stakepool-field-value">
+            <StakePoolSelect
+              options={unconfiguredStakePools}
+              value={selectedUnconfigured}
+              onChange={onChangeSelectedUnconfigured}
+            />
+          </div>
         </div>
-        <div className="stakepool-content-nest-from-address-wallet-icon"></div>
-      </div>
-      <div className="stakepool-content-nest-api-key-instructions">
-        <span>
-          <T id="stake.addPool.info" m={`
-            Please select your desired stakepool from the above dropdown and follow these instructions:
-
-            1) Create an account or login to your existing account at {stakePoolLink}.
-            2) Once logged in, select the 'Settings' tab.
-            3) Copy and paste your Api Key into the field below (typically starts with 'eyJhb...').
-            4) Click Add and enter your private passphrase.
-
-            {noticeSpan} If you receive an error about the script not being redeemable when attempting to add your stakepool, you can try the following:
-            - Each stakepool account you create can only be associated with 1 wallet.  If you have previously created this stakepool account with a different wallet (different seed), then you must create a new account.
-            - If you had previously used a 'voting account', for your ticket purchases, please go to the Accounts page and create a new account.  This may now allow you to successfully import your script for your stakepool.
-            `}
-          values={{
-            stakePoolLink: <a className="stakepool-link" onClick={function(x){shell.openExternal(x);}.bind(null, selectedUnconfigured.label)}>{selectedUnconfigured.label}</a>,
-            noticeSpan: <span className="stakepool-highligh-text-orange">
-              <T id="stake.addPool.notice" m="Notice!" />
-            </span>
-          }}/>
-        </span>
-      </div>
-      <div className="stakepool-content-nest-to-address">
-        <div className="stakepool-content-nest-api-key">
-          <div className="stakepool-input-form">
+        <div className="stakepool-field">
+          <div className="stakepool-field-label">
+            <T id="stakepool.apikey" m="API Key" />:
+          </div>
+          <div className="stakepool-field-value">
             <TextInput
               type="text"
-              className="stakepool-content-nest-address-amount-sum"
+              className="stakepool-add-apikey"
               placeholder={intl.formatMessage(messages.apiKeyPlaceholder)}
               value={apiKey}
               onChange={e => onChangeApiKey(e.target.value)}
             />
           </div>
+          <div className="stakepool-field-value-error">
+            {apiKey ? null : <T id="stake.addPool.errors.noApiKey" m="*Please enter your API key" /> }
+          </div>
         </div>
-        <div className="stakepool-api-key-error">
-          {apiKey ? null : <T id="stake.addPool.errors.noApiKey" m="*Please enter your API key" /> }
+      </div>
+      <div className="stakepool-add-area-right">
+        <div className="stakepool-add-instructions">
+          <T id="stake.addPool.info" m={
+            "Create an account or login to your existing account at {stakePoolLink} Once logged in, select the ‘Settings’ tab, copy and paste your API KEY into the field."
+          }
+          values={{
+            stakePoolLink: <a className="stakepool-link" onClick={function(x){shell.openExternal(x);}.bind(null, selectedUnconfigured.label)}>{selectedUnconfigured.label}</a>
+          }}/>
+          <div className="stakepool-add-not-redeemable">
+            <T id="stake.notRedeemed" m={"Script not redeemable?"} />
+          </div>
         </div>
       </div>
     </div>
-    <PassphraseModalButton
-      modalTitle={<T id="stake.addPoolConfirmation" m="Stakepool Confirmation" />}
-      disabled={!apiKey}
-      className="stakepool-content-send"
-      onSubmit={onSetStakePoolInfo}
-      buttonLabel={<T id="stake.addPool.addBtn" m="Add" />}
-    />
-    {configuredStakePools.length ? (
-      <SlateGrayButton
-        className="stakepool-hide-config"
-        onClick={onCancelAddStakePool}
-      ><T id="stake.addPool.cancelBtn" m="Cancel" /></SlateGrayButton>
-    ) : null}
+    <div className="stakepool-add-toolbar">
+      <PassphraseModalButton
+        modalTitle={<T id="stake.addPoolConfirmation" m="Stakepool Confirmation" />}
+        disabled={!apiKey}
+        className="stakepool-confirm-button"
+        onSubmit={onSetStakePoolInfo}
+        buttonLabel={<T id="stake.addPool.addBtn" m="Continue" />}
+      />
+      {configuredStakePools.length ? (
+        <SlateGrayButton
+          className="stakepool-hide-config"
+          onClick={onCancelAddStakePool}
+        ><T id="stake.addPool.cancelBtn" m="Cancel" /></SlateGrayButton>
+      ) : null}
+    </div>
   </Aux>
 );
 
