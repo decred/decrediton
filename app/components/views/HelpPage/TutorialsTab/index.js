d4d41d2d2e0268c2db5d2a530079bd2073f7e4a3
Matheus Degiovani <opensource@matheusd.com>
2018-03-21 09:39:07 -0300

    Add tutorials tab to help

---
+++ app/components/views/HelpPage/TutorialsTab/index.js
@@ -0,0 +1,22 @@
+import { FormattedMessage as T } from "react-intl";
+import { DescriptionHeader } from "layout";
+import { Link } from "react-router-dom";
+
+export const TutorialsTabHeader = () =>
+  <DescriptionHeader
+    description={<T id="help.description.tutorials" m="Learn about the various aspects of the Decred network." />}
+  />;
+
+export const TutorialsTab = () => (
+  <div className="overview-no-tickets">
+    <Link to="/mytickets/purchase" className="purchaseTickets">
+      <T id="home.noTickets.purchase" m="Stake Your Idle DCR and Earn Rewards" /> →
+    </Link>
+    <Link to="/tutorial/staking" className="whatIsStaking">
+      <T id="home.noTickets.staking" m="What is Staking (Proof-of-Stake)?" /> →
+    </Link>
+    <Link to="/tutorial/ticketLifecycle" className="ticketLifeCycle">
+      <T id="home.noTickets.lifecycle" m="Learn About the Ticket Lifecycle" /> →
+    </Link>
+  </div>
+);
