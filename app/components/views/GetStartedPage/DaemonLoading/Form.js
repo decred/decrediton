2294b7379bf443f3d4d7726a7fa6c2b25236df99
Alex Yocom-Piatt <ay-p@decred.org>
2018-04-17 15:04:43 -0400

    Update release version

--- app/components/views/GetStartedPage/DaemonLoading/Form.js
+++ app/components/views/GetStartedPage/DaemonLoading/Form.js
@@ -53,7 +53,7 @@ export default ({
           <SlateGrayButton className="tutorial-button" onClick={onShowTutorial}>
             <T id="getStarted.learnBasics" m="Learn the Basics" />
           </SlateGrayButton>
-          <span onClick={onShowReleaseNotes} className="whatsnew"><T id="getStarted.whatsNew" m="What's New in {version}" values={{ version: ("v1.1.2") }}/></span>
+          <span onClick={onShowReleaseNotes} className="whatsnew"><T id="getStarted.whatsNew" m="What's New in {version}" values={{ version: ("v1.2.0") }}/></span>
         </div>
         <div className="loader-bar">
           <LinearProgressFull
