b667fd07cbf3bd1456755332b8398393302a8f6a
Alex Yocom-Piatt <ay-p@decred.org>
2018-05-02 14:49:41 -0500

    Refactor Control Actions (#1317)
    Originally from PR from go1dfish

--- app/wallet/client.js
+++ app/wallet/client.js
@@ -1,11 +1,7 @@
 import Promise from "promise";
 import { TicketTypes } from "helpers/tickets";
-import {
-  PingRequest, NetworkRequest, AccountNumberRequest, AccountsRequest,
-  BalanceRequest, TicketPriceRequest, StakeInfoRequest,
-  AgendasRequest, VoteChoicesRequest, SetVoteChoicesRequest, GetTicketsRequest,
-} from "middleware/walletrpc/api_pb";
 import { withLog as log, logOptionNoResponseData, withLogNoData, withLogNoResponseData } from "./app";
+import * as api from "middleware/walletrpc/api_pb";
 
 const promisifyReq = (fnName, Req) => log((service, ...args) => new Promise((ok, fail) =>
   service[fnName](new Req(), ...args, (err, res) => err ? fail(err) : ok(res))), fnName);
@@ -13,30 +9,33 @@ const promisifyReq = (fnName, Req) => log((service, ...args) => new Promise((ok,
 const promisifyReqLogNoData = (fnName, Req) => withLogNoData((service, ...args) => new Promise((ok, fail) =>
   service[fnName](new Req(), ...args, (err, res) => err ? fail(err) : ok(res))), fnName);
 
-export const getNetwork = promisifyReq("network", NetworkRequest);
-export const getStakeInfo = promisifyReqLogNoData("stakeInfo", StakeInfoRequest);
-export const getTicketPrice = promisifyReq("ticketPrice", TicketPriceRequest);
-export const getAccounts = promisifyReqLogNoData("accounts", AccountsRequest);
-export const getAgendas = promisifyReq("agendas", AgendasRequest);
-export const getVoteChoices = promisifyReq("voteChoices", VoteChoicesRequest);
-export const doPing = promisifyReq("ping", PingRequest);
+export const getNetwork = promisifyReq("network", api.NetworkRequest);
+export const getStakeInfo = promisifyReqLogNoData("stakeInfo", api.StakeInfoRequest);
+export const getTicketPrice = promisifyReq("ticketPrice", api.TicketPriceRequest);
+export const getAccounts = promisifyReqLogNoData("accounts", api.AccountsRequest);
+export const getAgendas = promisifyReq("agendas", api.AgendasRequest);
+export const getVoteChoices = promisifyReq("voteChoices", api.VoteChoicesRequest);
+export const doPing = promisifyReq("ping", api.PingRequest);
+export const loadActiveDataFilters = promisifyReq("loadActiveDataFilters", api.LoadActiveDataFiltersRequest);
+export const getTicketBuyerConfig = promisifyReq("ticketBuyerConfig", api.TicketBuyerConfigRequest);
+export const stopAutoBuyer = promisifyReq("stopAutoBuyer", api.StopAutoBuyerRequest);
 
 export const getBalance = withLogNoResponseData((walletService, accountNum, requiredConfs) => new Promise((ok, fail) => {
-  const request = new BalanceRequest();
+  const request = new api.BalanceRequest();
   request.setAccountNumber(accountNum);
   request.setRequiredConfirmations(requiredConfs);
   walletService.balance(request, (err, res) => err ? fail(err) : ok(res));
 }), "Get Balance");
 
 export const getAccountNumber = log((walletService, accountName) => new Promise((ok, fail) => {
-  const request = new AccountNumberRequest();
+  const request = new api.AccountNumberRequest();
   request.setAccountName(accountName);
   walletService.accountNumber(request, (err, res) => err ? fail(err) : ok(res));
 }), "Get Account Number");
 
 export const getTickets = log((walletService, startHeight, endHeight) => new Promise((ok, fail) => {
   const tickets = [];
-  const request = new GetTicketsRequest();
+  const request = new api.GetTicketsRequest();
   request.setStartingBlockHeight(startHeight);
   request.setEndingBlockHeight(endHeight);
   const getTx = walletService.getTickets(request);
@@ -51,11 +50,10 @@ export const getTickets = log((walletService, startHeight, endHeight) => new Pro
 
 export const setAgendaVote = log((votingService, agendaId, choiceId) =>
   new Promise((ok, fail) => {
-    const request = new SetVoteChoicesRequest();
-    const choice = new SetVoteChoicesRequest.Choice();
+    const request = new api.SetVoteChoicesRequest();
+    const choice = new api.SetVoteChoicesRequest.Choice();
     choice.setChoiceId(choiceId);
     choice.setAgendaId(agendaId);
     request.addChoices(choice);
     votingService.setVoteChoices(request, (err, res) => err ? fail(err) : ok(res));
-  }),
-"Set Agenda Vote");
+  }), "Set Agenda Vote");
--- app/wallet/service.js
+++ app/wallet/service.js
@@ -1,15 +1,8 @@
 import Promise from "promise";
 import * as client from "middleware/grpc/client";
 import { reverseHash } from "../helpers/byteActions";
-import {
-  NextAddressRequest,
-  DecodeRawTransactionRequest,
-  ValidateAddressRequest,
-  GetTransactionsRequest,
-  TransactionDetails,
-  PublishUnminedTransactionsRequest,
-} from "middleware/walletrpc/api_pb";
 import { withLog as log, withLogNoData, logOptionNoResponseData } from "./index";
+import * as api from "middleware/walletrpc/api_pb";
 
 const promisify = fn => (...args) => new Promise((ok, fail) => fn(...args,
   (res, err) => err ? fail(err) : ok(res)));
@@ -23,26 +16,29 @@ export const getDecodeService = promisify(client.getDecodeMessageService);
 
 export const getNextAddress = log((walletService, accountNum) =>
   new Promise((resolve, reject) => {
-    const request = new NextAddressRequest();
+    const request = new api.NextAddressRequest();
     request.setAccount(accountNum);
     request.setKind(0);
+    request.setGapPolicy(api.NextAddressRequest.GapPolicy.GAP_POLICY_WRAP);
     walletService
       .nextAddress(request, (error, response) => error ? reject(error) : resolve(response));
   })
     .then(response => ({
-      publicKey: response.getPublicKey()
+      ...response,
+      publicKey: response.getPublicKey(),
+      address: response.getAddress()
     })), "Get Next Address", logOptionNoResponseData());
 
 export const validateAddress = withLogNoData((walletService, address) =>
   new Promise((resolve, reject) => {
-    const request = new ValidateAddressRequest();
+    const request = new api.ValidateAddressRequest();
     request.setAddress(address);
     walletService.validateAddress(request, (error, response) => error ? reject(error) : resolve(response));
   }), "Validate Address");
 
 export const decodeTransaction = withLogNoData((decodeMessageService, rawTx) =>
   new Promise((resolve, reject) => {
-    var request = new DecodeRawTransactionRequest();
+    var request = new api.DecodeRawTransactionRequest();
     var buffer = Buffer.isBuffer(rawTx) ? rawTx : Buffer.from(rawTx, "hex");
     var buff = new Uint8Array(buffer);
     request.setSerializedTransaction(buff);
@@ -71,11 +67,11 @@ export const TRANSACTION_TYPE_COINBASE = "Coinbase";
 
 // Map from numerical into string transaction type
 export const TRANSACTION_TYPES = {
-  [TransactionDetails.TransactionType.REGULAR]: TRANSACTION_TYPE_REGULAR,
-  [TransactionDetails.TransactionType.TICKET_PURCHASE]: TRANSACTION_TYPE_TICKET_PURCHASE,
-  [TransactionDetails.TransactionType.VOTE]: TRANSACTION_TYPE_VOTE,
-  [TransactionDetails.TransactionType.REVOCATION]: TRANSACTION_TYPE_REVOCATION,
-  [TransactionDetails.TransactionType.COINBASE]: TRANSACTION_TYPE_COINBASE
+  [api.TransactionDetails.TransactionType.REGULAR]: TRANSACTION_TYPE_REGULAR,
+  [api.TransactionDetails.TransactionType.TICKET_PURCHASE]: TRANSACTION_TYPE_TICKET_PURCHASE,
+  [api.TransactionDetails.TransactionType.VOTE]: TRANSACTION_TYPE_VOTE,
+  [api.TransactionDetails.TransactionType.REVOCATION]: TRANSACTION_TYPE_REVOCATION,
+  [api.TransactionDetails.TransactionType.COINBASE]: TRANSACTION_TYPE_COINBASE
 };
 
 export const TRANSACTION_DIR_SENT = "sent";
@@ -102,7 +98,7 @@ export function formatTransaction(block, transaction, index) {
   let creditAddresses = [];
   transaction.getCreditsList().forEach((credit) => creditAddresses.push(credit.getAddress()));
 
-  if (type === TransactionDetails.TransactionType.REGULAR) {
+  if (type === api.TransactionDetails.TransactionType.REGULAR) {
     if (amount > 0) {
       direction = TRANSACTION_DIR_RECEIVED;
     } else if (amount < 0 && (fee == Math.abs(amount))) {
@@ -139,7 +135,7 @@ export function formatUnminedTransaction(transaction, index) {
 export const streamGetTransactions = withLogNoData((walletService, startBlockHeight,
   endBlockHeight, targetTransactionCount, dataCb) =>
   new Promise((resolve, reject) => {
-    var request = new GetTransactionsRequest();
+    var request = new api.GetTransactionsRequest();
     request.setStartingBlockHeight(startBlockHeight);
     request.setEndingBlockHeight(endBlockHeight);
     request.setTargetTransactionCount(targetTransactionCount);
@@ -191,6 +187,6 @@ export const getTransactions = (walletService, startBlockHeight,
   });
 
 export const publishUnminedTransactions = log((walletService) => new Promise((resolve, reject) => {
-  const req = new PublishUnminedTransactionsRequest();
+  const req = new api.PublishUnminedTransactionsRequest();
   walletService.publishUnminedTransactions(req, (err) => err ? reject(err) : resolve());
 }), "Publish Unmined Transactions");
