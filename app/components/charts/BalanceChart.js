799e8bede0cd8931f02755484466b18ec4ba2e7e
Alex Yocom-Piatt <ay-p@decred.org>
2018-05-08 15:13:45 -0400

    First pass chart styling

--- app/components/charts/BalanceChart.js
+++ app/components/charts/BalanceChart.js
@@ -1,21 +1,18 @@
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
 import { injectIntl } from "react-intl";
 import messages from "./messages";
-import { yAxisStyle, xAxisStyle, homeChartSize, padding, radiusTop,
-  radiusMiddle, radiusBottom } from "./Styles";
+import { yAxisStyle, xAxisStyle, homeChartSize, padding, radiusTop, radiusBottom } from "./Styles";
 import ChartTooltip from "./ChartTooltip";
 
 const BalanceChart = ({ data, intl }) => {
 
   const availableKey = intl.formatMessage(messages.availableKey);
   const lockedKey = intl.formatMessage(messages.lockedKey);
-  const immatureKey = intl.formatMessage(messages.immatureKey);
 
   const displayData = data.map(s => ({
     name: intl.formatMessage(messages.dayMonthDisplay, { value: s.time }),
     legendName: intl.formatMessage(messages.fullDayDisplay, { value: s.time }),
     [lockedKey]: s.locked,
-    [immatureKey]: s.immature,
     [availableKey]: s.available,
   }));
 
@@ -25,7 +22,6 @@ const BalanceChart = ({ data, intl }) => {
       <YAxis orientation="right" style={xAxisStyle} padding={padding} />
       <Tooltip content={<ChartTooltip />} />
       <Bar barSize={8} dataKey={availableKey} stackId="a" fill="#2971ff" radius={radiusBottom} />
-      <Bar barSize={8} dataKey={immatureKey} stackId="a" fill="#69d5f7" radius={radiusMiddle} />
       <Bar barSize={8} dataKey={lockedKey} stackId="a" fill="#0c1e3e" radius={radiusTop} />
     </BarChart>
   );
