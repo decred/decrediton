241790e57d19bbb441b9c3623419d9eb5047a6fa
Alex Yocom-Piatt <ay-p@decred.org>
2018-05-08 16:37:54 -0400

    More chart styling

--- app/components/charts/BalanceChart.js
+++ app/components/charts/BalanceChart.js
@@ -1,7 +1,7 @@
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
 import { injectIntl } from "react-intl";
 import messages from "./messages";
-import { yAxisStyle, xAxisStyle, homeChartSize, padding, radiusTop, radiusBottom } from "./Styles";
+import { yAxisStyle, xAxisStyle, homeChartSize, padding, radiusTop, radiusBottom, hoverFill } from "./Styles";
 import ChartTooltip from "./ChartTooltip";
 
 const BalanceChart = ({ data, intl }) => {
@@ -12,17 +12,17 @@ const BalanceChart = ({ data, intl }) => {
   const displayData = data.map(s => ({
     name: intl.formatMessage(messages.dayMonthDisplay, { value: s.time }),
     legendName: intl.formatMessage(messages.fullDayDisplay, { value: s.time }),
-    [lockedKey]: s.locked,
     [availableKey]: s.available,
+    [lockedKey]: s.locked,
   }));
 
   return (
     <BarChart stackOffset="sign" width={homeChartSize.width} height={homeChartSize.height} data={displayData}>
-      <XAxis dataKey="name" style={yAxisStyle} />
-      <YAxis orientation="right" style={xAxisStyle} padding={padding} />
-      <Tooltip content={<ChartTooltip />} />
-      <Bar barSize={8} dataKey={availableKey} stackId="a" fill="#2971ff" radius={radiusBottom} />
-      <Bar barSize={8} dataKey={lockedKey} stackId="a" fill="#0c1e3e" radius={radiusTop} />
+      <XAxis tickLine={false} dataKey="name" style={yAxisStyle} />
+      <YAxis tickLine={false} orientation="right" style={xAxisStyle} padding={padding} />
+      <Tooltip cursor={hoverFill} content={<ChartTooltip />} />
+      <Bar barSize={8} dataKey={lockedKey} stackId="a" fill="#0c1e3e" radius={radiusBottom} />
+      <Bar barSize={8} dataKey={availableKey} stackId="a" fill="#2971ff" radius={radiusTop} />
     </BarChart>
   );
 };
