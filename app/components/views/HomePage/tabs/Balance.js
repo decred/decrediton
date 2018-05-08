91997b52eb3433dae49a8f529a229c4d2f4762c6
Alex Yocom-Piatt <ay-p@decred.org>
2018-05-08 14:44:12 -0400

    Overview tab value styling

--- app/components/views/HomePage/tabs/Balance.js
+++ app/components/views/HomePage/tabs/Balance.js
@@ -13,21 +13,19 @@ const HomePage = ({
   return (
     <div className="overview-content-wrapper">
       <div className="overview-spendable-locked-wrapper">
-        <div className="overview-spendable-locked">
-          <Balance
-            classNameWrapper="overview-balance-spendable-locked"
-            classNameUnit="overview-balance-spendable-locked-unit"
-            amount={spendableTotalBalance} />
-          <div className="overview-balance-spendable-locked-label">
-            <T id="home.currentTotalSpendableBalanceLabel" m="Available" />
-          </div>
-          <Balance
-            classNameWrapper="overview-balance-spendable-locked"
-            classNameUnit="overview-balance-spendable-locked-unit"
-            amount={lockedTotalBalance} />
-          <div className="overview-balance-spendable-locked-label">
-            <T id="home.currentTotalLockedBalanceLabel" m="Locked" />
-          </div>
+        <Balance
+          classNameWrapper="overview-balance-spendable-locked"
+          classNameUnit="overview-balance-spendable-locked-unit"
+          amount={spendableTotalBalance} />
+        <div className="overview-balance-spendable-locked-label">
+          <T id="home.currentTotalSpendableBalanceLabel" m="Available" />
+        </div>
+        <Balance
+          classNameWrapper="overview-balance-spendable-locked"
+          classNameUnit="overview-balance-spendable-locked-unit"
+          amount={lockedTotalBalance} />
+        <div className="overview-balance-spendable-locked-label">
+          <T id="home.currentTotalLockedBalanceLabel" m="Locked" />
         </div>
       </div>
       <div className="overview-chart-wrapper">
