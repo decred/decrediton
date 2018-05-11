74630bb1b96e7b1764e6cbbbfe34c730c27c316a
Alex Yocom-Piatt <ay-p@decred.org>
2018-05-11 13:58:41 -0400

    styling for page

--- app/components/views/FatalErrorPage.js
+++ app/components/views/FatalErrorPage.js
@@ -9,17 +9,25 @@ class FatalErrorPage extends React.Component {
     const { daemonError, walletError, shutdownApp } = this.props;
     return (
       <div className="page-body getstarted">
-        <div className="getstarted loader">
-          <T id="fatal.header.title" m="Fatal error" />:
-          { daemonError &&
-            <div className="shutdown-text"><T id="fatal.daemon.title" m="Daemon Error" /> {daemonError}</div>
-          }
-          { walletError &&
-            <div className="shutdown-text"><T id="fatal.wallet.title" m="Wallet Error" /> {walletError}</div>
-          }
-          <KeyBlueButton onClick={shutdownApp}>
-            <T id="fatal.button" m="Close Wallet"/>
-          </KeyBlueButton>
+        <div className="fatal-error-page">
+          <div className="fatal-error-title"><T id="fatal.header.title" m="Fatal error" />:</div>
+          <div className="fatal-error-area">
+            { daemonError &&
+              <div className="fatal-error"><T id="fatal.daemon.title" m="Daemon Error" /> {daemonError}</div>
+            }
+            { walletError &&
+              <div className="fatal-error"><T id="fatal.wallet.title" m="Wallet Error" /> {walletError}</div>
+            }
+          </div>
+          <div className="fatal-error-title"><T id="fatal.suggestion.title" m="Suggested action to resolve error" />:</div>
+          <div className="fatal-error-area">
+            <div className="fatal-error-suggestion"><T id="fatal.suggestion.value" m="This error typically means you have another instance of daemon running.  You should check your taskmanager or profiler to shutdown any still running daemon and then try again." /></div>
+          </div>
+          <div className="fatal-error-toolbar">
+            <KeyBlueButton onClick={shutdownApp}>
+              <T id="fatal.button" m="Close Wallet"/>
+            </KeyBlueButton>
+          </div>
         </div>
       </div>
     );
