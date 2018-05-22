d67b59031f1f1a8d29f7af75abf5e16a7bda5883
Matheus Degiovani <opensource@matheusd.com>
2018-05-22 15:28:42 -0300

    Improving layout

--- app/components/views/GetStartedPage/StakePools/Form.js
+++ app/components/views/GetStartedPage/StakePools/Form.js
@@ -23,64 +23,63 @@ const Form = ({
   onChangePassPhrase,
   onContinueCreation,
 }) => (
-  <Aux>
-    <div className="advanced-page-form">
-      <div className="advanced-daemon-row">
-        <div className="stakepool-add-area-left">
-          <div className="stakepool-field">
-            <div className="stakepool-field-label">
-              <T id="stakepool.label" m="Stakepool" />:
-            </div>
-            <div className="stakepool-field-value">
-              <StakePoolSelect
-                options={unconfiguredStakePools}
-                value={selectedUnconfigured}
-                onChange={onChangeSelectedUnconfigured}
-              />
-            </div>
+  <div className="">
+    <div className="getstarted-stakepool-add-row">
+      <div className="getstarted-stakepool-add-col left">
+        <div className="field">
+          <div className="label">
+            <T id="getStarted.stakepool.label" m="Stakepool" />:
           </div>
-          <div className="stakepool-field">
-            <div className="stakepool-field-label">
-              <T id="stakepool.apikey" m="API Key" />:
-            </div>
-            <div className="stakepool-field-value">
-              <TextInput
-                type="text"
-                className="stakepool-add-apikey"
-                placeholder={intl.formatMessage(messages.apiKeyPlaceholder)}
-                value={apiKey}
-                onChange={e => onChangeApiKey(e.target.value)}
-              />
-            </div>
-            <div className="stakepool-field-value-error">
-              {apiKey ? null : <T id="stake.addPool.errors.noApiKey" m="*Please enter your API key" /> }
-            </div>
+          <div className="value">
+            <StakePoolSelect
+              options={unconfiguredStakePools}
+              value={selectedUnconfigured}
+              onChange={onChangeSelectedUnconfigured}
+            />
           </div>
-          { walletPrivatePassphrase ? null :
-            <div className="stakepool-field">
-              <div className="stakepool-field-label">
-                <T id="stakepool.passPhrase" m="Private Passphrase" />:
-              </div>
-              <div className="stakepool-field-value">
-                <PasswordInput
-                  value={passPhrase}
-                  onChange={e => onChangePassPhrase(e.target.value)}
-                />
-              </div>
-            </div> }
-          <KeyBlueButton onClick={onSetStakePoolInfo} loading={isSavingStakePoolConfig} disabled={isSavingStakePoolConfig}>
-            <T id="getStarted.stakePools.addBtn" m="Add" />
-          </KeyBlueButton>
-          <InvisibleButton onClick={onContinueCreation}>
-            <T id="getStarted.stakePools.continueBtn" m="Continue" />
-          </InvisibleButton>
         </div>
+        <div className="field">
+          <div className="label">
+            <T id="stakepool.apikey" m="API Key" />:
+          </div>
+          <div className="value">
+            <TextInput
+              type="text"
+              className="stakepool-add-apikey"
+              placeholder={intl.formatMessage(messages.apiKeyPlaceholder)}
+              value={apiKey}
+              onChange={e => onChangeApiKey(e.target.value)}
+            />
+          </div>
+        </div>
+        { walletPrivatePassphrase ? null :
+          <div className="field">
+            <div className="label">
+              <T id="stakepool.passPhrase" m="Private Passphrase" />:
+            </div>
+            <div className="value">
+              <PasswordInput
+                value={passPhrase}
+                onChange={e => onChangePassPhrase(e.target.value)}
+              />
+            </div>
+          </div> }
       </div>
-      <div className="stakepool-add-area-right">
-        <T id="getStarted.stakePools.info" m="Add your existing stakepools APIs here. You can always add them later if you want." />
+
+      <div className="getstarted-stakepool-add-col right">
+        <T id="getStarted.stakePools.info" m="Add your existing stakepools APIs here. You can always add them later if you want. After you're done, you can press 'continue'." />
       </div>
     </div>
-  </Aux>
+
+    <div className="getstarted-stakepool-add-buttons">
+      <KeyBlueButton onClick={onSetStakePoolInfo} loading={isSavingStakePoolConfig} disabled={isSavingStakePoolConfig}>
+        <T id="getStarted.stakePools.addBtn" m="Add" />
+      </KeyBlueButton>
+      <InvisibleButton onClick={onContinueCreation}>
+        <T id="getStarted.stakePools.continueBtn" m="Continue" />
+      </InvisibleButton>
+    </div>
+  </div>
 );
 
 export default injectIntl(Form);
