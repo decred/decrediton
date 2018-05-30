71b35100b0af4c12e62575ed503bbd163f188fc5
Alex Yocom-Piatt <ay-p@decred.org>
2018-05-30 17:01:11 -0400

    getstarted modal work

--- app/containers/GetStarted.js
+++ app/containers/GetStarted.js
@@ -3,13 +3,16 @@ import GetStartedPosition from "components/views/GetStartedPosition";
 import LanguageSelectPage from "components/views/GetStartedPage/LanguageSelectPage";
 import TutorialPage from "components/views/GetStartedPage/TutorialPage";
 import PrivacyPage from "components/views/GetStartedPage/PrivacyPage";
+import { BlurableContainer } from "layout";
 import { Route, Switch } from "react-router-dom";
 
 export default () =>
-  <Switch>
-    <Route path="/getstarted/language"  component={LanguageSelectPage} />
-    <Route path="/getstarted/tutorial"  component={TutorialPage} />
-    <Route path="/getstarted/privacy"   component={PrivacyPage} />
-    <Route path="/getstarted/initial"   component={GetStartedPage} />
-    <Route path="/getstarted"           component={GetStartedPosition} />
-  </Switch>;
+  <BlurableContainer className="getstarted-page-body">
+    <Switch>
+      <Route path="/getstarted/language"  component={LanguageSelectPage} />
+      <Route path="/getstarted/tutorial"  component={TutorialPage} />
+      <Route path="/getstarted/privacy"   component={PrivacyPage} />
+      <Route path="/getstarted/initial"   component={GetStartedPage} />
+      <Route path="/getstarted"           component={GetStartedPosition} />
+    </Switch>
+  </BlurableContainer>;
