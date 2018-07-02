1ad4451ae3ec43e37f8e25c2235ee723e5630340
Alex Yocom-Piatt <ay-p@decred.org>
2018-07-02 13:47:41 -0400

    Fix recent transactions on overview when no tickets (#1516)

--- app/components/views/HomePage/tables/RecentTransactions.js
+++ app/components/views/HomePage/tables/RecentTransactions.js
@@ -8,14 +8,13 @@ import "style/HomePage.less";
 import NoTransactionsLinks from "./NoTransactionsLinks";
 
 const RecentTransactions = ({
-  tickets,
   transactions,
   getTransactionsRequestAttempt,
   getAccountsResponse,
   rowNumber,
   goToTransactionHistory
 }) => {
-  const hasTxs = (transactions.length > 0) && (tickets.length > 0);
+  const hasTxs = (transactions.length > 0);
   return (
     getTransactionsRequestAttempt ? <DecredLoading /> :
       <Aux>
@@ -30,9 +29,8 @@ const RecentTransactions = ({
           }
         </div>
         <div className="home-content-nest">
-          {transactions.length > 0 ? tickets.length > 0 ?
+          {transactions.length > 0 ?
             <TxHistory overview limit={rowNumber} {...{ getAccountsResponse, transactions }} /> :
-            <TxHistory limit={6} {...{ getAccountsResponse, transactions }} /> :
             <NoTransactionsLinks />}
         </div>
       </Aux>
