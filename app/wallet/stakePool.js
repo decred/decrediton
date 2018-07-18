2454fc23637167bd6d204ebe9b420771662e94e9
vctt94 <victor.duarte.oliveira@gmail.com>
2018-07-18 15:49:31 -0300

    promissify getStakePoolInfo

--- app/wallet/stakePool.js
+++ app/wallet/stakePool.js
@@ -10,13 +10,9 @@ const promisifyReqLogNoData = (fnName, Req) => withLogNoData( (...args) => new P
   Req(...args, (res, err) => err ? fail(err) : ok(res))
 ), fnName);
 
-export const getStakePoolInfo = withLogNoData(() =>
-  new Promise((resolve, reject) =>
-    api.stakePoolInfo((response, error) => !response ? reject(error) : resolve(response))),
-"Get Stakepool Info");
-
 export const getPurchaseInfo = promisifyReq("getPurchaseInfo", api.getPurchaseInfo);
 export const setStakePoolAddress = promisifyReq("setStakePoolAddress", api.setStakePoolAddress);
 export const setVoteChoices = promisifyReq("setVoteChoices", api.setVoteChoices)
 export const getAllStakePoolStats = promisifyReqLogNoData("getAllStakePoolStats", api.allStakePoolStats)
 export const getStakePoolStats = promisifyReqLogNoData("getStakePoolStats", api.statsFromStakePool)
+export const getStakePoolInfo = promisifyReqLogNoData("getStakePoolInfo", api.stakePoolInfo)
