9975ac4ca8e09ad459a1a5c233351cd3db850917
ਜੁਝਾਰ ਸਿੰਘ <jujhar.pannu@gmail.com>
2018-07-20 14:24:24 -0400

    Paid typofix (#1544)

--- app/components/views/TransactionsPage/ExportTab/messages.js
+++ app/components/views/TransactionsPage/ExportTab/messages.js
@@ -9,7 +9,7 @@ export default {
       { name: "hash", description: <T id="export.transactions.fields.hash" m="Hash of the transaction (txid)" /> },
       { name: "type", description: <T id="export.transactions.fields.type" m="Detected transaction type (regular, ticket purchase, vote, etc)" /> },
       { name: "direction", description: <T id="export.transactions.fields.direction" m="Detected direction of a regular transaction in relation to the wallet (send/receive/transfer)" /> },
-      { name: "fee", description: <T id="export.transactions.fields.fee" m="Fee payed by the wallet for the transaction" /> },
+      { name: "fee", description: <T id="export.transactions.fields.fee" m="Fee paid by the wallet for the transaction" /> },
       { name: "amount", description: <T id="export.transactions.fields.amount" m="Net amount sent (including fees) or received by the wallet on this transaction." /> },
       { name: "credits", description: <T id="export.transactions.fields.credits" m="Sum of credits (transaction outputs) of the wallet included in this transaction." /> },
       { name: "debits", description: <T id="export.transactions.fields.debits" m="Sum of debits (transaction inputs) of the wallet included in this transaction." /> },
