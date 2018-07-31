a1ab5acad045ded45af632a6fa799571e621f32c
vctt94 <victor.duarte.oliveira@gmail.com>
2018-07-31 17:36:31 -0300

    Add modal overlay

--- app/components/PseudoModal/WatchingOnlyWarn.js
+++ app/components/PseudoModal/WatchingOnlyWarn.js
@@ -1,11 +1,14 @@
 const WatchingOnlyWarnModal = () => (
-  <div className={"pseudo-modal info-modal"}>
-    <div className={"watch-only-modal-title"}>This function is disabled for watch-only Wallets</div>
-    <div className={"watch-only-modal-content"}>
-      In order to gain full access to your Wallets functionalities,
-      please re-create your Wallet with the watch-only option disabled.
+  <Aux>
+    <div className={"pseudo-modal-overlay"}></div>
+    <div className={"pseudo-modal info-modal"}>
+      <div className={"watch-only-modal-title"}>This function is disabled for watch-only Wallets</div>
+      <div className={"watch-only-modal-content"}>
+        In order to gain full access to your Wallets functionalities,
+        please re-create your Wallet with the watch-only option disabled.
+      </div>
     </div>
-  </div>
+  </Aux>
 );
 
 export default WatchingOnlyWarnModal;
