c44415480568704ca9afc3784a596d7c0a57ed80
Alex Yocom-Piatt <ay-p@decred.org>
2018-08-07 14:04:30 -0400

    typos

--- app/components/views/GetStartedPage/ReleaseNotes/Form.js
+++ app/components/views/GetStartedPage/ReleaseNotes/Form.js
@@ -51,24 +51,24 @@ export default ({
           <p>
             While it's hard to give details of speed increases due to variances in systems,
             we've seen roughly a 5x increase in syncing a previously used seed to tip on
-            mainnet.  Previously syncing the chain would take roughly 1hr plus another 5-10
-            mins for the wallet to become synced, now we're seeing about 20mins total time
+            mainnet.  Previously, syncing the chain would take roughly 1hr plus another 5-10
+            mins for the wallet to become synced. Now, we're seeing about 20mins total time
             to being able to fully use a wallet to send or purchase tickets.
           </p>
           <p>
             In the coming releases, we are going to work on further speeding this process up,
-            as well as providing more feedback from dcrwallet to Decrediton to make a more
-            rich user experience.
+            as well as providing more feedback from dcrwallet to Decrediton to make a richer
+            user experience.
           </p>
           <p>
             *Note: Automatic ticket purchasing has been disabled for SPV.  We will be
             implementing a way to start and stop the new v2 ticketbuyer in dcrwallet.  This
-            new version is much more simple and really only has 1 option to set which would
+            new version is much simpler and really only has 1 option to set which would
             be an absolute balance to maintain.  This new version will also allow users to
             run multiple ticket purchases for each account.
           </p>
           <p>
-            This release also has been audited by our design team at Eeter LLC.  Most of the
+            This release also has been audited by our design team at Eeter, LLC.  Most of the
             pages have received an update to the styling to add more polish and be more
             resilient to various sizing and displays.  In the near future, we will be
             focusing on color themes and window sizing for small, medium and large displays.
@@ -78,13 +78,13 @@ export default ({
             While this functionality is still in beta-testing, what you see will be roughly
             the final form.  Proposals up for vote will be shown and your possible tickets
             will be compared to the proposal's allowed tickets to vote.  If there are
-            tickets available to vote, you simply choose your choice, enter your passphrase
+            tickets available to vote, you simply make your choice, enter your passphrase
             and your tickets are used to send the Politeia server your cryptographic proof
             of ownership.
           </p>
           New Features
           <ul>
-            <li>SPV integration - *Note: This integration is currently headen from normal
+            <li>SPV integration - *Note: This integration is currently hidden from normal
             usage, if you would like to test it, please set `"spv_mode": true`, in your
             `config.json` file.  With this new integration, there has been a new way of
             syncing that has been added.  Now instead of needing to do all of the wallet
@@ -100,10 +100,10 @@ export default ({
             support for those that choose to have the extra security.  These were both
             necessary since the addition of Politeia wallet's communicating with the outside
             world.  We felt the users should have full control over where the wallet is
-            communicating.  This support will be further improved upon in further releases
+            communicating.  This support will be further improved upon in future releases
             and requests for things are made.</li>
 
-            <li>Initial Politeia Integration - *Note: This integration is currently headen
+            <li>Initial Politeia Integration - *Note: This integration is currently hidden
             from normal usage, but can be accessed by setting `"politeia_beta": true`, in
             your `config.json` file.  When activated the user is shown a new page on the
             side bar menu, reading "Governance."  This will show them the Politeia
@@ -125,11 +125,10 @@ export default ({
             feel of the input errors and believe they are not too intrusive for users while
             being very clear of what needs to be corrected.</li>
 
-            <li>* Max Wallet Option - Users are now able to set the max number of wallets are
-            shown on the launcher screen.  The default is set to 3, but users can alter that
-            to whatever whole number they'd like.  But keep in mind that it was styled
-            assuming only 3 would be shown, so there may be some distortions as that number
-            grows.  </li>
+            <li>* Max Wallet Option - Users are now able to set the max wallets that are
+            shown on the launcher screen.  The default is currentlyset to 3.  But keep in mind
+            that it was styled assuming only 3 would be shown, so there may be some distortions
+            as that number grows.  </li>
 
             <li>* Refined About Modal - The additional window was removed in favor of a React
             modal.  This allowed us to clean up the code and remove a redundant menu bar
