8277277126b3dc5be270a89695baa0b0f5c6ef09
vctt94 <victor.duarte.oliveira@gmail.com>
2018-08-07 17:41:13 -0300

    add watching only icon on sidebar

--- app/components/SideBar/Logo.js
+++ app/components/SideBar/Logo.js
@@ -1,5 +1,11 @@
+import { Tooltip } from "shared";
+import { FormattedMessage as T } from "react-intl";
+
 const Logo = ({ isTestNet, expandSideBar, onReduceSideBar, onExpandSideBar  }) => (
-  <div className="sidebar-logo" onClick={!expandSideBar ? onExpandSideBar : null}>
+  <div className={expandSideBar ? "sidebar-logo" : "reduced-sidebar-logo"} onClick={!expandSideBar ? onExpandSideBar : null}>
+    <Tooltip text={<T id="createWallet.goBack" m="This is a watch-only wallet with limited functionality." /> }>
+      <div className="sidebar-watch-only-icon"/>
+    </Tooltip>
     <div className={!expandSideBar ? "hamburger" : isTestNet ? "testnet" : "mainnet"} />
     {expandSideBar &&
       <div className="sidebar-reduce-arrow" onClick={expandSideBar ? onReduceSideBar : null}>
