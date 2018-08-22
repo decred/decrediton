451b79467e99d83c11d97b620842223ba2d68e4e
VcTT <victor.duarte.oliveira@gmail.com>
2018-08-22 17:44:15 -0300

    [sidebar] fix sidebar watch only wallet icon always showing (#1609)

--- app/components/SideBar/Logo.js
+++ app/components/SideBar/Logo.js
@@ -1,11 +1,14 @@
 import { Tooltip } from "shared";
 import { FormattedMessage as T } from "react-intl";
 
-const Logo = ({ isTestNet, expandSideBar, onReduceSideBar, onExpandSideBar  }) => (
+const Logo = ({ isTestNet, expandSideBar, onReduceSideBar, onExpandSideBar, isWatchOnly, }) => (
   <div className={expandSideBar ? "sidebar-logo" : "reduced-sidebar-logo"} onClick={!expandSideBar ? onExpandSideBar : null}>
-    <Tooltip text={<T id="createWallet.goBack" m="This is a watch-only wallet with limited functionality." /> }>
-      <div className="sidebar-watch-only-icon"/>
-    </Tooltip>
+    {
+      isWatchOnly &&
+      <Tooltip text={<T id="createWallet.goBack" m="This is a watch-only wallet with limited functionality." /> }>
+        <div className="sidebar-watch-only-icon"/>
+      </Tooltip>
+    }
     <div className={!expandSideBar ? "hamburger" : isTestNet ? "testnet" : "mainnet"} />
     {expandSideBar &&
       <div className="sidebar-reduce-arrow" onClick={expandSideBar ? onReduceSideBar : null}>
