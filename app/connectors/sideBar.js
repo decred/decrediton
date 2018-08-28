19ad33375e9c1cefceec20a82512fe72e30c3a47
VcTT <victor.duarte.oliveira@gmail.com>
2018-08-28 18:48:39 -0300

    [sidebar] Fix Watch only logo (#1625)

--- app/connectors/sideBar.js
+++ app/connectors/sideBar.js
@@ -14,7 +14,7 @@ const mapStateToProps = selectorMap({
   showingSidebar: sel.showingSidebar,
   showingSidebarMenu: sel.showingSidebarMenu,
   expandSideBar: sel.expandSideBar,
-  isWatchOnly: sel.isWatchOnly,
+  isWatchOnly: sel.isWatchingOnly,
   tsDate: sel.tsDate,
 });
 
