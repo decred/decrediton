3f5f5ef5dce3d8807aa76941bac6d134d8374add
Alex Yocom-Piatt <ay-p@decred.org>
2018-09-17 15:10:53 -0400

    Rename links tab

--- app/components/views/HelpPage/index.js
+++ app/components/views/HelpPage/index.js
@@ -15,7 +15,7 @@ const HelpPageHeader = () =>
 const HelpPage = () =>
   <TabbedPage header={<HelpPageHeader />}>
     <Switch><Redirect from="/help" exact to="/help/links" /></Switch>
-    <Tab path="/help/links" component={LinksTab} header={LinksTabHeader} link={<T id="help.tab.links" m="Links"/>}/>
+    <Tab path="/help/links" component={LinksTab} header={LinksTabHeader} link={<T id="help.tab.sources" m="Sources"/>}/>
     <Tab path="/help/tutorials" component={TutorialsTab} header={TutorialsTabHeader} link={<T id="help.tab.tutorials" m="Tutorials"/>}/>
     <Tab path="/help/logs" component={LogsTab} header={LogsTabHeader} link={<T id="help.tab.logs" m="Logs"/>}/>
   </TabbedPage>;
