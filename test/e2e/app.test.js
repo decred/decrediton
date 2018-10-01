d355932d3fe431f02b3050c614076fefa2e1b5ad
vctt94 <victor.duarte.oliveira@gmail.com>
2018-10-01 17:20:02 -0300

    Add send Transaction test

--- test/e2e/app.test.js
+++ test/e2e/app.test.js
@@ -2,6 +2,12 @@ const assert = require('assert')
 const electron = require('electron')
 const path = require('path')
 const webdriverio = require('webdriverio')
+import {
+  WALLET_LAUNCH_BUTTON, WALLET_OVERVIEW_HEADER, WALLET_LAUNCH_PAGE, TRANSACTIONS_MENU_TAB,
+  SEND_TRANSACTION_ADDRESS_INPUT, TESTNET_ADDRESS, SEND_TRANSACTION_AMOUNT_INPUT, AMOUNT_TO_SEND,
+  SNACKBAR_SEND, PASSPHRASE_MODAL_INPUT, SEND_TRANSACTION_SEND_BUTTON, PASSPHRASE_TEST_ACCOUNT,
+  PASSPHRASE_MODAL_CONTINUE_BUTTON,
+} from "./pathConsts";
 
 let app, started, crashed
 
@@ -23,9 +29,9 @@ function launch(t) {
       }
       app = webdriverio.remote(options)
 
-      let initialElement = ".page-body"
-      await startApp(app, initialElement)
-      resolve(app)
+      let initialElement = WALLET_LAUNCH_PAGE;
+      await startApp(app, initialElement);
+      resolve(app);
 
     })
   }
@@ -43,15 +49,38 @@ describe('Application launch', async () => {
   beforeAll(async () => {
     await launch();
   })
-
   // afterAll(() => app.end())
 
   it('Sync Decrediton', async () => {
     await sleep(5000);
-    await app.click('//*[@id="root"]/div[1]/div/div/div/div/div[5]/div/div/div[1]/div[5]/span')
-    await sleep(10000);
-    const isLoaded = await app.getHTML('.overview-header');
+    app.click(WALLET_LAUNCH_BUTTON)
+    await sleep(15000);
+    const isLoaded = await app.getHTML(WALLET_OVERVIEW_HEADER);
     assert.ok(isLoaded);
-  }, 20000)
+  }, 21000)
+})
 
+describe("Send Transaction" , async () => {
+  it('Send Transaction', async () => {
+    await sleep(1000);
+    await app.click(TRANSACTIONS_MENU_TAB);
+    await sleep(1000);
+    app.setValue(SEND_TRANSACTION_ADDRESS_INPUT, TESTNET_ADDRESS);
+    app.setValue(SEND_TRANSACTION_AMOUNT_INPUT, AMOUNT_TO_SEND);
+    await sleep(1000);
+    app.click(SEND_TRANSACTION_SEND_BUTTON);
+    await sleep(1000);
+    app.setValue(PASSPHRASE_MODAL_INPUT, PASSPHRASE_TEST_ACCOUNT);
+    await sleep(1000);
+    app.click(PASSPHRASE_MODAL_CONTINUE_BUTTON);
+    await sleep(3000);
+    const isSent = await app.getHTML(SNACKBAR_SEND);
+    assert.ok(isSent);
+  }, 10000)
+})
+
+describe("Shutdown" , async () => {
+  it('Shutdown', async () => {
+    app.close();
+  })
 })
---
+++ test/e2e/pathConsts.js
@@ -0,0 +1,18 @@
+// Start Wallet Consts
+export const WALLET_LAUNCH_BUTTON = ".display-wallet-launch";
+export const WALLET_OVERVIEW_HEADER = ".overview-header";
+export const WALLET_LAUNCH_PAGE = ".page-body";
+
+// Menu links Consts
+export const TRANSACTIONS_MENU_TAB = ".transactionsIcon";
+
+// Send transaction information
+export const TESTNET_ADDRESS = "TsfDLrRkk9ciUuwfp2b8PawwnukYD7yAjGd";
+export const SEND_TRANSACTION_ADDRESS_INPUT = '//*[@id="root"]/div[1]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/div/div/div/div/div[2]/div[1]/div[1]/input';
+export const AMOUNT_TO_SEND = 0.001;
+export const SEND_TRANSACTION_AMOUNT_INPUT = '//*[@id="root"]/div[1]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/div/div/div/div/div[3]/div[2]/div[1]/input';
+export const SEND_TRANSACTION_SEND_BUTTON = '//*[@id="root"]/div[1]/div/div/div[2]/div/div/div/div[2]/div/div/div[3]/div[1]'
+export const PASSPHRASE_TEST_ACCOUNT = '12345678';
+export const PASSPHRASE_MODAL_INPUT = '//*[@id="modal-portal"]/div[2]/div[2]/div/div[2]/div/input';
+export const PASSPHRASE_MODAL_CONTINUE_BUTTON = '.passphrase-modal-save-button';
+export const SNACKBAR_SEND = '.snackbar-send'
