6544123f5f82adad6a2b84caa048df908f70bc13
Matheus Degiovani <opensource@matheusd.com>
2018-10-03 17:16:25 -0300

    [i18n] Update master translation files (#1686)

--- app/components/PseudoModal/WatchingOnlyWarn.js
+++ app/components/PseudoModal/WatchingOnlyWarn.js
@@ -1,11 +1,17 @@
+import { FormattedMessage as T } from "react-intl";
+
 const WatchingOnlyWarnModal = () => (
   <Aux>
     <div className={"pseudo-modal-overlay"}></div>
     <div className={"pseudo-modal info-modal"}>
-      <div className={"watch-only-modal-title"}>This function is disabled for watch-only Wallets</div>
+      <div className={"watch-only-modal-title"}>
+        <T id="watchingOnlyPseudoModal.title" m="This function is disabled for watch-only Wallets" />
+      </div>
       <div className={"watch-only-modal-content"}>
-        In order to gain full access to your Wallets functionalities,
-        please re-create your Wallet with the watch-only option disabled.
+        <T id="watchingOnlyPseudoModal.description" m={`
+          In order to gain full access to your Wallets functionalities,
+          please re-create your Wallet with the watch-only option disabled.
+        `}/>
       </div>
     </div>
   </Aux>
