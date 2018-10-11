879e0927d3f47abf5c9b69831f300ca03f19c42d
Matheus Degiovani <opensource@matheusd.com>
2018-10-11 16:59:39 -0300

    [i18n] Update for 1.3.1 (#1708)

--- app/i18n/docs/index.js
+++ app/i18n/docs/index.js
@@ -1,6 +1,11 @@
+import * as de from "./de";
 import * as en from "./en";
+import * as es from "./es";
+import * as fr from "./fr";
+import * as ja from "./ja";
 import * as pt from "./pt";
+import * as zh from "./zh";
 
-const Docs = { en, pt };
+const Docs = { de, en, es, fr, ja, pt, zh };
 
 export default Docs;
