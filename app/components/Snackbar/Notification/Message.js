694bc2af3df75b0d6a82b5025039a84624dfaec2
Alex Yocom-Piatt <ay-p@decred.org>
2018-10-11 14:53:01 -0400

    [notifications] Update design for notifications (#1702)

--- app/components/Snackbar/Notification/Message.js
+++ app/components/Snackbar/Notification/Message.js
@@ -4,10 +4,11 @@ import { injectIntl } from "react-intl";
 @autobind
 class Message extends React.Component {
   render() {
-    const { intl, message, values } = this.props;
+    const { intl, message, values, onDismissMessage } = this.props;
     const txt = intl.formatMessage(message, values);
     return (
       <div className="snackbar-message">
+        <div className="snackbar-close-button-top" onClick={onDismissMessage}/>
         {txt}
       </div>
     );
