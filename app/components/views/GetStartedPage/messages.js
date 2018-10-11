6c52d7bd8cf0793af94fe60b32cfc86388de34bb
Matheus Degiovani <opensource@matheusd.com>
2018-10-11 16:46:00 -0300

    I18n update for 1.3.1

--- app/components/views/GetStartedPage/messages.js
+++ app/components/views/GetStartedPage/messages.js
@@ -1,4 +1,4 @@
-import { FormattedMessage as T, defineMessages } from "react-intl";
+import { FormattedMessage as T } from "react-intl";
 import { ExternalLink, Tooltip } from "shared";
 import { AboutModalButtonInvisible } from "buttons";
 
@@ -9,25 +9,25 @@ export const BackBtnMsg = () => <T id="getStarted.backBtn" m="Cancel" />;
 export const GoBackMsg = () => <T id="logs.goBack" m="Go back" />;
 export const CreateWalletTitleMsg = () => <T id="createWallet.title" m={"Create a new wallet"}/>;
 export const ConfirmSeedMsg = () => <T id="confirmSeed.label" m="Confirm Seed" />;
-export const DiscoverLabelMsg = () => <T id="getStarted.discover.label" m="Scan for accounts" />;
+// export const DiscoverLabelMsg = () => <T id="getStarted.discover.label" m="Scan for accounts" />;
 export const LoaderTitleMsg = () => <T id="loader.title" m={"Welcome to Decrediton Wallet"}/>;
-export const DiscoverAccountsInfoMsg = () =>
-  <T id="getStarted.discoverAccountsInfo" m={`
-  Enter the passphrase you just created to scan the blockchain for additional accounts you may have previously created with your wallet.
+// export const DiscoverAccountsInfoMsg = () =>
+//   <T id="getStarted.discoverAccountsInfo" m={`
+//   Enter the passphrase you just created to scan the blockchain for additional accounts you may have previously created with your wallet.
 
-  Your account names aren't stored on the blockchain, so you will have to rename them after setting up Decrediton.
-  `}/>;
-export const ScanBtnMsg = () => <T id="getStarted.discoverAddresses.scanBtn" m="Scan" />;
+//   Your account names aren't stored on the blockchain, so you will have to rename them after setting up Decrediton.
+//   `}/>;
+// export const ScanBtnMsg = () => <T id="getStarted.discoverAddresses.scanBtn" m="Scan" />;
 export const LearnBasicsMsg = () => <T id="getStarted.learnBasics" m="Learn the Basics" />;
 export const NewSeedTabMsg = () => <T id="getStarted.newSeedTab" m="Create a New Wallet"/>;
 export const RestoreTabMsg = () => <T id="getStarted.restore" m="Restore Existing Wallet"/>;
 
-export const messages = defineMessages({
-  passphrasePlaceholder: {
-    id: "getStarted.discoverAddresses.passphrasePlaceholder",
-    defaultMessage: "Private Passphrase"
-  }
-});
+// export const messages = defineMessages({
+//   passphrasePlaceholder: {
+//     id: "getStarted.discoverAddresses.passphrasePlaceholder",
+//     defaultMessage: "Private Passphrase"
+//   }
+// });
 
 export const UpdateAvailableLink = ({ updateAvailable }) => (
   <Tooltip text={<T id="getStarted.updateAvailableTooltip" m="New version {version} available" values={{ version: (updateAvailable) }}/>}>
---
+++ app/i18n/docs/zh/index.js
@@ -0,0 +1,2 @@
+export { default as SeedCopyWarning } from "./Warnings/SeedCopy.md";
+export { default as WalletCreationWarning } from "./Warnings/WalletCreation.md";
