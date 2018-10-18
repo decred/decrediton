c0263e48f0c0d40669c4b0e9f9dde5925b8a0578
Jujhar Singh <jujhar.pannu@gmail.com>
2018-10-18 14:24:56 -0700

    -u more allusive passphrase update failed message (pt 2)

--- app/reducers/snackbar.js
+++ app/reducers/snackbar.js
@@ -156,7 +156,7 @@ const messages = defineMessages({
   },
   CHANGEPASSPHRASE_FAILED: {
     id: "settings.errors.changePassphraseFailed",
-    defaultMessage: "Entered incorrect private passphrase reset blocked."
+    defaultMessage: "Update passphrase failed. Incorrect private passphrase, please try again."
   },
   DECODERAWTXS_FAILED: {
     id: "decodeRawTx.errors.decodeFailed",
