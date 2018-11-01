e3dec2bc67ec20438fb8b01cb59d216a4a3489f0
Alex Yocom-Piatt <ay-p@decred.org>
2018-11-01 12:50:32 -0400

    another copy tweak

--- app/components/views/GetStartedPage/SpvChoicePage/TopLevelOptions.js
+++ app/components/views/GetStartedPage/SpvChoicePage/TopLevelOptions.js
@@ -24,14 +24,14 @@ export default ({ enableSpv, disableSpv }) => (
       <PrivacyOption
         title={<T id="spv.options.enable.title" m="Enable SPV" />}
         icon="enable-spv"
-        description={<T id="spv.options.enable.description" m="SPV will allow your wallets to be restored and used much more quickly." />}
+        description={<T id="spv.options.enable.description" m="SPV will allow your wallets to be restored and used much more quickly.  This speed comes at cost, with blocks not being fully verified.  It's 'less secure' but very unlikely that there will be any problems." />}
         onClick={enableSpv}
       />
 
       <PrivacyOption
         title={<T id="spv.options.disable.title" m="Disable SPV" />}
         icon="disable-spv"
-        description={<T id="spv.options.disable.description" m="This will use the regular Decred Daemon and fully verify blocks.  This will take longer, but is more secure." />}
+        description={<T id="spv.options.disable.description" m="This will use the regular Decred Daemon and fully verify blocks.  This will take longer, but is fully secure.  Any block or mined transaction can be fully trusted." />}
         onClick={disableSpv}
       />
 
