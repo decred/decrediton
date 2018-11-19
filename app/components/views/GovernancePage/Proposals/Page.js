af60aa2a17e22934507500c5353aa389d5af52cb
VcTT <victor.duarte.oliveira@gmail.com>
2018-11-19 11:56:58 -0200

    [governance] Use tabbedPage at proposals (#1797)

--- app/components/views/GovernancePage/Proposals/Page.js
+++ app/components/views/GovernancePage/Proposals/Page.js
@@ -1,34 +1,27 @@
 import { FormattedMessage as T } from "react-intl";
 import { PoliteiaLink as PiLink } from "shared";
-import { Switch, Route, NavLink } from "react-router-dom";
 import { ActiveVoteProposals, PreVoteProposals, VotedProposals } from "./ProposalList";
 import { shell } from "electron";
+import { TabbedPage, TabbedPageTab as Tab } from "layout";
+
+const PageHeader = () => (
+  <div className="proposals-community-header">
+    <div className="proposals-community-header-title"><T id="proposals.community.title" m="Proposals"/></div>
+    <p className="proposals-community-header-description"><T id="proposals.community.descr" m="Voting on community proposals allows you to have a say on how the project treasury is spent.
+        Participation in voting requires (PoS) tickets. Proposal creation, discussions and other features are available at" /> <a onClick={() => shell.openExternal("https://proposals.decred.org")}>proposals.decred.org</a></p>
+
+    <div className="links">
+      <PiLink><T id="proposals.community.createLink" m="Create a Proposal" /></PiLink>
+    </div>
+  </div>
+);
 
 const Page = () => (
-  <Aux>
-    <div className="proposals-community-header">
-      <div className="proposals-community-header-title"><T id="proposals.community.title" m="Proposals"/></div>
-      <p className="proposals-community-header-description"><T id="proposals.community.descr" m="Voting on community proposals allows you to have a say on how the project treasury is spent.
-          Participation in voting requires (PoS) tickets. Proposal creation, discussions and other features are available at" /> <a onClick={() => shell.openExternal("https://proposals.decred.org")}>proposals.decred.org</a></p>
-
-      <div className="links">
-        <PiLink><T id="proposals.community.createLink" m="Create a Proposal" /></PiLink>
-      </div>
-    </div>
-
-    <div className="proposals-status-links">
-      <NavLink to="/governance/proposals/prevote"><T id="proposals.statusLinks.preVote" m="Under Discussion" /></NavLink>
-      <NavLink to="/governance/proposals/activevote"><T id="proposals.statusLinks.underVote" m="Under Vote" /></NavLink>
-      <NavLink to="/governance/proposals/voted"><T id="proposals.statusLinks.voted" m="Finished voting" /></NavLink>
-    </div>
-
-    <Switch>
-      <Route path="/governance/proposals/prevote" component={PreVoteProposals} />
-      <Route path="/governance/proposals/activevote" component={ActiveVoteProposals} />
-      <Route path="/governance/proposals/voted" component={VotedProposals} />
-    </Switch>
-
-  </Aux>
+  <TabbedPage header={<PageHeader />} >
+    <Tab path="/governance/proposals/prevote" component={PreVoteProposals} link={<T id="proposals.statusLinks.preVote" m="Under Discussion" />}/>
+    <Tab path="/governance/proposals/activevote" component={ActiveVoteProposals} link={<T id="proposals.statusLinks.underVote" m="Under Vote" />} />
+    <Tab path="/governance/proposals/voted" component={VotedProposals} link={<T id="proposals.statusLinks.voted" m="Finished voting" />}/>
+  </TabbedPage>
 );
 
 export default Page;
