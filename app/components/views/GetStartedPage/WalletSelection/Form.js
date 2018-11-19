814d3ebbca81568c8f011dd4d35ea0ed65b30a93
vctt94 <victor.duarte.oliveira@gmail.com>
2018-11-19 11:20:40 -0200

    Fix undefined selectedWallet

--- app/components/views/GetStartedPage/WalletSelection/Form.js
+++ app/components/views/GetStartedPage/WalletSelection/Form.js
@@ -28,7 +28,7 @@ const WalletSelectionBodyBase = ({
   ...props,
 }) => {
   return (
-    availableWallets.length > 0 && !createWalletForm ?
+    availableWallets.length > 0 && selectedWallet && !createWalletForm ?
       <div className="advanced-page">
         <div className="advanced-page-form">
           <div className="advanced-daemon-row">
