f69f9beef10c63612505a6da64129d76c2618f65
Alex Yocom-Piatt <ay-p@decred.org>
2018-11-26 11:35:35 -0500

    [error] Fix message about invalid RPC version (#1828)

--- app/components/views/InvalidRPCVersion.js
+++ app/components/views/InvalidRPCVersion.js
@@ -10,9 +10,7 @@ const InvalidRPCVersion = ({ requiredWalletRPCVersion, walletRPCVersion }) => (
         id="invalidRPCVersion.info"
         m={`The API of the currently running wallet ({walletRPCVersion}) is not compatible with Decrediton (required version {requiredWalletRPCVersion}).
 
-        Please update the daemon (dcrd) and wallet (dcrwallet) to the latest version, then try again.
-
-        See the "Help ⮕ About" menu for the current version of the executables.`}
+        Please update the daemon (dcrd) and wallet (dcrwallet) to the latest version, then try again.`}
         values={{ walletRPCVersion, requiredWalletRPCVersion }}
       />
     </div>
