2eefaa3302c5ef82bd7efc558f57da07405ab6fe
Alex Yocom-Piatt <ay-p@decred.org>
2018-12-04 15:03:37 -0500

    typo on indexOf check

--- app/components/views/FatalErrorPage.js
+++ app/components/views/FatalErrorPage.js
@@ -39,7 +39,7 @@ class FatalErrorPage extends React.Component {
               <T id="fatal.suggestion.resources" m="This error typically means you have another instance of daemon running.  You should check your taskmanager or profiler to shutdown any still running daemon and then try again." /> :
               daemonError && daemonError.indexOf(DIFF_CONNECTION_ERROR) !== -1 ?
                 <T id="fatal.suggestion.diffConnection" m="This error typically means you have the testnet flag in your dcrd.conf file. You should check your dcrd.conf file and remove the testnet=1." /> :
-                daemonError && (daemonError.indexOf(corruptedError) > 0  || daemonError.indexOf(checkSumError)) ?
+                daemonError && (daemonError.indexOf(corruptedError) > 0  || daemonError.indexOf(checkSumError) > 0) ?
                   <Aux>
                     <div className="fatal-error-reset-blockchain">
                       <T id="fatal.suggestion.corrupted" m="This error means your blockchain data has somehow become corrupted.  Typically, this is caused by a sector on the HDD/SDD that went bad and its built-in SMART didn't repair it, or the more likely case, there was a memory issue which corrupted the data.  To resolve, you must delete your blockchain data and re-download.  Press the button below to complete the process. When you restart Decrediton, it will automatically begin your blockchain download. Please come to our support channel on slack/matrix/discord/rocketchat to get advice about running disk utilities. " />
