516b3e032c28d4a25816f4111efaa93bafe42045
Guang <guang.dcr@gmail.com>
2018-12-13 22:14:39 +0800

    Add zh 1.4.0 release notes

--- app/i18n/docs/zh/index.js
+++ app/i18n/docs/zh/index.js
@@ -3,3 +3,6 @@ export { default as WalletCreationWarning } from "./Warnings/WalletCreation.md";
 export { default as TrezorWipeWarning } from "./Warnings/TrezorWipe.md";
 export { default as TrezorFirmwareUpdateWarning } from "./Warnings/TrezorFirmwareUpdate.md";
 export { default as TrezorWalletCreationPassPhraseWarning } from "./Warnings/TrezorWalletCreationPassPhrase.md";
+
+export { default as ReleaseNote1_4_0 } from "./ReleaseNotes/v1.4.0.md";
+
