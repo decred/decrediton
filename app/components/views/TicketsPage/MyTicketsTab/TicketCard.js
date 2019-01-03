fd5b67db3ade56330655ea3cdb4b623cdd51b1e3
VcTT <victor.duarte.oliveira@gmail.com>
2019-01-03 16:45:21 -0200

    [myTickets] Fix my tickets card not clickable in some areas (#1971)

--- app/components/views/TicketsPage/MyTicketsTab/TicketCard.js
+++ app/components/views/TicketsPage/MyTicketsTab/TicketCard.js
@@ -2,12 +2,12 @@ import TicketInfoCard from "./TicketInfoCard";
 import VisibilitySensor from "react-visibility-sensor";
 
 const Card = ({ isVisible, ...props }) => {
-  const { expanded, ticket } = props;
+  const { expanded, ticket, onClick } = props;
   const className = "ticket-info-card" + (expanded ? " is-expanded" : "") +
   " ticket-card ticket-" + ticket.status;
 
   return (
-    <div className={className}>
+    <div className={className} onClick={() => onClick(ticket)}>
       {isVisible ? <TicketInfoCard {...props} /> : null}
     </div>
   );
