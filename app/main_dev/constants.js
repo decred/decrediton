d45b6df896dd677e94b5938cd08e4fc0016fee8b
Szabolcs Pasztor <spqpad@gmail.com>
2019-01-11 21:16:36 +0100

    Update help message

--- app/main_dev/constants.js
+++ app/main_dev/constants.js
@@ -38,17 +38,23 @@ export const MIN_RELAY_FEE = 0.0001;
 
 export const USAGE_MESSAGE = app ? `${app.getName()} version ${app.getVersion()}
 Usage
-  $ ${app.getName()} [--help] [--version] [--debug] [--testnet|--mainnet]
-               [--extrawalletargs=...]
+  $ ${app.getName()} [OPTIONS]
 
 Options
-  --help             Show help and exit
-  --version          Show version and exit
-  --debug  -d        Debug daemon/wallet messages
-  --testnet          Connect to testnet
-  --mainnet          Connect to mainnet
-  --extrawalletargs  Pass extra arguments to dcrwallet
-  --customBinPath    Custom path for dcrd/dcrwallet/dcrctl binaries
+  --help -h          Show help and exit.
+  --version -v       Show version and exit.
+  --debug -d         Debug daemon/wallet messages.
+  --testnet          Connect to testnet.
+  --mainnet          Connect to mainnet.
+  --advanced         Start in advanced daemon mode.
+  --spv              Start in SPV mode (cannot be used at the same time as advanced daemon mode).
+  --spvconnect       Specify direct peer for SPV connection in 'host:port' or 'host' format (latter uses the default SPV port). Supports comma-separated list of peers. Always use with --spv.
+  --rpcuser          Specify RPC username for advanced daemon mode connection
+  --rpcpass          Specify RPC password
+  --rpccert          Specify RPC Certificate
+  --rpcconnect       Specify RPC connection in 'host:port' or 'host' format (latter uses the default RPC port). Note that different ports are used for RPC and SPV connections.
+  --extrawalletargs  Pass extra arguments to dcrwallet.
+  --custombinpath    Custom path for dcrd/dcrwallet/dcrctl binaries.
 ` : null;
 
 export const DIFF_CONNECTION_ERROR = "daemon and decrediton does not have the same network";
