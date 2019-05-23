5ba6315cb0752a7ea8a425ecd5377fc5014f2294
William Entriken <github.com@phor.net>
2019-05-23 11:20:18 -0400

    [typo] Github -> GitHub (#2117)

--- app/components/views/HelpPage/LinksTab/index.js
+++ app/components/views/HelpPage/LinksTab/index.js
@@ -13,7 +13,7 @@ export const LinksTab = () => (
   <>
     <div className="tabbed-page-subtitle"><T id="help.subtitle.project" m="Project Related"/></div>
     <div className="help-icons-list">
-      <HelpLink className={"help-github-icon"} href="https://github.com/decred/decrediton" title={<T id="help.github.title" m="Github"/>} subtitle={<T id="help.github.subtitle" m="github.com/decred/decrediton"/>} />
+      <HelpLink className={"help-github-icon"} href="https://github.com/decred/decrediton" title={<T id="help.github.title" m="GitHub"/>} subtitle={<T id="help.github.subtitle" m="github.com/decred/decrediton"/>} />
       <HelpLink className={"help-docs-icon"} href="https://docs.decred.org/" title={<T id="help.documentation" m="Documentation" />} subtitle={<T id="help.documentation.subtitle" m="docs.decred.org"/>}/>
       <HelpLink className={"help-stakepools-icon"} href="https://decred.org/stakepools" title={<T id="help.stakepools" m=" Stakepools" />} subtitle={<T id="help.stakepools.subtitle" m="decred.org/stakepools"/>}/>
       <HelpLink className={"help-blockchain-explorer-icon"} href="https://explorer.dcrdata.org" title={<T id="help.blockchain" m=" Blockchain Explorer" />} subtitle={<T id="help.blockchain.subtitle" m="explorer.dcrdata.org"/>}/>
