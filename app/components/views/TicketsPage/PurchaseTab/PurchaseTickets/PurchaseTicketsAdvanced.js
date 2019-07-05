9b2913974765410986ec86dfef0e9568e5285a16
vctt94 <victor.duarte.oliveira@gmail.com>
2019-07-05 15:00:23 -0300

    fix design when showing advanced purchase ticket info

--- app/components/views/TicketsPage/PurchaseTab/PurchaseTickets/PurchaseTicketsAdvanced.js
+++ app/components/views/TicketsPage/PurchaseTab/PurchaseTickets/PurchaseTicketsAdvanced.js
@@ -33,22 +33,35 @@ const PurchaseTicketsAdvanced = ({
   formatMessage,
   ...props,
 }) => (
-  <div className="purchase-ticket-advanced-info-area">
-    <div className="is-row">
-      <T id="purchaseTickets.stakePoolLabel" m="Stake Pool" />:
-      <StakePoolSelect
-        options={configuredStakePools}
-        value={stakePool}
-        onChange={onChangeStakePool}
-        className="stakepool-purchase-ticket-input-select"
-      />
-      <div className="manage-pools-button" onClick={onShowStakePoolConfig} />
+  <div className="is-row purchase-ticket-advanced-info-area">
+    <div className="purchase-ticket-advanced-labels">
+      <div className="stakepool-purchase-ticket-label">
+        <T id="purchaseTickets.stakePoolLabel" m="Stake Pool" />:
+      </div>
+      <div className="stakepool-purchase-ticket-label">
+        <T id="purchaseTickets.ticketFee" m="Ticket Fee" />:
+      </div>
+      <div className="stakepool-purchase-ticket-label">
+        <T id="purchaseTickets.advanced.ticketAddress" m="Ticket Address" />:
+      </div>
+      <div className="stakepool-purchase-ticket-label">
+        <T id="purchaseTickets.advanced.poolAddress" m="Pool Address" />:
+      </div>
+      <div className="stakepool-purchase-ticket-label">
+        <T id="purchaseTickets.poolFees" m="Pool Fees" />:
+      </div>
     </div>
-    <div className="is-row">
-      <div className="is-row purchase-advanced-input">
-        <div className="stakepool-purchase-ticket-label">
-          <T id="purchaseTickets.ticketFee" m="Ticket Fee" />:
-        </div>
+    <div className="purchase-ticket-advanced-inputs" >
+      <div className="is-row">
+        <StakePoolSelect
+          options={configuredStakePools}
+          value={stakePool}
+          onChange={onChangeStakePool}
+          className="stakepool-purchase-ticket-input stakepool-advanced-big-input"
+        />
+        <div className="manage-pools-button" onClick={onShowStakePoolConfig} />
+      </div>
+      <div className="is-row">
         <FeeInput {...{ ...props }}
           name={"ticketFee"}
           required
@@ -58,9 +71,8 @@ const PurchaseTicketsAdvanced = ({
           value={ticketFee}
           onChange={onChangeTicketFee}
           showErrors={true}
+          className="stakepool-purchase-ticket-input advanced-small-input"
         />
-      </div>
-      <div className="is-row purchase-advanced-input">
         <div className="stakepool-purchase-ticket-label">
           <T id="purchaseTickets.txFee" m="Tx Fee" />:
         </div>
@@ -72,9 +84,8 @@ const PurchaseTicketsAdvanced = ({
           value={txFee}
           onChange={onChangeTxFee}
           showErrors={true}
+          className="stakepool-purchase-ticket-input advanced-small-input"
         />
-      </div>
-      <div className="is-row purchase-advanced-input">
         <div className="stakepool-purchase-ticket-label">
           <T id="purchaseTickets.advanced.expiry" m="Expiry" />:
         </div>
@@ -86,36 +97,22 @@ const PurchaseTicketsAdvanced = ({
           value={expiry}
           onChange={onChangeExpiry}
           showErrors={true}
+          className="stakepool-purchase-ticket-input advanced-small-input"
         />
       </div>
-    </div>
-    <div className="is-row">
-      <div className="stakepool-purchase-ticket-label">
-        <T id="purchaseTickets.advanced.ticketAddress" m="Ticket Address" />:
-      </div>
       <AddressInput
         disabled readOnly
-        // className="stakepool-content-nest-purchase-ticket-form-disabled"
+        className="stakepool-purchase-ticket-input stakepool-advanced-big-input"
         value={stakePool ? stakePool.value.TicketAddress : null}
       />
-    </div>
-    <div className="is-row">
-      <div className="stakepool-purchase-ticket-label">
-        <T id="purchaseTickets.advanced.poolAddress" m="Pool Address" />:
-      </div>
       <AddressInput
         disabled readOnly
-        // className="stakepool-content-nest-purchase-ticket-form-disabled"
+        className="stakepool-purchase-ticket-input stakepool-advanced-big-input"
         value={stakePool ? stakePool.value.PoolAddress : null}
       />
-    </div>
-    <div className="is-row">
-      <div className="stakepool-purchase-ticket-label">
-        <T id="purchaseTickets.poolFees" m="Pool Fees" />:
-      </div>
       <PercentInput
         disabled readOnly
-        // className="stakepool-content-nest-purchase-ticket-form-disabled"
+        className="stakepool-purchase-ticket-input advanced-small-input"
         value={stakePool ? stakePool.value.PoolFees : null}
       />
     </div>
