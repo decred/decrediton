20bdf07b2f46319a18dd8841bdec9f21eb125701
David Hill <dhill@mindcry.org>
2019-07-11 16:09:20 -0400

    fix linter; use double quotes

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
 
   return {
     "presets": [
-      [ "@babel/preset-env", { targets: { node: 'current', } } ],
+      [ "@babel/preset-env", { targets: { node: "current", } } ],
       "@babel/preset-react",
       "@babel/preset-flow"
     ],
