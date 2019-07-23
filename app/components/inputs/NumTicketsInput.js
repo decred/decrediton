80cfb13703c8c5b9ff35011d7c61a23c2d80453c
vctt94 <victor.duarte.oliveira@gmail.com>
2019-07-23 19:54:37 -0300

    fix lint

--- app/components/inputs/NumTicketsInput.js
+++ app/components/inputs/NumTicketsInput.js
@@ -14,19 +14,19 @@ const NumTicketsInput = ({
 }) => {
   const ticketUnitLabel = numTickets === 1 ? <T id="numTicketInput.unit" m="ticket" /> : <T id="numTicketsInput.unit" m="tickets" />;
   return (
-   <div className={"is-row stakepool-purchase-ticket-num-select"} >
-     <IntegerInput
-       {...{ required, onKeyDown, showErrors, invalid, invalidMessage, 
-       value: numTickets }}
-       className="ticket-numeric-input"
-       onChange={e => onChangeNumTickets && onChangeNumTickets(e.target.value)}
-       data-max-width="70"
-       unit={ticketUnitLabel}
-     />
-     <div key="more" className="num-tickets-icon more" onClick={incrementNumTickets} />
-     <div key="less" className="num-tickets-icon less" onClick={decrementNumTickets} />
-   </div>
- );
-}
+    <div className={"is-row stakepool-purchase-ticket-num-select"} >
+      <IntegerInput
+        {...{ required, onKeyDown, showErrors, invalid, invalidMessage,
+          value: numTickets }}
+        className="ticket-numeric-input"
+        onChange={e => onChangeNumTickets && onChangeNumTickets(e.target.value)}
+        data-max-width="70"
+        unit={ticketUnitLabel}
+      />
+      <div key="more" className="num-tickets-icon more" onClick={incrementNumTickets} />
+      <div key="less" className="num-tickets-icon less" onClick={decrementNumTickets} />
+    </div>
+  );
+};
 
 export default NumTicketsInput;
