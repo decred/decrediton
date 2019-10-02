48f45a9d201c64164c675bbde02d30ea9958c0db
vctt94 <victor.duarte.oliveira@gmail.com>
2019-10-02 10:00:28 -0300

    review fix

--- app/components/buttons/EnableExternalRequestButton.js
+++ app/components/buttons/EnableExternalRequestButton.js
@@ -4,9 +4,9 @@ import KeyBlueButton from "./KeyBlueButton";
 const EnableExternalRequestButton = ({ children, requestType,
   onAddAllowedRequestType, onClick }) => {
 
-  const onClickFunction = () => onClick ? onClick() && onAddAllowedRequestType(requestType) : onAddAllowedRequestType(requestType);
+  const onClickAllowed = () => onAddAllowedRequestType(requestType) && onClick ? onClick() : null;
 
-  return <KeyBlueButton onClick={onClickFunction}>{children}</KeyBlueButton>;
+  return <KeyBlueButton onClick={onClickAllowed}>{children}</KeyBlueButton>;
 };
 
 export default settings(EnableExternalRequestButton);
