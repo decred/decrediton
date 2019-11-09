5d20745035ada01bcfbd9c9bca12b68894bc5a72
vctt94 <victor.duarte.oliveira@gmail.com>
2019-11-09 13:33:16 -0200

    Accept windows break line in lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     ],
     "linebreak-style": [
       "error",
-      "unix"
+      (process.platform === "win32" ? "windows" : "unix")
     ],
     "quotes": [
       "error",
