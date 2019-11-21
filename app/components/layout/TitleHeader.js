766e6b8f4e6493b67ed2b77f2868b3ad84385101
VcTT <victor.duarte.oliveira@gmail.com>
2019-11-21 16:56:16 -0300

    [settings] Fix close wallet button missin in smaller screens (#2315)

--- app/components/layout/TitleHeader.js
+++ app/components/layout/TitleHeader.js
@@ -1,15 +1,17 @@
 const TitleHeader = ({ title, iconClassName, optionalButton }) => (
-  <div className="standalone-page-header-container">
-    <div className="title-header-icon">
-      <div className={iconClassName} />
+  <div className="standalone-page-header-container is-row">
+    <div className="is-row">
+      <div className="title-header-icon">
+        <div className={iconClassName} />
+      </div>
+      <div className="title-header-title">
+        {title}
+      </div>
     </div>
-    <div className="title-header-title">
-      {title}
-    </div>
-    {optionalButton &&
-    <div className="title-header-button">
-      {optionalButton}
-    </div>
+    { optionalButton &&
+      <div className="title-header-button">
+        {optionalButton}
+      </div>
     }
   </div>
 );
