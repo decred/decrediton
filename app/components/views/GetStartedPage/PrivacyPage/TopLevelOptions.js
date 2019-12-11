fe37621c960e4497c6c33363e9bdca99013b8b88
vctt94 <victor.duarte.oliveira@gmail.com>
2019-12-11 09:12:12 -0300

    Fix disabled classnamae changing background

--- app/components/views/GetStartedPage/PrivacyPage/TopLevelOptions.js
+++ app/components/views/GetStartedPage/PrivacyPage/TopLevelOptions.js
@@ -32,7 +32,7 @@ export default ({ setupStandardPrivacy, setupDisabledPrivacy, setupCustomPrivacy
 
       <PrivacyOption
         title={<T id="privacy.options.disabled.title" m="No Outbound Connections" />}
-        icon="disabled"
+        icon="icon-disabled"
         description={<T id="privacy.options.disabled.description" m="Disables all connections to third party (non-dcrd/non-dcrwallet) services. This may prevent you from using certain features of the app. Recommended for advanced users." />}
         onClick={setupDisabledPrivacy}
       />
