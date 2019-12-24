cc4f2e847a64fae7aae14fd7d6e3b61e83ae10c2
bgptr <bgptr@protonmail.com>
2019-12-24 15:29:07 +0100

    fix margin

--- app/components/views/GovernancePage/Proposals/PoliteiaDisabled.js
+++ app/components/views/GovernancePage/Proposals/PoliteiaDisabled.js
@@ -4,7 +4,7 @@ import { EXTERNALREQUEST_POLITEIA } from "main_dev/externalRequests";
 import { proposals } from "connectors";
 
 export default proposals(({ getTokenAndInitialBatch }) => (
-  <div>
+  <div className="politeia-disabled-wrapper">
     <p><T id="proposals.enablePoliteia.description" m="Politeia integration is currently disabled in your privacy settings. Please enable it if you want to be able to access the proposal system." /></p>
     <EnableExternalRequestButton requestType={EXTERNALREQUEST_POLITEIA} onClick={getTokenAndInitialBatch}>
       <T id="proposals.enablePoliteia.button" m="Enable Politeia Integration" />
