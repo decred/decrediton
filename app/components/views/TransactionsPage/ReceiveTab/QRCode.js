6817c80ee96a7c75b9db4cf23ea5f0edd97473a5
bgptr <bgptr@protonmail.com>
2019-12-25 12:20:55 +0100

    add logo

--- app/components/views/TransactionsPage/ReceiveTab/QRCode.js
+++ app/components/views/TransactionsPage/ReceiveTab/QRCode.js
@@ -6,7 +6,7 @@ class QRCode extends React.Component {
     addr: PropTypes.string.isRequired
   };
   render() {
-    const qr_img = qr.imageSync("decred:"+this.props.addr, { type: "svg" });
+    const qr_img = qr.imageSync("decred:"+this.props.addr, { type: "svg", ec_level: "H" });
     return (<div className="receive-content-nest-qrimage" dangerouslySetInnerHTML={{ __html:qr_img }}></div>);
   }
 }
