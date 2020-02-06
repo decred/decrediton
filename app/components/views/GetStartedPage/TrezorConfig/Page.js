82b08d452e53ba0697a7081b311b2f1177816924
vctt94 <victor.duarte.oliveira@gmail.com>
2020-02-06 19:56:05 -0300

    remove duplicated intl id

--- app/components/views/GetStartedPage/TrezorConfig/Page.js
+++ app/components/views/GetStartedPage/TrezorConfig/Page.js
@@ -7,7 +7,7 @@ export default ({ children, onSendBack }) => (
     <div className="go-back-screen-button-area">
       <Tooltip text={ <GoBackMsg /> }><div className="go-back-screen-button" onClick={onSendBack}/></Tooltip>
     </div>
-    <Subtitle title={<T id="settings.subtitle" m="Trezor Config"/>} />
+    <Subtitle title={<T id="settings.trezorConfig" m="Trezor Config"/>} />
     <div className="getstarted-trezor-config-sections">
       {children}
     </div>
