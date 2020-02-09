ca15f8f43fbbb9a4ff45cf01a758d6c00f25b79c
vctt94 <victor.duarte.oliveira@gmail.com>
2020-02-08 21:10:49 -0300

    add from and to information at overview tx

--- app/components/TxHistory/TxHistoryRow/RegularTxRow.js
+++ app/components/TxHistory/TxHistoryRow/RegularTxRow.js
@@ -2,22 +2,47 @@ import Row from "./Row";
 import { Balance } from "shared";
 import { createElement as h } from "react";
 import { timeMessage } from "./index";
+import { FormattedMessage as T } from "react-intl";
 
-const RegularTxRow = ({ txAmount, txDescription, txDirection, overview, txAccountName, pending, txTs, ...props }) => (
+const RegularTxRow = ({
+  txAmount, txDescription, txDirection, overview, txAccountName, pending, txTs,
+  txOutputAddresses, txInputOutpoints, ...props
+}) => (
   <Row {...{ ...props, txAccountName, pending, overview }}>
     <div className="is-row">
       <span className="icon" />
       <span className="transaction-amount-number"><Balance amount={txDirection !== "in" ? -txAmount : txAmount} /></span>
-      {!overview &&
+      { !overview &&
         <div className="transaction-status">
           <span className="transaction-account-name">{txAccountName}</span>
-        </div>}
-      {!pending &&
+        </div>
+      }
+      { !pending &&
         <div className="transaction-time-date-spacer">
           {timeMessage(txTs, props.intl)}
-        </div>}
+        </div>
+      }
     </div>
-    <div className="transaction-amount-hash">{(txDescription.addressStr || []).join(", ")}</div>
+    <div className="transaction-amount-hash">
+      {
+        txDirection !== "in" ? (
+          <>
+            <T id="txHistory.from" m="From " />
+            { txAccountName }
+            <T id="txHistory.to" m=" To " />
+            <span className="address-shorter">{ txOutputAddresses }</span>
+          </>
+        ) : (
+          <>
+            <T id="txHistory.from" m="From " />
+            <span className="address-shorter">{ txInputOutpoints }</span>
+            <T id="txHistory.to" m=" To " />
+            { txAccountName }
+          </>
+        )
+      }
+      
+    </div>
   </Row>
 );
 
