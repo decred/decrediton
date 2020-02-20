d2bbd73d51c25aadd7157f7843bc1cc773109817
vctt94 <victor.duarte.oliveira@gmail.com>
2020-02-20 11:18:36 -0300

    Add ticket counter when showing tickets

--- app/components/views/TicketsPage/PurchaseTab/UnsignedTickets.js
+++ app/components/views/TicketsPage/PurchaseTab/UnsignedTickets.js
@@ -7,9 +7,11 @@ const UnsignedTicket = ({ splitTx, unsignedTickets }) => (
     { splitTx && <UnsignedTx
       tx={splitTx} title={<T id="purchase.splitTx" m="Split Tx:" />} />
     }
-    { unsignedTickets && unsignedTickets.map(unsignedTicket => <UnsignedTx
-      tx={unsignedTicket}
-      title={ <T id="purchase.unsignedTickets" m="Unsigned Tickets:" />} />
+    { unsignedTickets && unsignedTickets.map((unsignedTicket, index) => <UnsignedTx
+      key={index}
+      tx={ unsignedTicket }
+      title={ <T id="purchase.unsignedTickets" m="Unsigned Ticket {number}"
+        values={{ number: index + 1 }} />} />
     )}
   </>
 );
