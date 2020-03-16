c3d5b2c2a67aaf12ac251b32d23438d420e15095
vctt94 <victor.duarte.oliveira@gmail.com>
2020-03-16 17:23:59 -0300

    Make tabbedPage use hooks

--- app/components/layout/TabbedPage.js
+++ app/components/layout/TabbedPage.js
@@ -1,10 +1,11 @@
 import { Switch, Route, matchPath } from "react-router-dom";
 import { isArray } from "util";
 import { RoutedTabsHeader, RoutedTab } from "shared";
-import { routing, theming } from "connectors";
 import { TransitionMotion, spring } from "react-motion";
 import theme from "theme";
-import { createElement } from "react";
+import { createElement as h, cloneElement as k, useEffect, useState, useRef, useReducer } from "react";
+import { useSelector } from "react-redux";
+import * as sel from "selectors";
 
 export const TabbedPageTab = ({ children }) => children;
 TabbedPageTab.propTypes = {
@@ -12,135 +13,149 @@ TabbedPageTab.propTypes = {
   link: PropTypes.node.isRequired
 };
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 function getTabs(children) {
   if (!isArray(children)) children = [ children ];
   return children.filter(c => c.type === TabbedPageTab).map((c, i) => ({ index: i, tab: c }));
 }
 
-@autobind
-class TabbedPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this._tabs = getTabs(props.children);
-    const matchedTab = this.matchedTab(props.location);
-    const styles = this.getStyles(matchedTab);
-    this.state = { matchedTab, dir: "l2r", styles };
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.location.pathname === prevProps.location.pathname) {
-      return;
-    }
-
-    if (typeof this.props.onChange === "function") this.props.onChange();
-
-    const matchedTab = this.matchedTab(this.props.location);
-    const dir = prevState.matchedTab && matchedTab &&
-      prevState.matchedTab.index > matchedTab.index ? "r2l" : "l2r";
-    const styles = this.getStyles(matchedTab);
-    this.setState({ matchedTab, dir, styles });
-  }
-
-  matchedTab(location) {
-    return this._tabs.find(t => !!matchPath(location.pathname, { path: t.tab.props.path }));
-  }
-
-  getStyles(matchedTab) {
-    if (!matchedTab) {
-      return [];
-    }
-
-    const element = React.isValidElement(matchedTab.tab.props.component) ?
-      matchedTab.tab.props.component :
-      createElement(matchedTab.tab.props.component, matchedTab.tab.props, null);
-    return [ {
-      key: matchedTab.tab.props.path,
-      data: { matchedTab, element },
-      style: { left: spring(0, theme("springs.tab")), opacity: 1 }
-    } ];
-  }
-
-  willLeave() {
-    const pos = this.state.dir === "l2r" ? -1000 : +1000;
-    return { left: spring(pos, { stiffness: 180, damping: 20 }), opacity: spring(0)  };
-  }
-
-  willEnter() {
-    const pos = this.state.dir === "l2r" ? +1000 : -1000;
-    return { left: pos, opacity: 1 };
-  }
-
-  // returns the state.styles in a static container, without animations.
-  staticStyles() {
-    return (
-      <>
-        {this.state.styles.map(s =>
-          <div className="tab-content visible" key={s.key}>
-            {s.data.element}
-          </div>
-        )}
-      </>
-    );
-  }
-
-  // returns the state.styles wrapped in a TransitionMotion, to show the animations
-  animatedStyles() {
-    return (
-      <TransitionMotion
-        styles={this.state.styles}
-        willLeave={this.willLeave}
-        willEnter={this.willEnter}
-      >
-        {interpolatedStyles => {
-          return (<>
-            {interpolatedStyles.map(s => {
-              return (
-                <div
-                  className={[ "tab-content", Math.abs(s.style.left) < 0.1 ? "visible" : "" ].join(" ")}
-                  style={{ left: s.style.left, right: -s.style.left,
-                    opacity: s.style.opacity,
-                    visibility: Math.abs(s.style.left) > 990 ? "hidden" : "" }}
-                  key={s.key}
-                >
-                  {s.data.element}
-                </div>
-              );
-            })}
-          </>);
-        }}
-      </TransitionMotion>
-    );
-  }
-
-  render() {
-    let { children, header, className } = this.props;
-    if (!isArray(children)) children = [ children ];
-
-    const tabs = children.filter(c => c.type === TabbedPageTab);
-    const nonTabs = children.filter(c => c.type !== TabbedPageTab);
-
-    const tabHeaders = tabs.map(c => (RoutedTab(c.props.path, c.props.link)));
-
-    const headers = tabs.map(c =>
-      <Route key={c.props.path} path={c.props.path} component={c.props.header} />
-    );
-
-    const tabContents = this.props.uiAnimations ? this.animatedStyles() : this.staticStyles();
-    return (
-      <div className="tabbed-page">
-        <div className="tabbed-page-header">
-          {header}
-          <Switch>{headers}</Switch>
-          <RoutedTabsHeader tabs={tabHeaders} caret={this.props.caret}/>
-        </div>
-
-        <div className={"tabbed-page-body" + (className ? (" " + className) : "")}>
-          {tabContents}
-          {nonTabs}
-        </div>
-      </div>
-    );
-  }
+function getMatchedTab(location, children) {
+  const tabs = getTabs(children);
+  return tabs.find(t => !!matchPath(location.pathname, { path: t.tab.props.path }));
 }
 
-export default routing(theming(TabbedPage));
+function getStyles(matchedTab) {
+  if (!matchedTab) {
+    return [];
+  }
+
+  const element = React.isValidElement(matchedTab.tab.props.component) ?
+    k(matchedTab.tab.props.component, { ...matchedTab.tab.props, ...matchedTab.tab.props.component.props }) :
+    // If the component props are needed, it is needed to make it a valid react element
+    // before send, otherwise they will be undfined.
+    h(matchedTab.tab.props.component, { ...matchedTab.tab.props }, null);
+  return [ {
+    key: matchedTab.tab.props.path,
+    data: { matchedTab, element },
+    style: { left: spring(0, theme("springs.tab")), opacity: 1 }
+  } ];
+}
+
+function  willLeave(dir) {
+  const pos = dir === "l2r" ? -1000 : +1000;
+  return { left: spring(pos, { stiffness: 180, damping: 20 }), opacity: spring(0)  };
+}
+
+function  willEnter(dir) {
+  const pos = dir === "l2r" ? +1000 : -1000;
+  return { left: pos, opacity: 1 };
+}
+
+// returns the state.styles in a static container, without animations.
+function staticStyles(styles) {
+  return (
+    <>
+      {styles.map(s =>
+        <div className="tab-content visible" key={s.key}>
+          {s.data.element}
+        </div>
+      )}
+    </>
+  );
+}
+
+// returns the state.styles wrapped in a TransitionMotion, to show the animations
+function animatedStyles(styles, dir) {
+  return (
+    <TransitionMotion
+      styles={styles}
+      willLeave={() => willLeave(dir)}
+      willEnter={() => willEnter(dir)}
+    >
+      {interpolatedStyles => {
+        return (<>
+          {interpolatedStyles.map(s => {
+            return (
+              <div
+                className={[ "tab-content", Math.abs(s.style.left) < 0.1 ? "visible" : "" ].join(" ")}
+                style={{ left: s.style.left, right: -s.style.left,
+                  opacity: s.style.opacity,
+                  visibility: Math.abs(s.style.left) > 990 ? "hidden" : "" }}
+                key={s.key}
+              >
+                {s.data.element}
+              </div>
+            );
+          })}
+        </>);
+      }}
+    </TransitionMotion>
+  );
+}
+
+function TabbedPage(props) {
+  const { location, uiAnimations } = useSelector(state => ({
+    location: sel.location(state),
+    uiAnimations: sel.uiAnimations(state)
+  }));
+  const [ matchedTab, setMatchedTab ] = useReducer(() => getMatchedTab(location, props.children));
+
+  const previous = usePrevious({ matchedTab, location });
+  const [ dir, setDir ] = useState("l2r");
+  useEffect( () => {
+    setMatchedTab(getMatchedTab(location, props.children));
+  }, []);
+  useEffect(() => {
+    if (previous && previous.location.pathname === location.pathname ) return;
+    if (typeof props.onChange === "function") props.onChange();
+    const matchedTab = getMatchedTab(location, props.children);
+    if (!matchedTab) return;
+    // if (previous && previous.matchedTab) is false, it probably means it is the first time rendering
+    // therefore we use "l2r", as it is probably the first tab.
+    const dir = (previous && previous.matchedTab) ?
+      previous.matchedTab.index > matchedTab.index ? "r2l" : "l2r"
+      : "l2r";
+    setDir(dir);
+    setMatchedTab(matchedTab);
+  }, [ location ]);
+  let { children, header, className } = props;
+  if (!isArray(children)) children = [ children ];
+
+  const tabs = children.filter(c => c.type === TabbedPageTab);
+  const nonTabs = children.filter(c => c.type !== TabbedPageTab);
+
+  const tabHeaders = tabs.map(c => (RoutedTab(c.props.path, c.props.link)));
+
+  const headers = tabs.map(c =>
+    <Route key={c.props.path} path={c.props.path} component={c.props.header} />
+  );
+
+  const styles = getStyles(matchedTab);
+
+  const tabContents = uiAnimations ? animatedStyles(styles, dir) : staticStyles();
+
+  return (
+    <div className="tabbed-page">
+      <div className="tabbed-page-header">
+        {header}
+        <Switch>{headers}</Switch>
+        <RoutedTabsHeader tabs={tabHeaders} caret={props.caret}/>
+      </div>
+
+      <div className={"tabbed-page-body" + (className ? (" " + className) : "")}>
+        {tabContents}
+        {nonTabs}
+      </div>
+    </div>
+  );
+}
+
+export default TabbedPage;
