41a93b01406e1b78457a77fc263933f8b1507c64
Alex Yocom-Piatt <ay-p@decred.org>
2020-04-22 10:51:20 -0400

    need to transform hash

--- app/wallet/client.js
+++ app/wallet/client.js
@@ -1,5 +1,6 @@
 import Promise from "promise";
 import { TicketTypes } from "helpers/tickets";
+import { strHashToRaw } from "../helpers/byteActions";
 import { withLog as log, logOptionNoResponseData, withLogNoData, withLogNoResponseData } from "./app";
 import * as api from "middleware/walletrpc/api_pb";
 
@@ -84,8 +85,8 @@ export const setAgendaVote = log((votingService, agendaId, choiceId) =>
     votingService.setVoteChoices(request, (err, res) => err ? fail(err) : ok(res));
   }), "Set Agenda Vote");
 
-export const abandonTransaction = log((walletService, txid) => new Promise((resolve, reject) => {
+export const abandonTransaction = log((walletService, txHash) => new Promise((resolve, reject) => {
   const req = new api.AbandonTransactionRequest();
-  req.setTransactionHash(txid);
+  req.setTransactionHash(Buffer.isBuffer(txHash) ? txHash : strHashToRaw(txHash));
   walletService.abandonTransaction(req, (err) => err ? reject(err) : resolve());
 }), "Abandon Transaction");
