dbde228aad78d04a07bf4140324bd8cee70a6377
Amir Massarwa <amassarwi@gmail.com>
2020-04-26 13:15:00 +0200

    fix NoTicketsVotingInfo button

--- app/components/views/ProposalDetailsPage/helpers/NoTicketsVotingInfo.jsx
+++ app/components/views/ProposalDetailsPage/helpers/NoTicketsVotingInfo.jsx
@@ -1,10 +1,15 @@
 import styles from "../ProposalDetails.module.css";
+import { KeyBlueButton } from "buttons";
 import { FormattedMessage as T } from "react-intl";
 
 const NoTicketsVotingInfo = ({ showPurchaseTicketsPage }) => (
   <>
-    <div className={styles.noTickets}><T id="proposalDetails.votingInfo.noTickets" m="Voting is only available upon participation in Staking." /></div>
-    <KeyBlueButton onClick={showPurchaseTicketsPage}><T id="proposalDetails.votingInfo.startStakingBtn" m="Start Staking" /></KeyBlueButton>
+    <div className={styles.noTickets}>
+      <T id="proposalDetails.votingInfo.noTickets" m="Voting is only available upon participation in Staking." />
+    </div>
+    <KeyBlueButton className={styles.overviewVotingButton}  onClick={showPurchaseTicketsPage}>
+      <T id="proposalDetails.votingInfo.startStakingBtn" m="Start Staking" />
+    </KeyBlueButton>
   </>
 );
 
