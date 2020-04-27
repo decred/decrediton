d34f35676b05296b9db3e74de102fb464df77e9b
guilhermemntt <guilherme.mnetto@gmail.com>
2020-04-27 05:48:46 -0300

    Fix indentation

--- app/components/views/TicketsPage/PurchaseTab/PurchaseTickets/Form.js
+++ app/components/views/TicketsPage/PurchaseTab/PurchaseTickets/Form.js
@@ -39,88 +39,88 @@ const PurchaseTicketsForm = ({
   onDismissBackupRedeemScript,
   isWatchingOnly
 }) => (
-    <>
-      <div className="purchase-ticket-area-row is-row">
-        <div className="is-row purchase-ticket-input-address">
-          <div className="purchase-ticket-area-row-label"><T id="purchaseTickets.accountFrom" m="From" />:</div>
-          <AccountsSelect className="stakepool-purchase-ticket-input-select"
-            {...{ account, onChange: onChangeAccount }} />
-          <div className="stakepool-info-icon account-select-icon"></div>
-        </div>
-        <div className="is-row purchase-ticket-input-amount">
-          <div className="purchase-ticket-area-row-label">
-            <T id="purchaseTickets.ticketAmount" m="Amount" />:</div>
-          <NumTicketsInput
-            required
-            invalid={!getIsValid()}
-            invalidMessage={<T id="purchaseTickets.errors.insufficientBalance" m="Not enough funds" />}
-            validMessage={<PurchaseValidMessage {...{ spendable: account.spendable, ticketPrice, numTicketsToBuy }} />}
-            numTickets={numTicketsToBuy}
-            incrementNumTickets={onIncrementNumTickets}
-            decrementNumTickets={onDecrementNumTickets}
-            onChangeNumTickets={onChangeNumTickets}
-            onKeyDown={handleOnKeyDown}
-            showErrors={true}
-          />
-        </div>
+  <>
+    <div className="purchase-ticket-area-row is-row">
+      <div className="is-row purchase-ticket-input-address">
+        <div className="purchase-ticket-area-row-label"><T id="purchaseTickets.accountFrom" m="From" />:</div>
+        <AccountsSelect className="stakepool-purchase-ticket-input-select"
+          {...{ account, onChange: onChangeAccount }} />
+        <div className="stakepool-info-icon account-select-icon"></div>
       </div>
-      <div className="stakepool-purchase-ticket-info">
-        <div className="purchase-ticket-action-buttons is-column">
-          <TicketsCogs opened={!isShowingAdvanced} onClick={onToggleShowAdvanced} />
-          <ImportScriptIconButton />
-        </div>
-        <TransitionMotionWrapper {...{
-          styles: !isShowingAdvanced ? getQuickBarComponent : getAdvancedComponent,
-          willEnter: !isShowingAdvanced ? () => willEnter(270) : () => willEnter(80),
-          willLeave
-        }}
+      <div className="is-row purchase-ticket-input-amount">
+        <div className="purchase-ticket-area-row-label">
+          <T id="purchaseTickets.ticketAmount" m="Amount" />:</div>
+        <NumTicketsInput
+          required
+          invalid={!getIsValid()}
+          invalidMessage={<T id="purchaseTickets.errors.insufficientBalance" m="Not enough funds" />}
+          validMessage={<PurchaseValidMessage {...{ spendable: account.spendable, ticketPrice, numTicketsToBuy }} />}
+          numTickets={numTicketsToBuy}
+          incrementNumTickets={onIncrementNumTickets}
+          decrementNumTickets={onDecrementNumTickets}
+          onChangeNumTickets={onChangeNumTickets}
+          onKeyDown={handleOnKeyDown}
+          showErrors={true}
         />
       </div>
-      {!dismissBackupRedeemScript && (
-        <div className="warning-area">
-          <ShowWarning warn={<T id="purchase.ticket.backup.redeem.warn"
-            m="You must backup your redeem script. More information about it can be found at {link}"
-            values={{
-              link:
-                <ExternalLink href={"https://docs.decred.org/wallets/decrediton/using-decrediton/#backup-redeem-script"}>
-                  <T id="purchase.ticket.decred.docs" m="Decred docs" />
-                </ExternalLink>
-            }}
-          />} />
-          <div className="is-row backup-buttons-row-area">
-            <InvisibleConfirmModalButton
-              modalTitle={<T id="purchase.ticket.modal.title" m="Dismiss Button" />}
-              modalContent={<T id="purchase.ticket.modal.desc"
-                m="Are you sure you want to dismiss this message? Make sure your redeem scripts are backed up." />}
-              buttonLabel={<T id="purchase.ticket.dismiss.warn" m="Dismiss Message" />}
-              onSubmit={() => onDismissBackupRedeemScript()}
-              className="stakepool-content-send"
-            />
-            <KeyBlueButton className="vsp-warning-backup-redeem-button" onClick={onShowStakePoolConfig}>
-              <T id="purchase.ticket.warn.button" m="Backup Redeem Scripts" />
-            </KeyBlueButton>
-          </div>
+    </div>
+    <div className="stakepool-purchase-ticket-info">
+      <div className="purchase-ticket-action-buttons is-column">
+        <TicketsCogs opened={!isShowingAdvanced} onClick={onToggleShowAdvanced} />
+        <ImportScriptIconButton />
+      </div>
+      <TransitionMotionWrapper {...{
+        styles: !isShowingAdvanced ? getQuickBarComponent : getAdvancedComponent,
+        willEnter: !isShowingAdvanced ? () => willEnter(270) : () => willEnter(80),
+        willLeave
+      }}
+      />
+    </div>
+    {!dismissBackupRedeemScript && (
+      <div className="warning-area">
+        <ShowWarning warn={<T id="purchase.ticket.backup.redeem.warn"
+          m="You must backup your redeem script. More information about it can be found at {link}"
+          values={{
+            link:
+              <ExternalLink href={"https://docs.decred.org/wallets/decrediton/using-decrediton/#backup-redeem-script"}>
+                <T id="purchase.ticket.decred.docs" m="Decred docs" />
+              </ExternalLink>
+          }}
+        />} />
+        <div className="is-row backup-buttons-row-area">
+          <InvisibleConfirmModalButton
+            modalTitle={<T id="purchase.ticket.modal.title" m="Dismiss Button" />}
+            modalContent={<T id="purchase.ticket.modal.desc"
+              m="Are you sure you want to dismiss this message? Make sure your redeem scripts are backed up." />}
+            buttonLabel={<T id="purchase.ticket.dismiss.warn" m="Dismiss Message" />}
+            onSubmit={() => onDismissBackupRedeemScript()}
+            className="stakepool-content-send"
+          />
+          <KeyBlueButton className="vsp-warning-backup-redeem-button" onClick={onShowStakePoolConfig}>
+            <T id="purchase.ticket.warn.button" m="Backup Redeem Scripts" />
+          </KeyBlueButton>
         </div>
-      )}
-      <div className="stakepool-purchase-ticket-buttons-area">
+      </div>
+    )}
+    <div className="stakepool-purchase-ticket-buttons-area">
+      <PassphraseModalButton
+        modalTitle={<T id="tickets.revokeConfirmations" m="Revoke Tickets Confirmation" />}
+        className="stakepool-content-revoke-button"
+        onSubmit={onRevokeTickets}
+        buttonLabel={<T id="purchaseTickets.revokeBtn" m="Revoke" />}
+      />
+      {isWatchingOnly ?
+        <KeyBlueButton disabled={getIsValid && !getIsValid()} onClick={onPurchaseTickets}>
+          {purchaseLabel()}
+        </KeyBlueButton> :
         <PassphraseModalButton
-          modalTitle={<T id="tickets.revokeConfirmations" m="Revoke Tickets Confirmation" />}
-          className="stakepool-content-revoke-button"
-          onSubmit={onRevokeTickets}
-          buttonLabel={<T id="purchaseTickets.revokeBtn" m="Revoke" />}
+          modalTitle={<T id="tickets.purchaseConfirmation" m="Ticket Purchase Confirmation" />}
+          disabled={getIsValid && !getIsValid()}
+          onSubmit={onPurchaseTickets}
+          buttonLabel={purchaseLabel()}
         />
-        {isWatchingOnly ?
-          <KeyBlueButton disabled={getIsValid && !getIsValid()} onClick={onPurchaseTickets}>
-            {purchaseLabel()}
-          </KeyBlueButton> :
-          <PassphraseModalButton
-            modalTitle={<T id="tickets.purchaseConfirmation" m="Ticket Purchase Confirmation" />}
-            disabled={getIsValid && !getIsValid()}
-            onSubmit={onPurchaseTickets}
-            buttonLabel={purchaseLabel()}
-          />
-        }
-      </div>
-    </>);
+      }
+    </div>
+  </>);
 
 export default PurchaseTicketsForm;
