02865be0b1e0ee4f17e5f00005b2940c772de197
Amir Massarwa <amassarwi@gmail.com>
2020-04-29 22:34:18 +0200

    fix undefined Aux

--- app/components/shared/TransitionMotionWrapper.js
+++ app/components/shared/TransitionMotionWrapper.js
@@ -22,7 +22,7 @@ const TransitionMotionWrapper = ({
     h("div", { className: props.className }, children.map(child));
   if (!uiAnimations) {
     const actual = isFunction(styles) ? styles(props) : styles;
-    return h(Aux, {}, children(actual));
+    return h("div", {}, children(actual));
   }
 
   return h(TransitionMotion, tmProps, children);
