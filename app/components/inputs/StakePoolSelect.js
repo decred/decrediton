63aad8b6fc4871a205572c1e2dbe682369317d6f
guilhermemntt <guilherme.mnetto@gmail.com>
2020-04-29 21:06:46 -0300

    Add label attribute to created VSP object

--- app/components/inputs/StakePoolSelect.js
+++ app/components/inputs/StakePoolSelect.js
@@ -45,7 +45,7 @@ class StakePoolSelect extends React.Component {
   }
 
   newOptionCreator(obj) {
-    return { value: { Host: this.lastInput }, label: obj.label, Host: this.lastInput, newOption: true };
+      return { value: { Host: this.lastInput }, label: this.lastInput, Host: this.lastInput, newOption: true };
   }
 
   onInputChange(input) {
