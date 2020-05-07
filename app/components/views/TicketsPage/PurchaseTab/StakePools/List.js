533e9c115598439267093e4efe61e9a6f4d8f73a
guilhermemntt <guilherme.mnetto@gmail.com>
2020-05-06 21:54:01 -0300

    Add title and button actions

--- app/components/views/TicketsPage/PurchaseTab/StakePools/List.js
+++ app/components/views/TicketsPage/PurchaseTab/StakePools/List.js
@@ -1,6 +1,7 @@
 import { FormattedMessage as T } from "react-intl";
-import { KeyBlueButton, SlateGrayButton, RemoveStakePoolButton } from "buttons";
+import { KeyBlueButton } from "buttons";
 import { BackupRedeemScriptModal } from "modals";
+import { CopyToClipboard } from "shared";
 import styles from "./StakePools.module.css"
 import "style/Layout.less";
 import "style/StakePool.less";
@@ -15,27 +16,36 @@ const StakePoolsList = ({
   showModal
 }) => (
     <>
-      {[1, 2].map((
-        // {value: { Host, TicketAddress, PoolFees, Script } }
-      ) => {
-        return (<div className={styles.configuredVSP}>
-          <div className={styles.close} />
-          <div className={styles.vspURL}><T id="www" m="{value}" values={{ value: "https://teepad.com.br" }} /></div>
-          <div className={styles.poolFee}><T id="wwww" m="Pool fees: {value}" values={{ value: <span className={styles.poolFeePercentage}><T id="e" m="{value} %" values={{ value: 43 }} /></span> }} /></div>
-          <div className={styles.vspData}>
-            <span>Ticket address:</span>
-            <div className={styles.txDetails}>ewewewwewewedwewewewe</div>
-            <span>Script:</span>
-            <div style={{ position: "relative" }}>
-              <div className={styles.txDetailsRaw}>ewewewwewerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrwedwewewewe</div>
-              <div className={styles.copyToClipboard} />
-              <div className={styles.txDetailsRawShadow} />
-            </div>
-          </div>
-        </div>);
-      })}
+      <div className={styles.configuredVSPs}>
+        <div className={styles.title}>Configured VSPs</div>
+        <div>
+          {configuredStakePools.map((
+            { value: { Host, TicketAddress, PoolFees, Script } }
+          ) => {
+            return (<div className={styles.configuredVSP}>
+              <div className={styles.close} onClick={() => onRemoveStakePool(Host)} />
+              <div className={styles.vspURL}><T id="www" m="{value}" values={{ value: Host }} /></div>
+              <div className={styles.poolFee}><T id="wwww" m="Pool fees: {value}" values={{ value: <span className={styles.poolFeePercentage}><T id="e" m="{value} %" values={{ value: PoolFees }} /></span> }} /></div>
+              <div className={styles.vspData}>
+                <span>Ticket address:</span>
+                <div className={styles.txDetails}>{TicketAddress}</div>
+                <span>Script:</span>
+                <div style={{ position: "relative" }}>
+                  <div className={styles.txDetailsRaw}>{Script}</div>
+                  <CopyToClipboard className={styles.copyToClipboard} textToCopy={Script} />
+                  <div className={styles.txDetailsRawShadow} />
+                </div>
+              </div>
+              <div>
+                <BackupRedeemScriptModal key={"modal" + Host} className="backup-redeem-script-modal"
+                  show={showModal} script={Script} onCancelModal={toggleBackupModal}
+                />
+              </div>
+            </div>);
+          })}
+        </div>
 
-      {/*
+        {/*
       <div className="stakepool-flex-height">
         <div className="stakepool-content-nest-from-address">
           <div className="stakepool-content-nest-prefix-configured"><T id="stakepools.list.title" m="Configured VSP:" /></div>
@@ -98,13 +108,17 @@ const StakePoolsList = ({
         </div>
       </div>
       /* */}
-      <KeyBlueButton className="stakepool-content-send" disabled={rescanRequest} onClick={onShowAddStakePool}>
-        <T id="stakepools.list.form.submit" m="Add VSP" />
-      </KeyBlueButton>
-      <SlateGrayButton
-        className="stakepool-hide-config"
-        onClick={onHideStakePoolConfig}
-      ><T id="stakepools.list.form.cancel" m="Cancel" /></SlateGrayButton>
+
+      </div>
+      <div className={styles.buttonContainer}>
+        <div
+          className={styles.cancelVSP}
+          onClick={onHideStakePoolConfig}
+        ><T id="stakepools.list.form.cancel" m="Cancel" /></div>
+        <KeyBlueButton className={styles.addVSP} disabled={rescanRequest} onClick={onShowAddStakePool}>
+          <T id="stakepools.list.form.submit" m="Add VSP" />
+        </KeyBlueButton>
+      </div>
     </>
   );
 
