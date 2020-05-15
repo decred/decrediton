4741868be0ae5de40a3eb0d300be372d4bdb165f
guilhermemntt <guilherme.mnetto@gmail.com>
2020-05-15 02:29:36 -0300

    Delete .less file

--- app/components/views/TransactionsPage/ReceiveTab/Page.js
+++ app/components/views/TransactionsPage/ReceiveTab/Page.js
@@ -4,8 +4,6 @@ import { Subtitle } from "shared";
 import { KeyBlueButton } from "buttons";
 import QRCode from "./QRCode";
 import { FormattedMessage as T, injectIntl, defineMessages } from "react-intl";
-import "style/ReceivePage.less";
-import "style/MiscComponents.less";
 import style from "./ReceiveTab.module.css";
 
 const messages = defineMessages({
@@ -61,76 +59,73 @@ const ReceivePage = ({
             </div>
           </div>
         </div>)}
-        <Subtitle title={<T id="receive.subtitle" m="Receive DCR" />} />
-        <div className={style.receiveContent}>
-          <div className={style.receiveContentNestForAddress}>
-            <div className="receive-content-nest-prefix prefix-long">
-              <T id="receive.accountLabel" m="This address is for" />:
+      <Subtitle title={<T id="receive.subtitle" m="Receive DCR" />} />
+      <div className={style.receiveContent}>
+        <div className={style.receiveContentNestForAddress}>
+          <div className={style.receiveContentNestPrefix}>
+            <T id="receive.accountLabel" m="This address is for" />:
           </div>
-            <div className="receive-content-nest-prefix prefix-short">
-              <T id="receive.shortAccountLabel" m="Address is for" />:
+            <ReceiveAccountsSelect showAccountsButton className={style.receiveSelectAccountInput} />
+        </div>
+        <div className={style.receiveRequestedAmount}>
+          <div className={style.receiveContentNestPrefix}>
+            <T id="receive.requestedAmountLabel" m="Requested amount" />:
           </div>
-            <div className={style.receiveSelectAccountInput}>
-              <ReceiveAccountsSelect showAccountsButton className={style.receiveSelectAccountInputValue} />
-            </div>
+          <div className={style.receiveSelectAmountInput}>
+            <DcrInput
+              className={style.requestedAmountInput}
+              required={false}
+              showErrors={error && error.amount}
+              invalid={error && error.amount}
+              invalidMessage={error && error.amount}
+              amount={amountAtomValue}
+              placeholder={intl.formatMessage(messages.amountPlaceholder)}
+              onChangeAmount={e => onValidateAmount(e)}
+              onKeyDown={onKeyDown}
+            />
           </div>
-          <div className={style.receiveRequestedAmount}>
-            <div className="receive-content-nest-prefix prefix-long">
-              <T id="receive.requestedAmountLabel" m="Requested amount" />:
-          </div>
-            <div className={style.receiveSelectAmountInput}>
-              <DcrInput
-                className={style.requestedAmountInput}
-                required={false}
-                showErrors={error && error.amount}
-                invalid={error && error.amount}
-                invalidMessage={error && error.amount}
-                amount={amountAtomValue}
-                placeholder={intl.formatMessage(messages.amountPlaceholder)}
-                onChangeAmount={e => onValidateAmount(e)}
-                onKeyDown={onKeyDown}
-              />
-            </div>
-          </div>
+        </div>
 
-          <div className={style.line}>
-            <div className={style.receiveContentNestQR + " " + (tooltip && style.border)}>
-              {nextAddress}
-              <div className={style.receiveContentNestCopyQR + " " + (tooltip && style.opacity)}>
-                <div className={style.receiveContentNestCopyQRArrow} />
-                <div className={style.receiveContentNestCopyQRText}>
-                  {sum ?
-                    <T id="receiveTab.tooltipCopied" m="&#10003;   Copied to Clipboard" /> :
-                    <T id="receiveTab.tooltipGenerated" m="&#x2b;   New Address Generated" />}
-                </div>
-              </div>
-            </div>
-            <div className={style.receiveContentButtons}>
-              <div>
-                <div class={style.receiveContentCopyButton} onClick={() => {
-                  setSum(false);
-                  updateCopy()
-                }} />
-                <span><T id="receiveTab.copyHash" m="Copy" /></span>
-              </div>
-              <div><div class={style.receiveContentQRButton} onClick={() => setModal(true)} />
-                <span><T id="receiveTab.viewQR" m="View QR" /></span>
-              </div>
-              <div><div class={style.receiveContentShareButton} />
-                <span><T id="receiveTab.shareQR" m="Share" /></span>
+        <div className={style.line}>
+          <div className={style.receiveContentNestQR + " " + (tooltip && style.border)}>
+            {nextAddress}
+            <div className={style.receiveContentNestCopyQR + " " + (tooltip && style.opacity)}>
+              <div className={style.receiveContentNestCopyQRArrow} />
+              <div className={style.receiveContentNestCopyQRText}>
+                {sum ?
+                  <T id="receiveTab.tooltipGenerated" m="&#x2b; New Address Generated" /> :
+                  <T id="receiveTab.tooltipCopied" m="&#10003;  Copied to Clipboard" />}
               </div>
             </div>
           </div>
+          {/* <div className={style.receiveContentButtons}> */}
+            <div className={style.copyParent}>
+              <div className={style.receiveContentCopyButton} onClick={() => {
+                setSum(false);
+                updateCopy()
+              }} />
+              <span><T id="receiveTab.copyHash" m="Copy" /></span>
+            </div>
+            <div className={style.viewQRParent}>
+              <div class={style.receiveContentQRButton} onClick={() => setModal(true)} />
+              <span><T id="receiveTab.viewQR" m="View QR" /></span>
+            </div>
+            <div className={style.shareQRParent}>
+              <div class={style.receiveContentShareButton} />
+              <span><T id="receiveTab.shareQR" m="Share" /></span>
+            </div>
+          {/* </div> */}
         </div>
-        <div className={style.generateButton}>
-          <KeyBlueButton size="large" block={false} onClick={() => {
-            setSum(true)
-            updateCopy()
-            onRequestAddress();
-          }}>
-            <T id="receive.newAddressBtn" m="Generate new address" />
-          </KeyBlueButton>
-        </div>
+      </div>
+      <div className={style.generateButton}>
+        <KeyBlueButton size="large" block={false} className={style.blueButton} onClick={() => {
+          setSum(true)
+          updateCopy()
+          onRequestAddress();
+        }}>
+          <T id="receive.newAddressBtn" m="Generate new address" />
+        </KeyBlueButton>
+      </div>
     </>
   );
 }
