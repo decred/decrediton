c840ce57c5acba1e6dba8496e56081673d6b6e70
Guilherme Marques <39631429+guilhermemntt@users.noreply.github.com>
2020-06-01 15:00:04 -0300

    Fix scrollbar returning to top after loading proposals (#2502)

--- app/components/views/GovernancePage/Proposals/ProposalsList/ProposalsList.jsx
+++ app/components/views/GovernancePage/Proposals/ProposalsList/ProposalsList.jsx
@@ -6,35 +6,40 @@ import styles from "./ProposalsList.module.css";
 
 const ProposalsList = ({ finishedVote, tab }) => {
   const { noMoreProposals, state, proposals, loadMore } = useProposalsList(tab);
+
   switch (state.value) {
     case "idle":
       return <NoProposals />;
     case "loading":
-      return (
-        <div className={styles.loadingPage}>
-          <PoliteiaLoading center />
-        </div>
-      );
     case "success":
-      return proposals[tab] && proposals[tab].length ? (
-        <InfiniteScroll
-          hasMore={!noMoreProposals}
-          loadMore={loadMore}
-          initialLoad={false}
-          useWindow={false}
-          threshold={300}>
-          <div className={styles.proposalList}>
-            {proposals[tab].map((v) => (
-              <ProposalsListItem
-                key={v.token}
-                {...v}
-                finishedVote={finishedVote}
-              />
-            ))}
-          </div>
-        </InfiniteScroll>
-      ) : (
-        <NoProposals />
+      return (
+        <>
+          {proposals && proposals[tab] && proposals[tab].length ? (
+            <InfiniteScroll
+              hasMore={!noMoreProposals}
+              loadMore={loadMore}
+              initialLoad={false}
+              useWindow={false}
+              threshold={300}>
+              <div className={styles.proposalList}>
+                {proposals[tab].map((v) => (
+                  <ProposalsListItem
+                    key={v.token}
+                    {...v}
+                    finishedVote={finishedVote}
+                  />
+                ))}
+              </div>
+            </InfiniteScroll>
+          ) : (
+            <NoProposals />
+          )}
+          {state.value == "loading" && (
+            <div className={styles.loadingPage}>
+              <PoliteiaLoading center />
+            </div>
+          )}
+        </>
       );
     default:
       return null;
