7c3e9b2d2e990cf0786cf75fbceb4d55bc952f63
amass <amassarwi@gmail.com>
2020-06-04 01:19:47 +0200

    add arrow & hook expanded mode

--- app/components/views/ProposalDetailsPage/helpers/EligibleTickets/EligibleTickets.jsx
+++ app/components/views/ProposalDetailsPage/helpers/EligibleTickets/EligibleTickets.jsx
@@ -1,55 +1,27 @@
+import { useState } from "react";
 import styles from "./EligibleTickets.module.css";
+import { classNames } from "pi-ui";
 import { FormattedMessage as T } from "react-intl";
 
 const EligibleTickets = ({ tickets }) => {
+  const [isExapnded, setIsExpanded] = useState();
   const numOfTickets = tickets.length;
+  const toggleExapndedHandler = () => setIsExpanded(!isExapnded);
   return (
-    <div className={styles.wrapper}>
-      <div>
-        <T
-          id="proposals.detail.wallet.eligible.header"
-          m="Tickets eligible for voting: "
-        />
-        <span>{`${numOfTickets}`} total</span>
+    <div className={styles.wrapper} onClick={toggleExapndedHandler}>
+      <div className={classNames(styles.header, isExapnded && styles.expanded)}>
+        <div>
+          <T
+            id="proposals.detail.wallet.eligible.header"
+            m="Tickets eligible for voting: "
+          />
+          <span className={styles.total}>{`${numOfTickets}`} total</span>
+        </div>
+        <div className={styles.arrow}></div>
       </div>
+      {isExapnded && <div>Hellyeah</div>}
     </div>
   );
 };
 
-/* <div>
-          {walletEligibleTickets && (
-            <VerticalAccordion
-              header={
-                <div>
-                  <T
-                    id="proposals.detail.wallet.eligible.header"
-                    m="Wallet Eligible Tickets "
-                  />
-                </div>
-              }
-              show={showWalletEligibleTickets}
-              onToggleAccordion={() =>
-                toggleWalletEligibleTickets(!showWalletEligibleTickets)
-              }
-              className={styles.walletEligibleTickets}
-              headerClassName={styles.walletEligibleTicketsHeader}
-              arrowClassName={styles.walletEligibleTicketsArrow}>
-              {walletEligibleTickets.map((t, i) => (
-                <div
-                  className={classNames(
-                    "is-row",
-                    styles.walletEligibleTicketsRow
-                  )}
-                  key={`ticket-${i + 1}`}>
-                  <div className={styles.walletEligibleTicketsLabel}>
-                    <T id="proposals.detail.tickets" m="Ticket " />
-                    {i + 1}:{" "}
-                  </div>
-                  <div>{t.ticket}</div>
-                </div>
-              ))}
-            </VerticalAccordion>
-          )}
-        </div> */
-// TOOD: add proptypes
 export default EligibleTickets;
