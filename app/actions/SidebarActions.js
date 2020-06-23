27a6dbe22f103cede61716f85ed2e3ed6280c90c
bgptr <bgptr@protonmail.com>
2020-06-23 08:59:16 +0200

    recude sidebar redux actions

--- app/actions/SidebarActions.js
+++ app/actions/SidebarActions.js
@@ -1,22 +1,31 @@
 // @flow
+import * as sel from "selectors";
 
 export const EXPAND_SIDE_MENU = "EXPAND_SIDE_MENU";
 export const REDUCE_SIDE_MENU = "REDUCE_SIDE_MENU";
 export const SIDEBAR_TO_BOTTOM = "SIDEBAR_TO_BOTTOM";
 export const SIDEBAR_LEAVE_BOTTOM = "SIDEBAR_LEAVE_BOTTOM";
 
-export const expandSideBar = () => (dispatch) => {
-  dispatch({ type: EXPAND_SIDE_MENU });
+export const expandSideBar = () => (dispatch, getState) => {
+  if (!sel.expandSideBar(getState())) {
+    dispatch({ type: EXPAND_SIDE_MENU });
+  }
 };
 
-export const reduceSideBar = () => (dispatch) => {
-  dispatch({ type: REDUCE_SIDE_MENU });
+export const reduceSideBar = () => (dispatch, getState) => {
+  if (sel.expandSideBar(getState())) {
+    dispatch({ type: REDUCE_SIDE_MENU });
+  }
 };
 
-export const sidebarToBottom = () => (dispatch) => {
-  dispatch({ type: SIDEBAR_TO_BOTTOM });
+export const sidebarToBottom = () => (dispatch, getState) => {
+  if (!sel.sidebarOnBottom(getState())) {
+    dispatch({ type: SIDEBAR_TO_BOTTOM });
+  }
 };
 
-export const onSidebarLeaveBottom = () => (dispatch) => {
-  dispatch({ type: SIDEBAR_LEAVE_BOTTOM });
+export const onSidebarLeaveBottom = () => (dispatch, getState) => {
+  if (sel.sidebarOnBottom(getState())) {
+    dispatch({ type: SIDEBAR_LEAVE_BOTTOM });
+  }
 };
