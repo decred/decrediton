ec708c7f5390fa9617e69fce05a2494de5e03c50
amass <amassarwi@gmail.com>
2020-06-24 14:31:05 +0200

    fix settings scroll & select fields

--- app/components/views/GetStartedPage/CreateWalletPage/CreateWallet.jsx
+++ app/components/views/GetStartedPage/CreateWalletPage/CreateWallet.jsx
@@ -1,5 +1,8 @@
 import { classNames } from "pi-ui";
 import styles from "../GetStarted.module.css";
+// SeedWordSelect.css includes custom styling for seed word select which need to be loaded *after*
+// ReactSelectGlobal.less, both imports should be deleted when migrating to pi-ui's Select componentt!
+import "./SeedWordSelect.css";
 
 const CreateWalletPage = ({ StateComponent }) => (
   <div className={classNames(styles.getstarted, styles.content)}>
