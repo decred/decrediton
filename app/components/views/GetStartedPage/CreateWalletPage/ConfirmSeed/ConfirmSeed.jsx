625c74d989921e83db028425105f36feb57a24e6
amass <amassarwi@gmail.com>
2020-06-30 00:15:20 +0200

    func. confirm seed

--- app/components/views/GetStartedPage/CreateWalletPage/ConfirmSeed/ConfirmSeed.jsx
+++ app/components/views/GetStartedPage/CreateWalletPage/ConfirmSeed/ConfirmSeed.jsx
@@ -1,85 +1,83 @@
+import { useState, useMemo } from "react";
 import ConfirmSeedForm from "./Form";
 
-// functional comp!!
+const ConfirmSeed = ({
+  mnemonic,
+  sendBack,
+  setPassPhrase,
+  onCreateWallet,
+  isValid,
+  isCreatingWallet,
+  setSeed,
+  setError,
+  decodeSeed
+}) => {
+  const splitMnemonicArr = mnemonic.split(" ");
+  const [splitMnemonic] = useState(splitMnemonicArr);
+  const randomThreshold = 0.3;
+  const seedWordsArr = useMemo(
+    () =>
+      splitMnemonic.map((word, index) => {
+        const shouldShow = Math.random() > randomThreshold;
+        return {
+          word: shouldShow ? word : "",
+          show: shouldShow,
+          index,
+          match: shouldShow
+        };
+      }),
+    [splitMnemonic]
+  );
+  const [seedWords, setSeedWords] = useState(seedWordsArr);
 
-@autobind
-class ConfirmSeed extends React.Component {
-  constructor(props) {
-    super(props);
-    const splitMnemonic = this.props.mnemonic.split(" ");
-    // 0.3
-    const randomThreshold = 0.3;
-    const seedWords = splitMnemonic.map((word, index) => {
-      const shouldShow = Math.random() > randomThreshold;
-      return {
-        word: shouldShow ? word : "",
-        show: shouldShow,
-        index,
-        match: shouldShow
-      };
-    });
-    this.state = {
-      seedWords,
-      splitMnemonic
-    };
-  }
+  const validateSeed = (updatedSeedWords) => {
+    // this logic compares state's updated seed word array(seedWords)
+    // against mnemonic props
+    const seedWordStr = updatedSeedWords
+      .map((seedWord) => seedWord.word)
+      .join(" ");
+    if (seedWordStr === mnemonic) {
+      decodeSeed(mnemonic)
+        // if no errors happened we set the seed at our machine state
+        .then((response) => {
+          setSeed(response.getDecodedSeed());
+          setError("");
+        })
+        .catch((e) => {
+          setError(e);
+          setSeed([]);
+        });
+    } else {
+      setSeed([]);
+      setError("*Please confirm the missing words");
+    }
+  };
 
-  render() {
-    const { onChangeSeedWord } = this;
-    const { seedWords } = this.state;
-    const {
-      sendBack,
-      setPassPhrase,
-      onCreateWallet,
-      isValid,
-      isCreatingWallet
-    } = this.props;
-    return (
-      <ConfirmSeedForm
-        {...{
-          isCreatingWallet,
-          seedWords,
-          onChangeSeedWord,
-          sendBack,
-          onCreateWallet,
-          isValid,
-          setPassPhrase
-        }}
-      />
-    );
-  }
-
-  onChangeSeedWord(seedWord, update) {
-    const { seedWords, splitMnemonic } = this.state;
-    const { mnemonic } = this.props;
-    const updatedSeedWords = seedWords;
+  const onChangeSeedWord = (seedWord, update) => {
+    const updatedSeedWords = [...seedWords];
     updatedSeedWords[seedWord.index] = {
       word: update,
       show: seedWord.show,
       index: seedWord.index,
       match: splitMnemonic[seedWord.index] === update
     };
-    this.setState({ seedWords: updatedSeedWords }, () => {
-      const seedWordStr = seedWords.map((seedWord) => seedWord.word).join(" ");
-      if (seedWordStr === mnemonic) {
-        this.setState({ seedWordsError: null });
-        this.props
-          .decodeSeed(mnemonic)
-          // if no errors happened we set the seed at our machine state
-          .then((response) => {
-            this.props.setSeed(response.getDecodedSeed());
-            this.props.setError("");
-          })
-          .catch((e) => {
-            this.props.setError(e);
-            this.props.setSeed([]);
-          });
-      } else {
-        this.props.setSeed([]);
-        this.props.setError("*Please confirm the missing words");
-      }
-    });
-  }
-}
+    setSeedWords(updatedSeedWords);
+    validateSeed(updatedSeedWords);
+  };
+
+  return (
+    <ConfirmSeedForm
+      {...{
+        isCreatingWallet,
+        seedWords,
+        onChangeSeedWord,
+        sendBack,
+        onCreateWallet,
+        isValid,
+        setPassPhrase
+      }}
+    />
+  );
+};
 
 export default ConfirmSeed;
