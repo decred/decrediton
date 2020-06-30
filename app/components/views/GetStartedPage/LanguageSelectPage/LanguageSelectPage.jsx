a7d8068700924fc0328656039165706f2e425b05
amass <amassarwi@gmail.com>
2020-06-30 16:55:01 +0200

    func. LanguageSelectPage

--- app/components/views/GetStartedPage/LanguageSelectPage/LanguageSelectPage.jsx
+++ app/components/views/GetStartedPage/LanguageSelectPage/LanguageSelectPage.jsx
@@ -1,42 +1,37 @@
+import { useMemo, useState, useCallback } from "react";
 import LanguageSelectPage from "./Page";
-import { daemonStartup } from "connectors";
+import { daemonStartup } from "connectors"; // replace with a custom hook
 
-// xxxx: functionalllllll
+const LanguageSelect = ({
+  availableLanguages,
+  defaultLocale,
+  onSelectLanguage,
+  isTestNet
+}) => {
+  const defaultLang = useMemo(
+    () =>
+      availableLanguages.find((v) => v.language === defaultLocale) ||
+      availableLanguages[0],
+    [availableLanguages, defaultLocale]
+  );
 
-@autobind
-class LanguageSelect extends React.Component {
-  constructor(props) {
-    super(props);
-    const { availableLanguages, defaultLocale } = this.props;
-    this.state = {
-      selectedLang:
-        availableLanguages.find((v) => v.language === defaultLocale) ||
-        availableLanguages[0]
-    };
-  }
+  const [selectedLang, setSelectedLang] = useState(defaultLang);
 
-  render() {
-    const { selectedLang } = this.state;
-    const { onChangeSelectedLang, onSelectLang } = this;
-    const { availableLanguages, isTestNet } = this.props;
-    return (
-      <LanguageSelectPage
-        {...{
-          selectedLang,
-          availableLanguages,
-          onChangeSelectedLang,
-          onSelectLang,
-          isTestNet
-        }}
-      />
-    );
-  }
-  onChangeSelectedLang(newLang) {
-    this.setState({ selectedLang: newLang });
-  }
-  onSelectLang() {
-    this.props.onSelectLanguage(this.state.selectedLang);
-  }
-}
+  const onSelectLang = useCallback(() => {
+    onSelectLanguage(selectedLang);
+  }, [onSelectLanguage, selectedLang]);
+
+  return (
+    <LanguageSelectPage
+      {...{
+        selectedLang,
+        availableLanguages,
+        onChangeSelectedLang: setSelectedLang,
+        onSelectLang,
+        isTestNet
+      }}
+    />
+  );
+};
 
 export default daemonStartup(LanguageSelect);
--- app/components/views/GetStartedPage/LanguageSelectPage/Page.jsx
+++ app/components/views/GetStartedPage/LanguageSelectPage/Page.jsx
@@ -10,35 +10,34 @@ const LanguageSelectPage = ({
   onChangeSelectedLang,
   onSelectLang,
   isTestNet
-}) => {
-  return (
-    <div
-      className={classNames(
-        styles.pageBody,
-        styles.getstarted,
-        isTestNet && styles.testnetBody
-      )}>
-      <div className={styles.getstartedNew}>
-        <div className={styles.title}>
-          <T id="selectLang.title" m={"Welcome to Decrediton Wallet"} />
-        </div>
-        <div className={styles.titleSub}>
-          <T id="selectLang.titleSub" m={"Choose your language"} />
-        </div>
-        <div className={styles.toolbar}>
-          <LanguageSelectInput
-            value={selectedLang}
-            onChange={onChangeSelectedLang}
-            valueKey="key"
-            labelKey="description"
-            options={availableLanguages}
-          />
-          <KeyBlueButton className={styles.button} onClick={onSelectLang}>
-            <T id="selectLang.continueBtn" m={"Continue"} />
-          </KeyBlueButton>
-        </div>
+}) => (
+  <div
+    className={classNames(
+      styles.pageBody,
+      styles.getstarted,
+      isTestNet && styles.testnetBody
+    )}>
+    <div className={styles.getstartedNew}>
+      <div className={styles.title}>
+        <T id="selectLang.title" m={"Welcome to Decrediton Wallet"} />
+      </div>
+      <div className={styles.titleSub}>
+        <T id="selectLang.titleSub" m={"Choose your language"} />
+      </div>
+      <div className={styles.toolbar}>
+        <LanguageSelectInput
+          value={selectedLang}
+          onChange={onChangeSelectedLang}
+          valueKey="key"
+          labelKey="description"
+          options={availableLanguages}
+        />
+        <KeyBlueButton className={styles.button} onClick={onSelectLang}>
+          <T id="selectLang.continueBtn" m={"Continue"} />
+        </KeyBlueButton>
       </div>
     </div>
-  );
-};
+  </div>
+);
+
 export default LanguageSelectPage;
