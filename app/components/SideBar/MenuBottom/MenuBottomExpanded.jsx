2cc61e1bf31ea65effaa30c82e6e527ae34807f3
bgptr <bgptr@protonmail.com>
2020-07-21 15:19:04 +0200

    add data-testid to MenuBottomExpanded

--- app/components/SideBar/MenuBottom/MenuBottomExpanded.jsx
+++ app/components/SideBar/MenuBottom/MenuBottomExpanded.jsx
@@ -18,6 +18,7 @@ const MenuBarExpanded = ({
 }) => (
   <div className={style.bottom}>
     <div
+      data-testid="total-balance-container"
       className={style.short}
       onMouseEnter={rescanRequest ? null : onShowAccounts}
       onMouseLeave={rescanRequest ? null : onHideAccounts}>
