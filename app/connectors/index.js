fead846cbcc27da3c82c5b6cb28e05ac46f7ca05
Thiago Figueiredo <thiagofigueiredo777@gmail.com>
2020-08-05 08:45:08 -0300

    fix tests

--- app/connectors/index.js
+++ app/connectors/index.js
@@ -11,9 +11,6 @@ export { default as receive } from "./receive";
 export { default as rescan } from "./rescan";
 export { default as send } from "./send";
 export { default as service } from "./service"; // TODO: hook already exists in hooks/useService.js but connector is still used in class components
-export { default as settings } from "./settings"; // TODO: hook already exists in hooks/useSettings.js  but connector is still used in class components
-export { default as shutdownPage } from "./shutdownPage";
-export { default as spv } from "./spv";
 export { default as snackbar } from "./snackbar";
 export { default as stakeInfo } from "./stakeInfo";
 export { default as ticketAutoBuyer } from "./ticketAutoBuyer";
