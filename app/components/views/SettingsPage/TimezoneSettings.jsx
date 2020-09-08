b76a999b653579a7b4c441724f9dc94c662b232a
amass <amassarwi@gmail.com>
2020-09-08 15:00:23 +0300

    Use pi-ui's radio buttons in timezone settings

--- app/components/views/SettingsPage/TimezoneSettings.jsx
+++ app/components/views/SettingsPage/TimezoneSettings.jsx
@@ -1,69 +1,38 @@
 import { FormattedMessage as T } from "react-intl";
-import { classNames } from "pi-ui";
+import { RadioButtonGroup, classNames } from "pi-ui";
 import styles from "./Settings.module.css";
 
-const AllowableRequestType = ({
-  id,
-  name,
-  label,
-  description,
-  checked,
-  onChange
-}) => (
-  <div className={classNames(styles.row, styles.rowChecklist)}>
-    <div className={styles.label}>{label}</div>
-    <div className={styles.timezoneRadio}>
-      <input
-        type="radio"
-        id={id}
-        name={name}
-        checked={checked}
-        onChange={onChange}
-      />
-      <label htmlFor={id}></label>
-    </div>
-    <div className={styles.checklistDescription}>{description}</div>
-  </div>
-);
-
 const TimezoneSettings = ({ tempSettings, onChangeTempSettings }) => {
-  const update = (value) => () => {
+  const update = (value) => {
     onChangeTempSettings({ timezone: value });
   };
+  const timezoneOptions = [
+    {
+      value: "local",
+      label: "Local"
+    },
+    {
+      value: "utc",
+      label: "UTC"
+    }
+  ];
 
   return (
     <div className={styles.timezone}>
       <div className={styles.columnTitle}>
         <T id="settings.timezone.title" m="Timezone" />
       </div>
-      <div className={styles.columnContent}>
-        <AllowableRequestType
-          label={<T id="settings.timezone.local.label" m="Local" />}
-          id="local"
-          name="timezone"
-          value="local"
-          onChange={update("local")}
-          checked={tempSettings.timezone == "local" ? true : false}
-          description={
-            <T
-              id="settings.timezone.local.description"
-              m="Use your local timezone"
-            />
-          }
-        />
-        <AllowableRequestType
-          label={<T id="settings.timezone.utc.label" m="UTC" />}
-          id="utc"
-          name="timezone"
-          value="utc"
-          onChange={update("utc")}
-          checked={tempSettings.timezone == "utc" ? true : false}
-          description={
-            <T
-              id="settings.timezone.utx.description"
-              m="Use Universal Coordinated Time"
-            />
-          }
+      <div
+        className={classNames(
+          styles.columnContent,
+          styles.radioButtonsWrapper
+        )}>
+        <RadioButtonGroup
+          options={timezoneOptions}
+          onChange={(option) => update(option.value)}
+          value={tempSettings.timezone}
+          vertical
+          optionsClassName={styles.timezoneOption}
         />
       </div>
     </div>
