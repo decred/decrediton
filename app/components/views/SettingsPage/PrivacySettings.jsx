eec71c47e7ebc671113038a40109d2810fddd727
Amir Massarwa <amassarwi@gmail.com>
2020-09-10 16:39:25 +0300

    [pi-ui][settings] Use pi-ui's Checkbox component instead of custom one (#2663)

--- app/components/views/SettingsPage/PrivacySettings.jsx
+++ app/components/views/SettingsPage/PrivacySettings.jsx
@@ -6,10 +6,9 @@ import {
   EXTERNALREQUEST_POLITEIA,
   EXTERNALREQUEST_DCRDATA
 } from "main_dev/externalRequests";
-
 import { ChangePassphraseButton } from "buttons";
 import { WatchOnlyWarnNotification } from "shared";
-import { classNames } from "pi-ui";
+import { classNames, Checkbox } from "pi-ui";
 import styles from "./Settings.module.css";
 
 const propTypes = {
@@ -19,23 +18,6 @@ const propTypes = {
   changePassphraseRequestAttempt: PropTypes.bool.isRequired,
   onChangeTempSettings: PropTypes.func.isRequired
 };
-
-const AllowableRequestType = ({
-  id,
-  label,
-  description,
-  checked,
-  onChange
-}) => (
-  <div className={classNames(styles.row, styles.rowChecklist)}>
-    <div className={styles.label}>{label}</div>
-    <div className={styles.privacyCheckbox}>
-      <input id={id} type="checkbox" checked={checked} onChange={onChange} />
-      <label htmlFor={id}></label>
-    </div>
-    <div className={styles.checklistDescription}>{description}</div>
-  </div>
-);
 
 const PrivacySettings = ({
   tempSettings,
@@ -88,104 +70,117 @@ const PrivacySettings = ({
               </WatchOnlyWarnNotification>
             </div>
           </div>
-          <AllowableRequestType
-            label={
-              <T
-                id="settings.privacy.networkStatus.label"
-                m="Network Information"
-              />
-            }
-            id="networking"
-            description={
-              <T
-                id="settings.privacy.networkStatus.description"
-                m="General network information (block height, etc) from decred.org"
-              />
-            }
-            checked={
-              tempSettings.allowedExternalRequests.indexOf(
-                EXTERNALREQUEST_NETWORK_STATUS
-              ) > -1
-            }
-            onChange={toggle(EXTERNALREQUEST_NETWORK_STATUS)}
-          />
-          <AllowableRequestType
-            label={
-              <T id="settings.privacy.stakepoolListing.label" m="VSP Listing" />
-            }
-            id="stakepool"
-            description={
-              <T
-                id="settings.privacy.stakepoolListing.description"
-                m="List of currently available VSPs from decred.org"
-              />
-            }
-            checked={
-              tempSettings.allowedExternalRequests.indexOf(
-                EXTERNALREQUEST_STAKEPOOL_LISTING
-              ) > -1
-            }
-            onChange={toggle(EXTERNALREQUEST_STAKEPOOL_LISTING)}
-          />
+          <div className={classNames(styles.row, styles.rowChecklist)}>
+            <Checkbox
+              label={
+                <T
+                  id="settings.privacy.networkStatus.label"
+                  m="Network Information"
+                />
+              }
+              id="networking"
+              description={
+                <T
+                  id="settings.privacy.networkStatus.description"
+                  m="General network information (block height, etc) from decred.org"
+                />
+              }
+              checked={
+                tempSettings.allowedExternalRequests.indexOf(
+                  EXTERNALREQUEST_NETWORK_STATUS
+                ) > -1
+              }
+              onClick={toggle(EXTERNALREQUEST_NETWORK_STATUS)}
+            />
+          </div>
+          <div className={classNames(styles.row, styles.rowChecklist)}>
+            <Checkbox
+              label={
+                <T
+                  id="settings.privacy.stakepoolListing.label"
+                  m="VSP Listing"
+                />
+              }
+              id="stakepool"
+              description={
+                <T
+                  id="settings.privacy.stakepoolListing.description"
+                  m="List of currently available VSPs from decred.org"
+                />
+              }
+              checked={
+                tempSettings.allowedExternalRequests.indexOf(
+                  EXTERNALREQUEST_STAKEPOOL_LISTING
+                ) > -1
+              }
+              onClick={toggle(EXTERNALREQUEST_STAKEPOOL_LISTING)}
+            />
+          </div>
         </div>
       </div>
       <div className={styles.column}>
         <div>
-          <AllowableRequestType
-            label={
-              <T id="settings.privacy.updateCheck.label" m="Update Check" />
-            }
-            id="update"
-            description={
-              <T
-                id="settings.privacy.updateCheck.description"
-                m="Get latest released version from github.org"
-              />
-            }
-            checked={
-              tempSettings.allowedExternalRequests.indexOf(
-                EXTERNALREQUEST_UPDATE_CHECK
-              ) > -1
-            }
-            onChange={toggle(EXTERNALREQUEST_UPDATE_CHECK)}
-          />
-          <AllowableRequestType
-            label={<T id="settings.privacy.politeia.label" m="Politeia" />}
-            id="politeia"
-            description={
-              <T
-                id="settings.privacy.politeia.description"
-                m="List and vote on proposals on proposals.decred.org"
-              />
-            }
-            checked={
-              tempSettings.allowedExternalRequests.indexOf(
-                EXTERNALREQUEST_POLITEIA
-              ) > -1
-            }
-            onChange={toggle(EXTERNALREQUEST_POLITEIA)}
-          />
-          <AllowableRequestType
-            label={
-              <T
-                id="settings.privacy.dcrdata.label"
-                m="Decred Block Explorer"
-              />
-            }
-            id="dcrdata"
-            description={
-              <T
-                id="settings.privacy.dcrdata.description"
-                m="Access chain information from dcrdata.decred.org"
-              />
-            }
-            checked={
-              tempSettings.allowedExternalRequests.indexOf(
-                EXTERNALREQUEST_DCRDATA
-              ) > -1
-            }
-            onChange={toggle(EXTERNALREQUEST_DCRDATA)}
-          />
+          <div className={classNames(styles.row, styles.rowChecklist)}>
+            <Checkbox
+              label={
+                <T id="settings.privacy.updateCheck.label" m="Update Check" />
+              }
+              id="update"
+              description={
+                <T
+                  id="settings.privacy.updateCheck.description"
+                  m="Get latest released version from github.org"
+                />
+              }
+              checked={
+                tempSettings.allowedExternalRequests.indexOf(
+                  EXTERNALREQUEST_UPDATE_CHECK
+                ) > -1
+              }
+              onClick={toggle(EXTERNALREQUEST_UPDATE_CHECK)}
+            />
+          </div>
+          <div className={classNames(styles.row, styles.rowChecklist)}>
+            <Checkbox
+              label={<T id="settings.privacy.politeia.label" m="Politeia" />}
+              id="politeia"
+              description={
+                <T
+                  id="settings.privacy.politeia.description"
+                  m="List and vote on proposals on proposals.decred.org"
+                />
+              }
+              checked={
+                tempSettings.allowedExternalRequests.indexOf(
+                  EXTERNALREQUEST_POLITEIA
+                ) > -1
+              }
+              onClick={toggle(EXTERNALREQUEST_POLITEIA)}
+            />
+          </div>
+          <div className={classNames(styles.row, styles.rowChecklist)}>
+            <Checkbox
+              label={
+                <T
+                  id="settings.privacy.dcrdata.label"
+                  m="Decred Block Explorer"
+                />
+              }
+              id="dcrdata"
+              description={
+                <T
+                  id="settings.privacy.dcrdata.description"
+                  m="Access chain information from dcrdata.decred.org"
+                />
+              }
+              checked={
+                tempSettings.allowedExternalRequests.indexOf(
+                  EXTERNALREQUEST_DCRDATA
+                ) > -1
+              }
+              onClick={toggle(EXTERNALREQUEST_DCRDATA)}
+            />
+          </div>
         </div>
       </div>
     </>
