98b7897f95d9a66eb2d533c749b43715b13e8d76
Matheus Degiovani <opensource@matheusd.com>
2020-09-16 12:23:29 -0300

    [trezor] Add ability to decode witness section of txs
    This adds the ability to decode the witness section of serialized transactions. This will be needed to fix some issues with the trezor signing code.

--- test/unit/wallet/service.spec.js
+++ test/unit/wallet/service.spec.js
@@ -3,7 +3,7 @@ import { hexToBytes } from "../../../app/helpers";
 
 import { purchasedTicketTx, voteTx } from "../../data/HexTransactions";
 import { decodedPurchasedTicketTx, decodedVoteTx } from "../../data/decodedTransactions";
-import { MainNetParams, TestNetParams } from "../../../app/constants";
+import { TestNetParams } from "../../../app/constants";
 
 // TODO create integrations tests directory, differentiate them at our
 // package.json mv this code part to there
