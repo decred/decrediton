c875f26ac8bc58cbe029596b9ecb7b920ad5d099
bgptr <bgptr@protonmail.com>
2020-10-12 17:46:15 +0200

    fix styles

--- app/components/views/HelpPage/TutorialsTab/index.js
+++ app/components/views/HelpPage/TutorialsTab/index.js
@@ -2,6 +2,7 @@ import { FormattedMessage as T } from "react-intl";
 import { DescriptionHeader } from "layout";
 import { Subtitle } from "shared";
 import { Link } from "react-router-dom";
+import recentTicketsStyles from "../../HomePage/RecentTickets/RecentTickets.module.css";
 
 export const TutorialsTabHeader = () => (
   <DescriptionHeader
@@ -17,14 +18,14 @@ export const TutorialsTabHeader = () => (
 export const TutorialsTab = () => (
   <>
     <Subtitle title={<T id="tutorials.subtitle" m="Tutorials" />} />
-    <div className="overview-no-tickets">
+    <div className={recentTicketsStyles.overviewNoTickets}>
       {/* <Link to="/mytickets/purchase" className="purchaseTickets">
         <T id="home.noTickets.purchase" m="Stake Your Idle DCR and Earn Rewards" /> →
       </Link> */}
-      <Link to="/tutorial/staking" className="whatIsStaking">
+      <Link to="/tutorial/staking" className={recentTicketsStyles.whatIsStaking}>
         <T id="tutorials.staking" m="What is Staking (Proof-of-Stake)?" /> →
       </Link>
-      <Link to="/tutorial/ticketLifecycle" className="ticketLifeCycle">
+      <Link to="/tutorial/ticketLifecycle" className={recentTicketsStyles.ticketLifeCycle}>
         <T
           id="tutorials.ticketLifecycle"
           m="Learn About the Ticket Lifecycle"
