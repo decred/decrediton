c71a6277201994e83927e4b6cd6019e8b5a9bbd9
bgptr <bgptr@protonmail.com>
2020-10-22 20:55:04 +0200

    persistent IsLegacy across screens

--- app/components/views/TicketsPage/MyTicketsTab/MyTicketsTab.jsx
+++ app/components/views/TicketsPage/MyTicketsTab/MyTicketsTab.jsx
@@ -5,12 +5,11 @@ import { useSelector } from "react-redux";
 import * as sel from "selectors";
 import "style/StakePool.less";
 
-function Purchase() {
+function Purchase({ isLegacy, toggleIsLegacy }) {
   // Legacy hooks - this can be removed after stopping support vsp v1/v2.
   // vsps v3 don't need to import script to configure them.
   const [isShowingVsp, toggleShowVsp] = useState(false);
   // const [isShowingImportScript, setShowImportScript] = useState(false);
-  const [isLegacy, toggleIsLegacy] = useState(false);
   const isPurchasingTickets = useSelector(sel.isPurchasingTickets);
   // end of legacy hooks
 
--- app/components/views/TicketsPage/TicketsPage.jsx
+++ app/components/views/TicketsPage/TicketsPage.jsx
@@ -8,6 +8,7 @@ import { Switch, Redirect } from "react-router-dom";
 import { FormattedMessage as T } from "react-intl";
 import { purchaseTickets } from "connectors";
 import { Balance } from "shared";
+import { useState } from "react";
 import { default as PurchaseTab } from "./PurchaseTab/PurchaseTab";
 import { default as StatisticsTab } from "./StatisticsTab/StatisticsTab";
 import { default as MyTicketsTab } from "./MyTicketsTab/MyTicketsTab";
@@ -39,28 +40,31 @@ const TabHeader = purchaseTickets(({ ticketPrice }) => (
   />
 ));
 
-export default () => (
-  <TabbedPage header={<PageHeader />}>
-    <Switch>
-      <Redirect from="/tickets" exact to="/tickets/purchase" />
-    </Switch>
-    <Tab
-      path="/tickets/purchase"
-      component={PurchaseTab}
-      header={TabHeader}
-      link={<T id="tickets.tab.purchase" m="Purchase Tickets" />}
-    />
-    <Tab
-      path="/tickets/mytickets"
-      component={MyTicketsTab}
-      header={TabHeader}
-      link={<T id="tickets.tab.mytickets" m="My Tickets" />}
-    />
-    <Tab
-      path="/tickets/statistics"
-      component={StatisticsTab}
-      header={TabHeader}
-      link={<T id="tickets.tab.statistics" m="Statistics" />}
-    />
-  </TabbedPage>
-);
+export default () => {
+  const [isLegacy, toggleIsLegacy] = useState(false);
+  return (
+    <TabbedPage header={<PageHeader />}>
+      <Switch>
+        <Redirect from="/tickets" exact to="/tickets/purchase" />
+      </Switch>
+      <Tab
+        path="/tickets/purchase"
+        component={<PurchaseTab {...{ isLegacy, toggleIsLegacy }} />}
+        header={TabHeader}
+        link={<T id="tickets.tab.purchase" m="Purchase Tickets" />}
+      />
+      <Tab
+        path="/tickets/mytickets"
+        component={<MyTicketsTab {...{ isLegacy, toggleIsLegacy }} />}
+        header={TabHeader}
+        link={<T id="tickets.tab.mytickets" m="My Tickets" />}
+      />
+      <Tab
+        path="/tickets/statistics"
+        component={StatisticsTab}
+        header={TabHeader}
+        link={<T id="tickets.tab.statistics" m="Statistics" />}
+      />
+    </TabbedPage>
+  );
+};
