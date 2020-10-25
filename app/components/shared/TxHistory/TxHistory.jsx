9ece1fe295e27b7a6b0ff40794447cef6d14fea9
guilhermemntt <guilherme.mnetto@gmail.com>
2020-10-25 14:14:22 -0300

    Rebase

--- app/components/shared/TxHistory/TxHistory.jsx
+++ app/components/shared/TxHistory/TxHistory.jsx
@@ -53,9 +53,9 @@ const TxHistory = ({
     <>
       {transactions.map((tx, index) => {
         if (limit && index >= limit) return;
-
-        const txTimestamp = tx.leaveTimestamp
-          ? tx.leaveTimestamp
+        if (!tx) return;
+        const txTimestamp = tx && tx.enterTimestamp
+          ? tx.enterTimestamp
           : tx.timestamp;
         // we define the transaction icon by its rowType, so we pass it as a
         // className props
