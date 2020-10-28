be70e1b1329fd9c3a19976dc88927cca4121410f
Matheus Degiovani <opensource@matheusd.com>
2020-10-28 11:28:42 -0300

    [launcher] Ask for passphrase if account discovery is needed (#2815)

---
+++ app/components/views/GetStartedPage/OpenWallet/DiscoverAccounts.jsx
@@ -0,0 +1,38 @@
+import { useState, useCallback } from "react";
+import PassphraseForm from "./PassphraseForm";
+
+const DiscoverAccounts = ({ onSendSetPassphrase }) => {
+  const [passPhrase, setPassPhrase] = useState("");
+
+  const onContinueHandler = useCallback(() => {
+    if (passPhrase == "") {
+      return;
+    }
+
+    onSendSetPassphrase(passPhrase);
+  }, [onSendSetPassphrase, passPhrase]);
+
+
+  const onKeyDown = useCallback(
+    (e) => {
+      if (e.keyCode == 13) {
+        e.preventDefault();
+        onContinueHandler();
+      }
+    },
+    [onContinueHandler]
+  );
+
+  return (
+    <PassphraseForm
+      {...{
+        passPhrase,
+        onSetPassPhrase: setPassPhrase,
+        onOpenWallet: onContinueHandler,
+        onKeyDown
+      }}
+    />
+  );
+};
+
+export default DiscoverAccounts;
