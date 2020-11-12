b36a66c20da1e1862d97e8043034126262cf1e9e
Matheus Degiovani <opensource@matheusd.com>
2020-11-12 16:32:42 -0300

    [i18n] Bring in Zh translation

--- app/i18n/docs/zh/index.js
+++ app/i18n/docs/zh/index.js
@@ -4,5 +4,8 @@ export { default as TrezorWipeWarning } from "./Warnings/TrezorWipe.md";
 export { default as TrezorFirmwareUpdateWarning } from "./Warnings/TrezorFirmwareUpdate.md";
 export { default as TrezorWalletCreationPassPhraseWarning } from "./Warnings/TrezorWalletCreationPassPhrase.md";
 export { default as LNForceCloseChannelWarning } from "./Warnings/LNForceCloseChannel.md";
+export { default as LNWalletCreationWarning } from "./Warnings/LNWalletCreation.md";
 
 export { default as ReleaseNote1_4_0 } from "./ReleaseNotes/v1.4.0.md";
+
+export { default as LNBackupInfo } from "./LN/BackupInfo.md";
