ac303aeadad1b96526fdcfe695c32d77b7b38362
amass <amassarwi@gmail.com>
2020-11-30 00:45:20 +0200

    ditch `Help.less` & cleanup

---
+++ app/components/buttons/HelpLink/HelpLink.jsx
@@ -0,0 +1,21 @@
+import { shell } from "electron";
+import { classNames } from "pi-ui";
+import styles from "./HelpLink.module.css";
+
+const HelpLink = ({ icon, onClick, href, title, subtitle, expand }) => (
+  <div
+    className={classNames(styles.icon, styles[icon])}
+    onClick={onClick ? onClick : () => shell.openExternal(href)}>
+    <div className={styles.text}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.subtitle}>{subtitle}</div>
+    </div>
+    {expand ? (
+      <div className={styles.expand} />
+    ) : (
+      <div className={styles.external} />
+    )}
+  </div>
+);
+
+export default HelpLink;
