2d6b4938b91a1af635c89894fff63bdb35238645
Victor Guedes <victorgcramos@gmail.com>
2020-12-01 19:46:37 -0300

    [get-started] style: improve messaging on restore wallet form

--- app/components/views/GetStartedPage/PreCreateWallet/CreateWalletForm.jsx
+++ app/components/views/GetStartedPage/PreCreateWallet/CreateWalletForm.jsx
@@ -1,10 +1,7 @@
 import { FormattedMessage as T, defineMessages } from "react-intl";
 import { TextInput } from "inputs";
-import {
-  KeyBlueButton,
-  InvisibleButton,
-  ToggleSwitch
-} from "buttons";
+import { KeyBlueButton, InvisibleButton, ToggleSwitch } from "buttons";
+import { Tooltip } from "shared";
 import { NewSeedTabMsg, RestoreTabMsg } from "../messages";
 import { classNames } from "pi-ui";
 import styles from "../GetStarted.module.css";
@@ -13,6 +10,20 @@ const messages = defineMessages({
   messageWalletNamePlaceholder: {
     id: "createwallet.walletname.placehlder",
     defaultMessage: "Choose a Name"
+  },
+  messageWalletNameTooltip: {
+    id: "createwallet.walletname.tooltip",
+    defaultMessage:
+      "The name is used to identify your wallet. Restoring a wallet does not require the name to match the previous wallet name."
+  },
+  messageWalletWatchOnlyTooltip: {
+    id: "createwallet.watchonly.tooltip",
+    defaultMessage:
+      "You’ll not be able to spend any DCR associated with that wallet. It is used only to view the balance and monitor the wallet's transaction activity"
+  },
+  messageWalletTrezorTooltip: {
+    id: "createwallet.trezor.tooltip",
+    defaultMessage: "Trezor is a hardware wallet."
   },
   messageWalletMasterPubKey: {
     id: "createwallet.walletpubkey.placeholder",
@@ -59,16 +70,22 @@ const CreateWalletForm = ({
         </div>
       </div>
     ) : (
-        <div className={styles.newWalletTitleArea}>
-          <div className={classNames(styles.walletIconSmall, styles.restore)} />
-          <div className={styles.newWalletTitle}>
-            <RestoreTabMsg />
-          </div>
+      <div className={styles.newWalletTitleArea}>
+        <div className={classNames(styles.walletIconSmall, styles.restore)} />
+        <div className={styles.newWalletTitle}>
+          <RestoreTabMsg />
         </div>
-      )}
+      </div>
+    )}
     <div className={styles.daemonRow}>
       <div className={styles.daemonLabel}>
-        <T id="createwallet.walletname.label" m="Wallet Name" />
+        {!isCreateNewWallet ? (
+          <Tooltip text={intl.formatMessage(messages.messageWalletNameTooltip)}>
+            <T id="createwallet.walletname.label" m="Wallet Name" />
+          </Tooltip>
+        ) : (
+          <T id="createwallet.walletname.label" m="Wallet Name" />
+        )}
       </div>
       <div className={styles.daemonInput}>
         <TextInput
@@ -89,8 +106,16 @@ const CreateWalletForm = ({
     {!isCreateNewWallet && (
       <>
         <div className={styles.daemonRow}>
+          <div className={styles.advancedOptionsLabel}>
+            <T id="createwallet.advancedOptions.label" m="Advanced Options" />:
+          </div>
+        </div>
+        <div className={styles.daemonRow}>
           <div className={styles.daemonLabel}>
-            <T id="createwallet.walletOnly.label" m="Watch only" />
+            <Tooltip
+              text={intl.formatMessage(messages.messageWalletWatchOnlyTooltip)}>
+              <T id="createwallet.walletOnly.label" m="Watch only" />
+            </Tooltip>
           </div>
           <div className={styles.daemonInput}>
             <div className={styles.walletSwitch}>
@@ -105,20 +130,27 @@ const CreateWalletForm = ({
         </div>
         <div className={styles.daemonRow}>
           <div className={styles.daemonLabel}>
-            <T id="createwallet.isTrezor.label" m="Trezor" />
+            <Tooltip
+              text={intl.formatMessage(messages.messageWalletTrezorTooltip)}>
+              <T id="createwallet.isTrezor.label" m="Trezor" />
+            </Tooltip>
           </div>
           <div className={styles.daemonInput}>
             <div className={styles.walletSwitch}>
               <ToggleSwitch
                 enabled={isTrezor}
                 onClick={toggleTrezor}
-                enabledText={<T id="createWallet.restore.trezor.enabled" m="Enabled" />}
-                notEnabledText={<T id="createWallet.restore.trezor.disabled" m="Disabled" />}
+                enabledText={
+                  <T id="createWallet.restore.trezor.enabled" m="Enabled" />
+                }
+                notEnabledText={
+                  <T id="createWallet.restore.trezor.disabled" m="Disabled" />
+                }
               />
+              <span onClick={onShowTrezorConfig}>
+                <T id="createWallet.isTrezor.setupLink" m="(setup device)" />
+              </span>
             </div>
-            <span onClick={onShowTrezorConfig} className={styles.whatsnew}>
-              <T id="createWallet.isTrezor.setupLink" m="(setup device)" />
-            </span>
           </div>
         </div>
         <div className={styles.daemonRow}>
