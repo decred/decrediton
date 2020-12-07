4c2c028f9d30f98037c65cca705581beef5bd91c
Alex Yocom-Piatt <ay-p@decred.org>
2020-12-07 12:07:18 -0500

    Add v1.6.0 release notes

--- app/i18n/docs/en/index.js
+++ app/i18n/docs/en/index.js
@@ -29,6 +29,7 @@ export { default as ReleaseNote1_3_0 } from "./ReleaseNotes/v1.3.0.md";
 export { default as ReleaseNote1_3_1 } from "./ReleaseNotes/v1.3.1.md";
 export { default as ReleaseNote1_4_0 } from "./ReleaseNotes/v1.4.0.md";
 export { default as ReleaseNote1_5_0 } from "./ReleaseNotes/v1.5.0.md";
+export { default as ReleaseNote1_6_0 } from "./ReleaseNotes/v1.6.0.md";
 
 export { default as StakingTutorialPage01 } from "./StakingTutorial/Page01.md";
 export { default as StakingTutorialPage02 } from "./StakingTutorial/Page02.md";
