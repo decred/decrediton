5bfbf98cbf380d2e01381f4bf217ef0690d0c9c0
Alex Yocom-Piatt <ay-p@decred.org>
2020-12-08 10:48:16 -0500

    add release note gif

--- app/components/views/GetStartedPage/ReleaseNotes/ReleaseNotes.jsx
+++ app/components/views/GetStartedPage/ReleaseNotes/ReleaseNotes.jsx
@@ -6,7 +6,7 @@ const availableVersions = [
   {
     version: "1.6.0",
     docName: "ReleaseNote1_6_0",
-    imgClassName: "release-image-v150"
+    imgClassName: "release-image-v160"
   },
   {
     version: "1.5.0",
