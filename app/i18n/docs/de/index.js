e9b001b4e250d7b87b6353ea3a2aeec9631fdd8a
Matheus Degiovani <opensource@matheusd.com>
2021-01-04 13:20:00 -0300

    [i18n] Additional de translation strings (#3089)

--- app/i18n/docs/de/index.js
+++ app/i18n/docs/de/index.js
@@ -1,4 +1,32 @@
+export { default as GetStartedTutorialPage01 } from "./GetStarted/TutorialPage01.md";
+export { default as GetStartedTutorialPage02 } from "./GetStarted/TutorialPage02.md";
+export { default as GetStartedTutorialPage03 } from "./GetStarted/TutorialPage03.md";
+export { default as GetStartedTutorialPage04 } from "./GetStarted/TutorialPage04.md";
+
+export { default as BalanceOverviewInfo } from "./InfoModals/BalanceOverview.md";
+export { default as DecredConstitution } from "./InfoModals/DecredConstitution.md";
+export { default as GapLimitInfo } from "./InfoModals/GapLimit.md";
+export { default as PassphraseInfo } from "./InfoModals/Passphrase.md";
+export { default as PurchasePrivacyTicketsInfo } from "./InfoModals/PurchasePrivacyTickets.md";
+export { default as PurchaseTicketsInfo } from "./InfoModals/PurchaseTickets.md";
+export { default as ScriptNotRedeemableInfo } from "./InfoModals/ScriptNotRedeemable.md";
+export { default as SeedInfo } from "./InfoModals/Seed.md";
+export { default as SendFromUnmixedInfo } from "./InfoModals/SendFromUnmixed.md";
+export { default as SignMessageInfo } from "./InfoModals/SignMessage.md";
+export { default as TicketAutoBuyerInfo } from "./InfoModals/TicketAutoBuyer.md";
+export { default as VerifyMessageInfo } from "./InfoModals/VerifyMessage.md";
+
 export { default as LNBackupInfo } from "./LN/BackupInfo.md";
+
+export { default as StakingTutorialPage01 } from "./StakingTutorial/Page01.md";
+export { default as StakingTutorialPage02 } from "./StakingTutorial/Page02.md";
+
+export { default as TicketLifecycleTutorialPage01 } from "./TicketLifecycleTutorial/Page01.md";
+export { default as TicketLifecycleTutorialPage02 } from "./TicketLifecycleTutorial/Page02.md";
+export { default as TicketLifecycleTutorialPage03 } from "./TicketLifecycleTutorial/Page03.md";
+export { default as TicketLifecycleTutorialPage04 } from "./TicketLifecycleTutorial/Page04.md";
+export { default as TicketLifecycleTutorialPage05 } from "./TicketLifecycleTutorial/Page05.md";
+export { default as TicketLifecycleTutorialPage06 } from "./TicketLifecycleTutorial/Page06.md";
 
 export { default as LNForceCloseChannelWarning } from "./Warnings/LNForceCloseChannel.md";
 export { default as LNWalletCreationWarning } from "./Warnings/LNWalletCreation.md";
