72bc9102675a7081520b2ffe69830eab5ca28aab
bgptr <bgptr@protonmail.com>
2021-02-16 18:44:37 +0100

    fix merge conflict

--- app/components/shared/TabsHeader/TabsHeader.jsx
+++ app/components/shared/TabsHeader/TabsHeader.jsx
@@ -3,34 +3,36 @@ import { Tabs, Tab, classNames } from "pi-ui";
 
 const TabsHeader = ({ tabs, setActiveTabIndex, activeTabIndex, tabsWrapperClassName, contentClassName }) => {
   return (
-    <div
-      className={classNames(
-        styles.tabsWrapper,
-        tabsWrapperClassName && tabsWrapperClassName
-      )}>
-      <Tabs
-        activeTabIndex={activeTabIndex}
-        onSelectTab={setActiveTabIndex}
-        contentClassName={classNames(
-          styles.tabsContent,
-          contentClassName && contentClassName
-        )}
-        className={styles.tabs}>
-        {tabs.map((tab, index) => {
-          return (
-            <Tab
-              label={tab.label}
-              key={index}
-              className={classNames(
-                styles.tab,
-                tab.icon,
-                activeTabIndex === index ? styles.active : styles.inactive
-              )}>
-              {tab.component}
-            </Tab>
-          );
-        })}
-      </Tabs>
+    <div className={styles.tabsContainer}>
+      <div
+        className={classNames(
+          styles.tabsWrapper,
+          tabsWrapperClassName && tabsWrapperClassName
+        )}>
+        <Tabs
+          activeTabIndex={activeTabIndex}
+          onSelectTab={setActiveTabIndex}
+          contentClassName={classNames(
+            styles.tabsContent,
+            contentClassName && contentClassName
+          )}
+          className={styles.tabs}>
+          {tabs.map((tab, index) => {
+            return (
+              <Tab
+                label={tab.label}
+                key={index}
+                className={classNames(
+                  styles.tab,
+                  tab.icon,
+                  activeTabIndex === index ? styles.active : styles.inactive
+                )}>
+                {tab.component}
+              </Tab>
+            );
+          })}
+        </Tabs>
+      </div>
     </div>
   );
 };
