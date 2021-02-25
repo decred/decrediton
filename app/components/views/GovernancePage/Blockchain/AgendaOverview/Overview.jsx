c1044fc56f5e82501ff9e84f3991b27f7c3dd626
amass <amassarwi@gmail.com>
2021-02-26 00:05:57 +0200

    [governance] fix consensus change POW blocks number threshold.

--- app/components/views/GovernancePage/Blockchain/AgendaOverview/Overview.jsx
+++ app/components/views/GovernancePage/Blockchain/AgendaOverview/Overview.jsx
@@ -19,7 +19,7 @@ const AgendaDetails = ({ name, onClose, description }) => (
       <div>
         <T
           id="agenda.overviewDescription"
-          m="Once the majority of the PoW miners have upgraded (95% of the 100 most recent blocks are at the latest version) and the majority of the PoS miners have upgraded (75% of the votes in a 2016 block interval), the voting process begins."
+          m="Once the majority of the PoW miners have upgraded (95% of the 1000 most recent blocks are at the latest version) and the majority of the PoS miners have upgraded (75% of the votes in a 2016 block interval), the voting process begins."
         />
       </div>
     </div>
