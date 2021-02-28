669b81325a3ff1acdf85e25ec642b1de2eae25ad
bgptr <bgptr@protonmail.com>
2021-02-28 18:28:30 +0100

    destruct props

--- app/components/buttons/KeyBlueButton/KeyBlueButton.jsx
+++ app/components/buttons/KeyBlueButton/KeyBlueButton.jsx
@@ -2,11 +2,8 @@ import Button from "../Button/Button";
 import { classNames } from "pi-ui";
 import styles from "./KeyBlueButton.module.css";
 
-const KeyBlueButton = (props) => (
-  <Button
-    {...props}
-    className={classNames(styles.keyBlueButton, props.className)}
-  />
+const KeyBlueButton = ({ className, ...props }) => (
+  <Button {...props} className={classNames(styles.keyBlueButton, className)} />
 );
 
 export default KeyBlueButton;
