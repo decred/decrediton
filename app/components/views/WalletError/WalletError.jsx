1e09bcb213a42e022372bca7e7182336eb10fea5
Amir Massarwa <amassarwi@gmail.com>
2021-03-08 15:42:10 +0200

    Polish WalletError & InvalidRPCVersion components. (#3294)

---
+++ app/components/views/WalletError/WalletError.jsx
@@ -0,0 +1,26 @@
+import { useWalletError } from "./hooks";
+import styles from "./WalletError.module.css";
+
+const WalletError = () => {
+  const { getNetworkError } = useWalletError();
+  return (
+  <div style={styles.view}>
+    <div style={styles.content}>
+      {getNetworkError ? (
+        <p>
+          {getNetworkError} Please verify that your dcrd is configured correctly
+          and restart.
+        </p>
+      ) : (
+        <p>
+          {" "}
+          We have detected that your wallet has disconnected. Please reload
+          Decrediton to fix this problem.{" "}
+        </p>
+      )}
+    </div>
+  </div>
+);
+};
+
+export default WalletError;
