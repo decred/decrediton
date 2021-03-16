abec9eb50ff02f8a8833b426864b77eb79310ee0
bgptr <bgptr@protonmail.com>
2021-03-16 13:10:57 +0100

    fix css

--- app/components/inputs/PassphraseModalField/PassphraseModalField.jsx
+++ app/components/inputs/PassphraseModalField/PassphraseModalField.jsx
@@ -3,7 +3,7 @@ import style from "./PassphraseModalField.module.css";
 export default ({ label, children }) => (
   <div className={style.passphraseModalField}>
     <label>
-      <span>{label}:</span>
+      <span className={style.labelSpan}>{label}:</span>
       <div className={style.field}>{children}</div>
     </label>
   </div>
