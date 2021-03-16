9a4c9b0081455d054deabb233044cecdcfe1ec88
amass <amassarwi@gmail.com>
2021-03-16 16:35:48 +0200

    rebase smoke.

--- app/connectors/index.js
+++ app/connectors/index.js
@@ -3,4 +3,5 @@ export { default as send } from "./send";
 export { default as ticketsList } from "./ticketsList";
 export { default as ticketsOverview } from "./ticketsOverview";
 export { default as routing } from "./routing";
+export { default as modalVisible } from "./modalVisible";
 export { default as lnPage } from "./lnPage";
