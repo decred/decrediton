916e862f3cc7ddb7c11f5eb11c100cc25bfba424
Amir Massarwa <amassarwi@gmail.com>
2021-03-18 16:02:22 +0200

    Ditch unused routing redux connector. (#3317)

--- app/connectors/index.js
+++ app/connectors/index.js
@@ -2,6 +2,5 @@ export { default as purchaseTickets } from "./purchaseTickets";
 export { default as send } from "./send";
 export { default as ticketsList } from "./ticketsList";
 export { default as ticketsOverview } from "./ticketsOverview";
-export { default as routing } from "./routing";
 export { default as modalVisible } from "./modalVisible";
 export { default as lnPage } from "./lnPage";
