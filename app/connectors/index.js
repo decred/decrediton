7cab84cebc0c1c234ecb81b88d409f6c1857f32b
amass <amassarwi@gmail.com>
2021-03-19 23:39:42 +0200

    cleanup

--- app/connectors/index.js
+++ app/connectors/index.js
@@ -1,4 +1,1 @@
-export { default as send } from "./send";
-export { default as ticketsList } from "./ticketsList";
-export { default as ticketsOverview } from "./ticketsOverview";
 export { default as lnPage } from "./lnPage";
