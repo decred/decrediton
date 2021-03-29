7cffa66bbc1752b43a63cbb65abb42aae068deb0
Amir Massarwa <amassarwi@gmail.com>
2021-03-29 17:15:26 +0300

    Replace main.css global vars with sharedTheme. (#3342)

---
+++ app/style/themes/sharedTheme.js
@@ -0,0 +1,13 @@
+const sharedTheme = {
+  "sidebar-width": "296px",
+  "sidebar-width-reduced": "60px",
+  "font-family-monospace": "'Source Code Pro', monospace",
+  "font-family-regular": "'Source Sans Pro', sans-serif",
+  "font-family-regular-semi-bold": "'Source Sans Pro Semi Bold', sans-serif",
+  "input-transition-time": "230ms",
+  "disabled-box-shadow-color": "rgba(0, 0, 0, 0.1)",
+  "disabled-box-shadow": "0 1px 1px var(--disabled-box-shadow-color)",
+  "input-timing-function": "ease-in-out"
+};
+
+export default sharedTheme;
