afa5218553d23fad8f8e76feed18d96319f66e45
bgptr <bgptr@protonmail.com>
2021-04-04 20:07:55 +0200

    infoModal

--- app/components/modals/InfoModal/InfoModal.jsx
+++ app/components/modals/InfoModal/InfoModal.jsx
@@ -1,7 +1,7 @@
 import Modal from "./Modal";
 import { KeyBlueButton } from "buttons";
 import { FormattedMessage as T } from "react-intl";
-import style from "./Modals.module.css";
+import styles from "./InfoModal.module.css";
 import { classNames } from "pi-ui";
 
 const InfoModal = ({
@@ -14,12 +14,12 @@ const InfoModal = ({
   draggable
 }) => (
   <Modal
-    className={classNames(style.info, double && style.double, modalClassName)}
+    className={classNames(styles.info, double && styles.double, modalClassName)}
     {...{ show, onCancelModal, draggable }}>
     {modalTitle ? <>{modalTitle}</> : null}
-    <div className={style.infoCloseButtonTop} onClick={onCancelModal} />
-    <div className={style.infoContent}>{modalContent}</div>
-    <KeyBlueButton className={style.infoCloseButton} onClick={onCancelModal}>
+    <div className={styles.infoCloseButtonTop} onClick={onCancelModal} />
+    <div className={styles.infoContent}>{modalContent}</div>
+    <KeyBlueButton className={styles.infoCloseButton} onClick={onCancelModal}>
       <T id="infoModal.btnClose" m="Got it" />
     </KeyBlueButton>
   </Modal>
