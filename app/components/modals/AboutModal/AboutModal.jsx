979d3ed9653f6cc0201d7158c6ee653d2eb563be
bgptr <bgptr@protonmail.com>
2021-04-04 18:18:24 +0200

    AboutModal

--- app/components/modals/AboutModal/AboutModal.jsx
+++ app/components/modals/AboutModal/AboutModal.jsx
@@ -2,29 +2,29 @@ import Modal from "../Modal";
 import { ExternalLink } from "shared";
 import { FormattedMessage as T } from "react-intl";
 import { useAboutModal } from "./hooks";
-import style from "../Modals.module.css";
+import styles from "./AboutModal.module.css";
 
 const AboutModal = ({ show, onCancelModal }) => {
   const { version, updateAvailable } = useAboutModal();
 
   return (
-    <Modal className={style.about} {...{ show, onCancelModal }}>
-      <div className={style.aboutIcon} />
-      <div className={style.aboutContent}>
-        <div className={style.aboutTitle}>
+    <Modal className={styles.about} {...{ show, onCancelModal }}>
+      <div className={styles.aboutIcon} />
+      <div className={styles.aboutContent}>
+        <div className={styles.aboutTitle}>
           <T id="aboutModal.decrediton" m="Decrediton" />
         </div>
         <div
-          className={style.infoModalCloseButtonTop}
+          className={styles.infoModalCloseButtonTop}
           onClick={onCancelModal}
         />
-        <div className={style.aboutTextParagraph}>
+        <div className={styles.aboutTextParagraph}>
           <T
             id="aboutModal.paragraph1"
             m="A cross platform GUI Wallet for Decred written in node.js using Electron"
           />
         </div>
-        <div className={style.aboutTextParagraph}>
+        <div className={styles.aboutTextParagraph}>
           <T
             id="aboutModal.paragraph2a"
             m="Decrediton is free and open source software, developed and designed by the global team of"
@@ -33,7 +33,7 @@ const AboutModal = ({ show, onCancelModal }) => {
             <T id="aboutModal.paragraph2b" m="Decred contributors" />
           </ExternalLink>
         </div>
-        <div className={style.aboutTextParagraph}>
+        <div className={styles.aboutTextParagraph}>
           <T
             id="aboutModal.paragraph3"
             m="Want to help or get involved, check out"
@@ -43,28 +43,28 @@ const AboutModal = ({ show, onCancelModal }) => {
           </ExternalLink>
         </div>
       </div>
-      <div className={style.aboutBottomArea}>
-        <div className={style.aboutBottomAreaLeft}>
+      <div className={styles.aboutBottomArea}>
+        <div>
           <T id="aboutModal.version" m="Version" /> {version} -&nbsp;
           {updateAvailable ? (
             <ExternalLink
               href="https://github.com/decred/decred-binaries/releases"
-              className={style.aboutUpgrade}>
+              className={styles.aboutUpgrade}>
               <T id="aboutModal.upgradeAvailable" m="Upgrade Available" />
             </ExternalLink>
           ) : (
             <ExternalLink
               href={`https://github.com/decred/decred-binaries/releases/tag/v${version}`}
-              className={style.aboutUpgrade}>
+              className={styles.aboutUpgrade}>
               <T id="aboutModal.whatsNew" m="What's New?" />
             </ExternalLink>
           )}
         </div>
-        <div className={style.aboutBottomAreaMiddle}>
+        <div className={styles.aboutBottomAreaMiddle}>
           Copyright &copy; 2020{" "}
           <ExternalLink href="https://decred.org">Decred</ExternalLink>
         </div>
-        <div className={style.aboutBottomAreaRight}>
+        <div className={styles.aboutBottomAreaRight}>
           <ExternalLink href="https://github.com/decred/decrediton/blob/master/LICENSE">
             <T id="aboutModal.licensing" m="Licensing information" />
           </ExternalLink>
--- app/components/modals/index.js
+++ app/components/modals/index.js
@@ -8,7 +8,7 @@ export { default as PassphraseModal } from "./PassphraseModal/PassphraseModal";
 export { default as ConfirmModal } from "./ConfirmModal";
 export { default as SeedCopyConfirmModal } from "./SeedCopyConfirmModal/SeedCopyConfirmModal";
 export { default as SendFromUnmixedAccountModal } from "./SendFromUnmixedAccountModal/SendFromUnmixedAccountModal";
-export { default as AboutModal } from "./AboutModal/AboutModal";
+export { default as AboutModal } from "./AboutModal";
 export { default as BackupRedeemScriptModal } from "./BackupRedeemScriptModal";
 export { default as AddMixerAccountsModal } from "./AddMixerAccountsModal";
 export { default as SyncVSPFailedTickets } from "./SyncVSPFailedTickets";
