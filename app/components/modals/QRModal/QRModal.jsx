c59140bc4a81cc6b065a4b24559c0080b0f7d0ca
amass <amassarwi@gmail.com>
2021-04-13 03:48:13 +0300

    introduce QRModal.module.css

--- app/components/modals/QRModal/QRModal.jsx
+++ app/components/modals/QRModal/QRModal.jsx
@@ -1,6 +1,5 @@
 import Modal from "../Modal";
-// XXX this isn't ideal, we should have modal specific css module!!!
-import styles from "../Modals.module.css";
+import styles from "./QRModal.module.css";
 import { classNames } from "pi-ui";
 
 const QRModal = ({
@@ -17,7 +16,7 @@ const QRModal = ({
     className={classNames(styles.qr, modalClassName)}
     {...{ show, onCancelModal, draggable }}>
     <div className={styles.qrTitle}>{modalTitle && modalTitle}</div>
-    <div className={styles.infoCloseButtonTop} onClick={onCancelModal} />
+    <div className={styles.closeButton} onClick={onCancelModal} />
     <div className={styles.qrContent}>{modalContent}</div>
     {pagesRemaining && <div className={styles.qrCounter}>{pagesRemaining}</div>}
     {pages}
