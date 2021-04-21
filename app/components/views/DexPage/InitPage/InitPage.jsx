6458a7f21d8a4f041787f337c80ff005324b9372
amass <amassarwi@gmail.com>
2021-04-21 17:30:09 +0300

    polish InitPage

--- app/components/views/DexPage/InitPage/InitPage.jsx
+++ app/components/views/DexPage/InitPage/InitPage.jsx
@@ -6,19 +6,28 @@ const InitPage = () => {
   const { onInitDex, initDexAttempt } = useDex();
 
   return (
-    <div>
-      <T
-        id="dex.newPassphrase"
-        m="Please set a new passphrase for the DEX.  You may use the same passphrase as you use for your wallet, or chose a new one.  ** Note ** If you lose the DEX passphrase, you will be forced to create a new DEX account and pay your exchange fees again."
-      />
+    <>
+      <div>
+        <T
+          id="dex.newPassphrase"
+          m="Please set a new passphrase for the DEX.  You may use the same passphrase as you use for your wallet, or chose a new one."
+        />
+      </div>
+      <div className="margin-top-s">
+        <T
+          id="dex.newPassphraseNote"
+          m="Note: If you lose the DEX passphrase, you will be forced to create a new DEX account and pay your exchange fees again."
+        />
+      </div>
       <SetNewPassphraseModalButton
+        className="margin-top-m"
         disabled={initDexAttempt}
         modalTitle={<T id="dex.initPassphrase" m="Set Dex Passphrase" />}
         loading={initDexAttempt}
         onSubmit={onInitDex}
         buttonLabel={<T id="dex.initPassphraseButton" m="Set Dex Passphrase" />}
       />
-    </div>
+    </>
   );
 };
 
