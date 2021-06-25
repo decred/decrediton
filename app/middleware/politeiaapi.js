1810a0558c757e248283a2df195621667c9fa80b
amass <amassarwi@gmail.com>
2021-06-25 17:56:19 +0300

    revert local pi testing changes

--- app/middleware/politeiaapi.js
+++ app/middleware/politeiaapi.js
@@ -3,12 +3,12 @@ import { postJSON } from "helpers/fetch";
 // Uncomment this and comment the following definition to test locally.
 // Also uncomment a code part at externalRequests.js, as politeia is a ssl-enabled
 // service and decrediton will fail to fetch locally with a self signed cert.
-export const POLITEIA_URL_TESTNET = "https://localhost:4443";
+// export const POLITEIA_URL_TESTNET = "https://localhost:4443";
 
 // Politeia doc source:
 // https://github.com/decred/politeia/blob/master/politeiawww/api/www/v1/api.md
 
-//export const POLITEIA_URL_TESTNET = "https://test-proposals.decred.org/api";
+export const POLITEIA_URL_TESTNET = "https://test-proposals.decred.org/api";
 export const POLITEIA_URL_MAINNET = "https://proposals.decred.org/api";
 
 const POST = (piURL, path, payload = {}) =>
