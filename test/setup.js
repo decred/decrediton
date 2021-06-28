35685bc2007b495854b28553c10832d9f049ba36
Matheus Degiovani <opensource@matheusd.com>
2021-06-28 18:12:02 -0300

    Show confirmation dialog for seed

--- test/setup.js
+++ test/setup.js
@@ -2,8 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { webcrypto } from "crypto";
 
 global.React = React;
 global.PropTypes = PropTypes;
+global.crypto = webcrypto;
 
 configure({ adapter: new Adapter() });
