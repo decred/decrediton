170e6462d0bee2637d6ee0351c4f73edc5ac136e
Matheus Degiovani <opensource@matheusd.com>
2021-06-30 11:36:38 -0300

    Add polyfill for node < v15

--- test/setup.js
+++ test/setup.js
@@ -2,10 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { webcrypto } from "crypto";
+
+// TODO: Can be removed once we move past node v14.
+import { Crypto } from "@peculiar/webcrypto";
 
 global.React = React;
 global.PropTypes = PropTypes;
-global.crypto = webcrypto;
+global.crypto = new Crypto();
 
 configure({ adapter: new Adapter() });
