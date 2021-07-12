4d0b037fb41a1f45d6b904774d34fd9a0b926c61
bgptr <bgptr@protonmail.com>
2021-07-12 16:18:44 +0200

    update AddAccount modal

--- app/components/modals/AddAccountModal/AddAccountModalContent.jsx
+++ app/components/modals/AddAccountModal/AddAccountModalContent.jsx
@@ -1,22 +1,33 @@
-import { FormattedMessage as T } from "react-intl";
-import { TextInput, PassphraseModalField } from "inputs";
+import { FormattedMessage as T, defineMessages } from "react-intl";
+import { TextInput } from "inputs";
 import { PassphraseModal } from "modals";
 
-const Modal = ({ name, hasFailedAttempt, setName, ...props }) => (
+const messages = defineMessages({
+  labelText: {
+    id: "addAccountModal.label",
+    defaultMessage: "New Account Name"
+  },
+  placeholderText: {
+    id: "addAccountModal.placeholder",
+    defaultMessage: "Write the new Account Name"
+  }
+});
+
+const Modal = ({ name, hasFailedAttempt, setName, intl, ...props }) => (
   <PassphraseModal {...props}>
-    <PassphraseModalField
-      label={<T id="addAccountModal.newAccountName" m="New Account Name" />}>
-      <TextInput
-        autoFocus
-        required
-        showErrors={hasFailedAttempt}
-        id="name"
-        type="text"
-        placeholder=""
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-    </PassphraseModalField>
+    <TextInput
+      newBiggerFontStyle
+      label={<T id="addAccountModal.newAccountName" m="New Account Name" />}
+      autoFocus
+      required
+      showErrors={hasFailedAttempt}
+      id="name"
+      type="text"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      label={intl.formatMessage(messages.labelText)}
+      placeholder={intl.formatMessage(messages.placeholderText)}
+    />
   </PassphraseModal>
 );
 
