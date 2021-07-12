bc9955e3b802151005b533fa6b751e6a3bcc9543
bgptr <bgptr@protonmail.com>
2021-07-12 12:59:52 +0200

    update PassphraseModal

--- app/components/modals/PassphraseModal/PassphraseModalContent.jsx
+++ app/components/modals/PassphraseModal/PassphraseModalContent.jsx
@@ -1,9 +1,24 @@
 import DefaultModal from "../Modal";
-import { FormattedMessage as T } from "react-intl";
+import { FormattedMessage as T, defineMessages } from "react-intl";
 import styles from "./PassphraseModal.module.css";
 import { classNames } from "pi-ui";
-import { PasswordInput, PassphraseModalField } from "inputs";
+import { PasswordInput } from "inputs";
 import { ButtonsToolbar } from "shared";
+
+const messages = defineMessages({
+  labelText: {
+    id: "passphraseModal.privatePassphrase",
+    defaultMessage: "Private Passphrase"
+  },
+  placeholderText: {
+    id: "passphraseModal.placeholder",
+    defaultMessage: "Write your Private Passphrase {ifRequired}"
+  },
+  ifRequiredText: {
+    id: "passphraseModal.ifRequired",
+    defaultMessage: " (if required)"
+  }
+});
 
 const Modal = ({
   modalClassName,
@@ -18,7 +33,8 @@ const Modal = ({
   submitLabel,
   isValid,
   passphraseLabel,
-  passphraseNotRequired
+  passphraseNotRequired,
+  intl
 }) => (
   <DefaultModal
     className={classNames(styles.passphrase, modalClassName)}
@@ -37,33 +53,28 @@ const Modal = ({
       <div className={styles.headerDescription}>{modalDescription}</div>
     </div>
     <div className={styles.content}>
-      <PassphraseModalField
+      <PasswordInput
+        newBiggerFontStyle
+        autoFocus={true}
+        required={!passphraseNotRequired}
+        id="passphrase"
         label={
-          passphraseLabel ? (
-            passphraseLabel
-          ) : (
-            <T id="passphraseModal.privatePassphrase" m="Private Passphrase" />
-          )
-        }>
-        <PasswordInput
-          autoFocus={true}
-          required={!passphraseNotRequired}
-          id="passphrase"
-          placeholder={
-            passphraseNotRequired ? (
-              <T id="passphraseModal.NotRequired" m="(if required)" />
-            ) : (
-              ""
-            )
-          }
-          value={passPhrase}
-          onChange={(e) => setPassPhrase(e.target.value)}
-          onKeyDownSubmit={onSubmit}
-          showErrors={
-            passPhrase !== null && !passPhrase && !passphraseNotRequired
-          }
-        />
-      </PassphraseModalField>
+          passphraseLabel
+            ? passphraseLabel
+            : intl.formatMessage(messages.labelText)
+        }
+        placeholder={intl.formatMessage(messages.placeholderText, {
+          ifRequired: passphraseNotRequired
+            ? intl.formatMessage(messages.ifRequiredText)
+            : ""
+        })}
+        value={passPhrase}
+        onChange={(e) => setPassPhrase(e.target.value)}
+        onKeyDownSubmit={onSubmit}
+        showErrors={
+          passPhrase !== null && !passPhrase && !passphraseNotRequired
+        }
+      />
       {children}
     </div>
     <div className={styles.toolbar}>
@@ -71,5 +82,4 @@ const Modal = ({
     </div>
   </DefaultModal>
 );
-
 export default Modal;
