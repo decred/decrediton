138a4bc8b075f4c556c68d571d1213163c8002bc
bgptr <bgptr@protonmail.com>
2021-10-21 20:42:57 +0200

    add tests

--- app/containers/Wallet/Wallet.jsx
+++ app/containers/Wallet/Wallet.jsx
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import { StaticSwitch } from "shared";
 import HomePage from "components/views/HomePage/HomePage";
 import SettingsPage from "components/views/SettingsPage/SettingsPage";
-import AccountsPage from "components/views/AccountsPage/AccountsPage";
+import AccountsPage from "components/views/AccountsPage";
 import WalletError from "components/views/WalletError";
 import ErrorScreen from "components/ErrorScreen";
 import InvalidRPCVersion from "components/views/InvalidRPCVersion";
