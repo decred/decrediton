76717df85b791536774815d57fa01e3744de96b4
Alex Yocom-Piatt <ay-p@decred.org>
2021-12-22 10:43:10 -0500

    Bump v1.7.0 and add first pass release notes

--- app/components/views/GetStartedPage/ReleaseNotes/ReleaseNotes.jsx
+++ app/components/views/GetStartedPage/ReleaseNotes/ReleaseNotes.jsx
@@ -4,6 +4,11 @@ import ReleaseNotesForm from "./Form";
 // versions with release notes available. From newer to older.
 const availableVersions = [
   {
+    version: "1.7.0",
+    docName: "ReleaseNote1_7_0",
+    imgClassName: "v160"
+  },
+  {
     version: "1.6.0",
     docName: "ReleaseNote1_6_0",
     imgClassName: "v160"
