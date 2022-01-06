b63be4574c62d3c0337efe6d8a2e94f9f24c3f44
bgptr <bgptr@protonmail.com>
2022-01-06 16:24:47 +0100

    add copy button to error type snackbars

--- app/components/Snackbar/Notification/Message.jsx
+++ app/components/Snackbar/Notification/Message.jsx
@@ -2,6 +2,7 @@
 import { injectIntl } from "react-intl";
 import { ProgressRing } from "indicators";
 import style from "../Snackbar.module.css";
+import { CopyToClipboardButton } from "buttons";
 
 const Message = ({
   intl,
@@ -9,21 +10,27 @@ const Message = ({
   values,
   onDismissMessage,
   progress,
-  topNotification
+  topNotification,
+  type
 }) => {
   const txt = intl.formatMessage(message, values);
   return (
-    <div className={style.snackbarMessage} data-testid="snackbar-message">
-      {topNotification && (
-        <button
-          aria-label="Close"
-          className={style.snackbarCloseButtonTop}
-          onClick={onDismissMessage}>
-          <ProgressRing radius={13} stroke={2} progress={progress} />
-        </button>
+    <>
+      <div className={style.snackbarMessage} data-testid="snackbar-message">
+        {topNotification && (
+          <button
+            aria-label="Close"
+            className={style.snackbarCloseButtonTop}
+            onClick={onDismissMessage}>
+            <ProgressRing radius={13} stroke={2} progress={progress} />
+          </button>
+        )}
+        {txt}
+      </div>
+      {type === "Error" && (
+        <CopyToClipboardButton textToCopy={txt} className={style.copyButton} />
       )}
-      {txt}
-    </div>
+    </>
   );
 };
 
